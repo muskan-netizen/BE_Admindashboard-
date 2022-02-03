5cd7f2f813dadf9d57bc3b35185b9d353c949222
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-02-03 14:47:06 +0530

    done changes

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -402,6 +402,7 @@ class OrderController extends BaseController
                     $order->loyalty_points_earned = $loyalty_points_earned['per_order_points'];
                     $order->loyalty_membership_id = $loyalty_points_earned['loyalty_card_id'];
                     $order->scheduled_date_time = $cart->schedule_type == 'schedule' ? $cart->scheduled_date_time : null;
+                    $order->scheduled_slot = $cart->scheduled_slot ?? null;
                     $order->subscription_discount = $total_subscription_discount;
                     $order->luxury_option_id = $luxury_option->id;
                     $order->payable_amount = $payable_amount;
