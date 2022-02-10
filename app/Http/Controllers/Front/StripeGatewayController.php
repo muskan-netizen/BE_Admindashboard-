95c2ec2ca028ed89faeddc8ed8c3aa48c9c50c20
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-02-10 11:32:33 +0530

    changes in stripe fpx payment cancellation

--- app/Http/Controllers/Front/StripeGatewayController.php
+++ app/Http/Controllers/Front/StripeGatewayController.php
@@ -451,16 +451,24 @@ class StripeGatewayController extends FrontController
 
                 $meta = $paymentIntent->metadata;
                 // \Log::info($meta);
-                $payment_form = $order_number = '';
+                $user_id = $payment_form = $order_number = '';
                 // $amount = $paymentIntent->amount / 100;
                 if($meta){
                     $payment_form = $meta->payment_form;
+                    $user_id = $meta->user_id;
                 }
+                $user = User::find($user_id);
 
                 if($payment_form == 'cart'){
                     $order_number = $meta->order_number;
                     $order = Order::where('order_number', $order_number)->first();
                     if($order){
+                        $wallet_amount_used = $order->wallet_amount_used;
+                        if($wallet_amount_used > 0){
+                            $wallet = $user->wallet;
+                            $wallet->depositFloat($wallet_amount_used, ['Wallet has been <b>refunded</b> for order cancellation']);
+                        }
+
                         $order_products = OrderProduct::select('id')->where('order_id', $order->id)->get();
                         foreach($order_products as $order_prod){
                             OrderProductAddon::where('order_product_id', $order_prod->id)->delete();
