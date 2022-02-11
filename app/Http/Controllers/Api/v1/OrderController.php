1fa09375f0c1525fcab68256b23a012bb4929cce
Harbans <harbans.singh@codebrewinnovations.com>
2022-02-11 13:19:39 +0530

    update api

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -1807,6 +1807,7 @@ class OrderController extends BaseController
             }
             $vendor_order_status = VendorOrderStatus::where('order_id', $order_id)->where('vendor_id', $vendor_id)->first();
             if ($vendor_order_status->order_status_option_id == 3 ) { //$request->status_option_id == 2){
+
                 return response()->json(['status' => 'error', 'message' => __('This Order has been rejected.')]);
             }
             $vendor_order_status_detail = VendorOrderStatus::where('order_id', $order_id)->where('vendor_id', $vendor_id)->where('order_status_option_id', $order_status_option_id)->first();
