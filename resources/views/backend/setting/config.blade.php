cc486e7ad484ea5150738704083a28b568392274
Harbans <harbans.singh@codebrewinnovations.com>
2022-02-17 15:28:03 +0530

    update config

--- resources/views/backend/setting/config.blade.php
+++ resources/views/backend/setting/config.blade.php
@@ -1126,7 +1126,7 @@ $sms_crendential = json_decode($preference->sms_credentials);
                         </span>
                      </div>
                   </div>
-                  <div class="col-md-4">
+                  <div class="col-md-4 d-none">
                      <div class="form-group d-flex justify-content-between mb-3">
                         <label for="address_is_car" class="mr-2 mb-0">{{__('Car Mode')}}<small class="d-block pr-5">{{__('Enable to change addres into car details.')}}</small></label>
                        <span> <input type="checkbox" data-plugin="switchery" name="address_is_car" id="address_is_car" class="form-control" data-color="#43bee1" @if((isset($preference) && $preference->address_is_car == '1')) checked='checked' @endif>
