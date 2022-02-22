2a0cb0f6b24b90d9d76708bed0811132322b2040
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-02-22 18:13:42 +0530

    stripe fpx webview related changes

--- app/Http/Controllers/Front/StripeGatewayController.php
+++ app/Http/Controllers/Front/StripeGatewayController.php
@@ -619,10 +619,10 @@ class StripeGatewayController extends FrontController
             $payment_form = $request->action;
             $returnParams = 'amount='. $request->amount . '&payment_form=' . $payment_form;
             if($payment_form == 'cart'){
-                $returnParams .= '&order='.$request->order;
+                $returnParams .= '&order='.$request->order_number;
             }
             elseif($payment_form == 'tip'){
-                $returnParams .= '&order='.$request->order;
+                $returnParams .= '&order='.$request->order_number;
             }
             $payment_retrive_stripe_fpx_url = url('payment/retrieve/stripe_fpx' .'/?'. $returnParams);
             
