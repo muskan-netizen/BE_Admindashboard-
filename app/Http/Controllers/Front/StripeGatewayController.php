3c9e52d0aa3949b0d64a7ab1aad5b13d57bf2442
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-02-22 18:00:24 +0530

    changes regarding stripe fpx webview

--- app/Http/Controllers/Front/StripeGatewayController.php
+++ app/Http/Controllers/Front/StripeGatewayController.php
@@ -266,7 +266,7 @@ class StripeGatewayController extends FrontController
             $secret_key = stripeFPXPaymentCredentials()->secret_key;
             $stripe = new \Stripe\StripeClient($secret_key);
             
-            $webhook_url = 'https://'.$domain.'/payment/webhook/stripe_fpx';
+            $webhook_url = 'https://635e-112-196-88-218.ngrok.io/payment/webhook/stripe_fpx';
             $webhook_exists = false;
 
             // $stripe->webhookEndpoints->delete(
@@ -348,7 +348,7 @@ class StripeGatewayController extends FrontController
                 $postdata['description'] = 'Subscription Checkout';
                 $postdata['metadata']['subscription_id'] = $request->subscription_id;
             }
-                    
+            
             $payment_intent = $stripe = $stripe->paymentIntents->create($postdata);
             
             return $this->successResponse($payment_intent->client_secret);
@@ -606,5 +606,32 @@ class StripeGatewayController extends FrontController
         }
         
         http_response_code(200);
+    }
+
+
+
+    public function paymentWebViewStripeFPX(Request $request, $domain='')
+    {
+        // try{
+            $user_id = $request->user_id;
+            $user = User::find($user_id);
+            Auth::login($user);
+            $payment_form = $request->action;
+            $returnParams = 'amount='. $request->amount . '&payment_form=' . $payment_form;
+            if($payment_form == 'cart'){
+                $returnParams .= '&order='.$request->order;
+            }
+            elseif($payment_form == 'tip'){
+                $returnParams .= '&order='.$request->order;
+            }
+            $payment_retrive_stripe_fpx_url = url('payment/retrieve/stripe_fpx' .'/?'. $returnParams);
+            
+            $request->request->add(['come_from' => 'app', 'payment_form' => $payment_form]);
+            $data = $request->all();
+            return view('frontend.payment_gatway.stripe_fpx_view')->with(['data' => $data, 'payment_retrive_stripe_fpx_url'=>$payment_retrive_stripe_fpx_url]);
+        // }
+        // catch(\Exception $ex){
+        //     return redirect()->back()->with('errors', $ex->getMessage());
+        // }
     }
 }
