7f2c0cfc97858c8ac7e2cc8ed6a0687f9d32c67c
dineshk123 <dineshk@codebrewinnovations.com>
2022-02-25 19:51:19 +0530

    merge

--- app/Http/Controllers/Front/VendorController.php
+++ app/Http/Controllers/Front/VendorController.php
@@ -171,12 +171,11 @@ class VendorController extends FrontController
             if(Session::has('vendors')){
                 $vendors = Session::get('vendors');
                 if(isset($vendor) && isset($vendor->id)){
-                    if(!in_array($vendor->id, $vendors)){
+                   // if(!in_array($vendor->id, $vendors)){
                         $listData =collect();
                         return view('frontend/vendor-'.$page)->with(['show_range' => $show_range, 'range_products' => $range_products, 'vendor' => $vendor, 'listData' => $listData, 'navCategories' => $navCategories, 'newProducts' => $newProducts, 'variantSets' => $variantSets, 'brands' => $brands]);
-                        //  return view('frontend.vendor-not-in-location')->with(['show_range' => $show_range, 'range_products' => $range_products, 'vendor' => $vendor, 'listData' => $listData, 'navCategories' => $navCategories, 'newProducts' => $newProducts, 'variantSets' => $variantSets, 'brands' => $brands]);
-                    //    abort(404);
-                    }
+                       
+                  //  }
                 }
                 
             }else{
