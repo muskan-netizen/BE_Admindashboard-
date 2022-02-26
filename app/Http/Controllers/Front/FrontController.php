abedd0d7ec97568729338630abb20a71d305121a
dineshk123 <dineshk@codebrewinnovations.com>
2022-02-26 12:40:24 +0530

    merge

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -201,8 +201,9 @@ class FrontController extends Controller
                 $vendors[] = $value->id;
             }
         }
-        $vendors = $vendors->toArray();
+       
         Session::put('vendors', $vendors);
+        
         return $vendors;
     }
 
