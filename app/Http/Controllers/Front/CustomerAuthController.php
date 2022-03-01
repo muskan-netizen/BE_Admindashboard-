237b918a02be15685894cc6a01e7067b2fec98e9
gourav sharma <gourav.s@codebrewinnovations.com>
2022-03-01 18:24:31 +0530

    vendor panel issue trans client

--- app/Http/Controllers/Front/CustomerAuthController.php
+++ app/Http/Controllers/Front/CustomerAuthController.php
@@ -791,7 +791,11 @@ class CustomerAuthController extends FrontController
             $vendor->status = 0;
             $vendor->name = $request->name;
             $vendor->email = $request->email;
-            $vendor->phone_no = $user->phone_no;
+            $vendor->phone_no = $user->phone_number;
+            $vendor->city = $request->city;
+            $vendor->state = $request->state;
+            $vendor->country = $request->country;
+            $vendor->pincode = $request->pincode;
             $vendor->address = $request->address;
             $vendor->website = $request->website;
             $vendor->latitude = $request->latitude;
