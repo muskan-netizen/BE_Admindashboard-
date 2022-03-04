050826b1a3b50545695c066cb178b4ee2b935c48
Sujata Mehta <sujata@codebrewinnovations.com>
2022-03-04 16:38:21 +0530

    product dimensions

--- app/Http/Controllers/Front/UserhomeController.php
+++ app/Http/Controllers/Front/UserhomeController.php
@@ -915,6 +915,12 @@ class UserhomeController extends FrontController
             $longitude = Session::get('longitude');
 
         }
+        $p_dim = '260/100';
+        if (isset($set_template)  && $set_template->template_id == 3){
+            $p_dim = '328/248';
+        }elseif(isset($set_template)  && $set_template->template_id == 2){
+            $p_dim = '260/180';
+        }
         $selectedAddress = ($request->has('selectedAddress')) ? Session::put('selectedAddress', $request->selectedAddress) : Session::get('selectedAddress');
         $selectedPlaceId = ($request->has('selectedPlaceId')) ? Session::put('selectedPlaceId', $request->selectedPlaceId) : Session::get('selectedPlaceId');
         $preferences = ClientPreference::first();
@@ -1144,7 +1150,7 @@ class UserhomeController extends FrontController
             foreach ($on_sale_product_details as  $on_sale_product_detail) {
                 $multiply = $on_sale_product_detail->variant->first() ? $on_sale_product_detail->variant->first()->multiplier : 1;
                 $title = $on_sale_product_detail->translation->first() ? $on_sale_product_detail->translation->first()->title : $on_sale_product_detail->sku;
-                $image_url = $on_sale_product_detail->media->first() ? $on_sale_product_detail->media->first()->image->path['proxy_url'] . '260/100' . $on_sale_product_detail->media->first()->image->path['image_path'] : $this->loadDefaultImage();
+                $image_url = $on_sale_product_detail->media->first() ? $on_sale_product_detail->media->first()->image->path['proxy_url'] . $p_dim . $on_sale_product_detail->media->first()->image->path['image_path'] : $this->loadDefaultImage();
                 $on_sale_products[] = array(
                     'tag_title' => $on_sale_title??'0',
                     'image_url' => $image_url,
@@ -1167,7 +1173,7 @@ class UserhomeController extends FrontController
             foreach ($new_product_details as  $new_product_detail) {
             $multiply = $new_product_detail->variant->first() ? $new_product_detail->variant->first()->multiplier : 1;
             $title = $new_product_detail->translation->first() ? $new_product_detail->translation->first()->title : $new_product_detail->sku;
-            $image_url = $new_product_detail->media->first() ? $new_product_detail->media->first()->image->path['proxy_url'] . '260/100' . $new_product_detail->media->first()->image->path['image_path'] : $this->loadDefaultImage();
+            $image_url = $new_product_detail->media->first() ? $new_product_detail->media->first()->image->path['proxy_url'] . $p_dim . $new_product_detail->media->first()->image->path['image_path'] : $this->loadDefaultImage();
             $new_products[] = array(
                 'tag_title' => $new_products_title??0,
                 'image_url' => $image_url,
@@ -1190,7 +1196,7 @@ class UserhomeController extends FrontController
             foreach ($feature_product_details as  $feature_product_detail) {
                 $multiply = $feature_product_detail->variant->first() ? $feature_product_detail->variant->first()->multiplier : 1;
                 $title = $feature_product_detail->translation->first() ? $feature_product_detail->translation->first()->title : $feature_product_detail->sku;
-                $image_url = $feature_product_detail->media->first() ? $feature_product_detail->media->first()->image->path['proxy_url'] . '260/100' . $feature_product_detail->media->first()->image->path['image_path'] : $this->loadDefaultImage();
+                $image_url = $feature_product_detail->media->first() ? $feature_product_detail->media->first()->image->path['proxy_url'] . $p_dim . $feature_product_detail->media->first()->image->path['image_path'] : $this->loadDefaultImage();
                 $feature_products[] = array(
                     'tag_title' => $featured_products_title??'0',
                     'image_url' => $image_url,
