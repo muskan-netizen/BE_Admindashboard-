8ae2b8a7f351a87bc77259f50e4e8896aa9ea633
chander_codebrew <chandar@codebrewinnovations.com>
2022-03-04 13:05:33 +0530

    css changes template 2

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -78,8 +78,40 @@
 {{-- <div class="offset-top @if((\Request::route()->getName() != 'userHome') || ($client_preference_detail->show_icons == 0)) inner-pages-offset @endif @if($client_preference_detail->hide_nav_bar == 1) set-hide-nav-bar @endif"></div> --}}
 
 @if(count($banners))
-<section class="p-0 small-slider">
-    <div class="slide-1 home-slider mb-md-4 mb-4">
+<section class="p-0 small-slider al_desktop_banner">
+    <div class="slide-1 home-slider mb-md-4 mb-4 ">
+        @foreach($banners as $banner)
+        @php
+        $url = '';
+        if($banner->link == 'category'){
+        if($banner->category != null){
+        $url = route('categoryDetail', $banner->category->slug);
+        }
+        }
+        else if($banner->link == 'vendor'){
+        if($banner->vendor != null){
+        $url = route('vendorDetail', $banner->vendor->slug);
+        }
+        }
+        @endphp
+        <div>
+            @if($url)
+            <a href="{{$url}}">
+                @endif
+                <div class="home text-center">
+                    <img src="{{$banner->image['image_fit'] . '1920/1080' . $banner->image['image_path']}}" class="bg-img blur-up lazyload">
+                </div>
+                @if($url)
+            </a>
+            @endif
+        </div>
+        @endforeach
+    </div>
+</section>
+@endif
+@if(count($banners))
+<section class="p-0 small-slider al_mobile_banner">
+    <div class="slide-1 home-slider mb-md-4 mb-4 ">
         @foreach($banners as $banner)
         @php
         $url = '';
