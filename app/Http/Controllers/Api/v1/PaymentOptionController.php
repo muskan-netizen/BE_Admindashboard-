ebfc702dd46f9fceec334aec4f03f7741c3ed612
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-03-09 11:37:21 +0530

    done

--- app/Http/Controllers/Api/v1/PaymentOptionController.php
+++ app/Http/Controllers/Api/v1/PaymentOptionController.php
@@ -54,8 +54,7 @@ class PaymentOptionController extends BaseController{
             }else{
                 $domain = $client->sub_domain.env('SUBMAINDOMAIN');
             }
-            $domain = '192.168.97.160:9000';
-            $server_url = "http://".$domain."/";
+            $server_url = "https://".$domain."/";
             $request->serverUrl = $server_url;
             $request->currencyId = $request->header('currency');
             $function = 'postPaymentVia_'.$gateway;
