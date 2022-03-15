48588c4d4dba1abad03c258f96820fedf8e9ad3a
gourav sharma <gourav.s@codebrewinnovations.com>
2022-03-15 13:06:32 +0530

    revert file change

--- app/Http/Controllers/Api/v1/ReturnOrderController.php
+++ app/Http/Controllers/Api/v1/ReturnOrderController.php
@@ -229,7 +229,6 @@ class ReturnOrderController extends BaseController{
      */
     public function vendorOrderForCancel(Request $request, $domain = '')
     {
-       // return $request->all();
         DB::beginTransaction();
         $client_preferences = ClientPreference::first();
         try {
