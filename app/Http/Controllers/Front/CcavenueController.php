91def4845a4fcdeaecd75858e6cb2267bf96b8cc
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-03-16 12:05:34 +0530

    done changes

--- app/Http/Controllers/Front/CcavenueController.php
+++ app/Http/Controllers/Front/CcavenueController.php
@@ -118,10 +118,10 @@ class CcavenueController extends Controller
      
     $merchant_data='';
     $number = $this->orderNumber($request); // order no
-	$working_key=$this->access_key;//Shared by CCAVENUES
-	$access_code=$this->access_code;//Shared by CCAVENUES
-	$url=$this->url;//Shared by CCAVENUES
-	$user = auth()->user();
+    $working_key=$this->access_key;//Shared by CCAVENUES
+    $access_code=$this->access_code;//Shared by CCAVENUES
+    $url=$this->url;//Shared by CCAVENUES
+    $user = auth()->user();
     $address = UserAddress::where('is_primary','1')->first();
     $merchant_data = 'merchant_id='.$this->merchant_id.'&order_id='.$number.'&amount='.$request->amt.'&currency='.getPrimaryCurrencyName().'&redirect_url='.route('ccavenue.success').'&cancel_url='.route('ccavenue.success').'&language=EN&billing_name='.$user->name.'&billing_address='.$address->address.'&billing_city='.$address->city.'&billing_state='.$address->state.'&billing_zip='.$address->pincode.'&billing_country='.$address->country.'&billing_tel='.$user->phone_number.'&billing_email='.$user->email.'&delivery_name='.$user->name.'&delivery_address='.$address->address.'&delivery_city='.$address->city.'&delivery_state='.$address->state.'&delivery_zip='.$address->pincode.'&delivery_country='.$address->country.'&delivery_tel='.$user->phone_number.'&merchant_param1='.$number.'&merchant_param2='.$request->from.'&merchant_param3=mob&merchant_param4=&merchant_param5='.$user->auth_token.'&promo_code=&customer_identifier=&';
     $encrypted_data=$this->encrypt($merchant_data,$working_key); // Method for encrypting the data.
