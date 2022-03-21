3496b50d04f584dbbd226ae4126a637afe9098c9
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-03-21 12:21:01 +0530

    done

--- app/Http/Controllers/Front/LalaMovesController.php
+++ app/Http/Controllers/Front/LalaMovesController.php
@@ -246,7 +246,7 @@ class LalaMovesController extends Controller
             ->update(['lalamove_tracking_url'=>$json->data->order->shareLink,'driver_id'=>$driverId]);
             $details = OrderVendor::where('web_hook_code',$trackingId)->first();
             \Log::info('OrderVendor 1=');
-            \Log::info($details);
+            \Log::info($details->toArray());
 
             VendorOrderDispatcherStatus::Create(['order_id'=>$details->order_id,'vendor_id'=>$details->vendor_id,'dispatcher_status_option_id'=>'1']);
         }elseif(isset($json->eventType) && $json->eventType == 'ORDER_STATUS_CHANGED' && $json->data->order->status == 'ON_GOING')
@@ -257,7 +257,7 @@ class LalaMovesController extends Controller
             ->update(['lalamove_tracking_url'=>$json->data->order->shareLink,'driver_id'=>$driverId]);
             $details = OrderVendor::where('web_hook_code',$trackingId)->first();
             \Log::info('OrderVendor 2=');
-            \Log::info($details);
+            \Log::info($details->toArray());
             VendorOrderStatus::Create(['order_id'=>$details->order_id,'vendor_id'=>$details->vendor_id,'order_status_option_id'=>'4']);
 
             VendorOrderDispatcherStatus::Create(['order_id'=>$details->order_id,'vendor_id'=>$details->vendor_id,'dispatcher_status_option_id'=>'2']);
