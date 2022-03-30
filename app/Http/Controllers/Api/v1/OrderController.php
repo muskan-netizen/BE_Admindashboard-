0613fe494f50831544c7fe613bce9e7a1b757a01
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-03-30 15:13:41 +0530

    Merge branch 'dev' of inderjit

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -2151,7 +2151,7 @@ class OrderController extends BaseController
 
            // $vendor_order_status = VendorOrderStatus::where('order_id', $order_id)->where('vendor_id', $vendor_id)->first();
             $currentOrderStatus = OrderVendor::where(['vendor_id' => $request->vendor_id, 'order_id' => $request->order_id])->first();
-            Log::info(($currentOrderStatus ? $currentOrderStatus->order_status_option_id : 'no'));
+            //Log::info(($currentOrderStatus ? $currentOrderStatus->order_status_option_id : 'no'));
 
             if ($currentOrderStatus->order_status_option_id == 3 ) { 
                 //$request->status_option_id == 2){
@@ -2192,8 +2192,12 @@ class OrderController extends BaseController
 
                 $orderPlaced = true;
                 $orderData = OrderVendor::where('vendor_id', $request->vendor_id)->where('order_id', $request->order_id)->first();
+                \Log::info('in request =');
+                \Log::info(json_encode($request->all()));
 
                 if ($request->order_status_option_id == 2) {
+                    \Log::info(json_encode($orderData));
+                    
                     //Check Order delivery type
                     if ($orderData->shipping_delivery_type=='D') {
                         //Create Shipping request for dispatcher
@@ -2203,6 +2207,7 @@ class OrderController extends BaseController
                             $orderPlaced = true;
                         }
                     }elseif($orderData->shipping_delivery_type=='L'){
+                    \Log::info('In lalamove');
                         //Create Shipping place order request for Lalamove
                         $orderPlaced = $this->placeOrderRequestlalamove($request);
                     }elseif($orderData->shipping_delivery_type=='SR'){
