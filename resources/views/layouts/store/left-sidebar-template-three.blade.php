d6316e7ea0185b813ee3ab27d409618a1701e8a6
chander_codebrew <chandar@codebrewinnovations.com>
2022-03-31 15:20:20 +0530

    css changes

--- resources/views/layouts/store/left-sidebar-template-three.blade.php
+++ resources/views/layouts/store/left-sidebar-template-three.blade.php
@@ -439,9 +439,9 @@ $pages = \App\Models\Page::with([
                   @foreach($navCategories as $cate)
                   @if($cate['name'])
                   <li class="al_main_category" data-aos="zoom-in">
-                     <a href="{{route('categoryDetail', $cate['slug'])}}" onmouseover='changeImage(this,1)' onmouseout='changeImage(this,0)'>
+                     <a href="{{route('categoryDetail', $cate['slug'])}}" class="{{isset($category) && $category->slug == $cate['slug'] ? 'current_category' : ''}}" onmouseover='changeImage(this,1)' onmouseout='changeImage(this,0)'>
                         @if($client_preference_detail->show_icons==1 && (\Request::route()->getName()=='userHome' || \Request::route()->getName()=='categoryDetail'))
-                        <div class="nav-cate-img {{ \Request::route()->getName()=='userHome' ? '' : 'activ_nav'}} {{isset($category) && $category->slug == $cate['slug'] ? 'current_category' : ''}}" > <img style="height:100px;width:100px;" class="blur-up lazyload" data-icon_two="{{!is_null($cate['icon_two']) ? $cate['icon_two']['image_fit'].'200/200'.$cate['icon_two']['image_path'] : $cate['icon']['image_fit'].'200/200'.$cate['icon']['image_path']}}" data-icon="{{$cate['icon']['image_fit']}}200/200{{$cate['icon']['image_path']}}" data-src="{{$cate['icon']['image_fit']}}150/150{{$cate['icon']['image_path']}}" alt=""> </div>
+                        <div class="nav-cate-img {{ \Request::route()->getName()=='userHome' ? '' : 'activ_nav'}} " > <img style="height:100px;width:100px;" class="blur-up lazyload" data-icon_two="{{!is_null($cate['icon_two']) ? $cate['icon_two']['image_fit'].'200/200'.$cate['icon_two']['image_path'] : $cate['icon']['image_fit'].'200/200'.$cate['icon']['image_path']}}" data-icon="{{$cate['icon']['image_fit']}}200/200{{$cate['icon']['image_path']}}" data-src="{{$cate['icon']['image_fit']}}150/150{{$cate['icon']['image_path']}}" alt=""> </div>
                         @endif
                         {{$cate['name']}}
                      </a>
@@ -489,7 +489,7 @@ $pages = \App\Models\Page::with([
       }
     %>
     <li class="al_main_category" data-aos="zoom-in">
-        <a href="{{route('categoryDetail')}}/<%=category.slug %>" onmouseover='changeImage(this,1)' onmouseout='changeImage(this,0)'>
+        <a href="{{route('categoryDetail')}}/<%=category.slug %>" class="{{isset($category) && $category->slug == $cate['slug'] ? 'current_category' : ''}}" onmouseover='changeImage(this,1)' onmouseout='changeImage(this,0)'>
             @if($client_preference_detail->show_icons==1)
             <div class="nav-cate-img {{ \Request::route()->getName()=='userHome' ? '' : 'activ_nav'}}">
                 <img style="height:100px;width:100px;" class="blur-up lazyload" data-icon_two="<%=icon_2_url %>" data-icon="<%=category.icon.image_fit %>200/200<%=category.icon.image_path %>" data-src="<%=category.icon.image_fit %>200/200<%=category.icon.image_path %>" alt="">
