355f65f45b6fd543cbc3cb43ada5418b84d12b1c
chander_codebrew <chandar@codebrewinnovations.com>
2022-04-06 15:51:33 +0530

    css changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -286,7 +286,7 @@ window.initializeSlider = function initializeSlider() {
         responsive: [
             { breakpoint: 1199, settings: { slidesToShow: 4, slidesToScroll: 3, infinite: true, dots: false, centerMode: false, } },
             { breakpoint: 991, settings: { slidesToShow: 3, slidesToScroll: 3, dots: false, centerMode: true, } },
-            { breakpoint: 767, settings: { slidesToShow: 2, slidesToScroll: 1, dots: false, centerMode: true, } }
+            { breakpoint: 767, settings: { slidesToShow: 1, slidesToScroll: 1, dots: false, centerMode: true, } }
         ]
     });
     $('.al_t2_suppliers-slider').slick({
