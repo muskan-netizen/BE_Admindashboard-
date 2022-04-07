c65f01fa9848d5f2c2f132b697c17b5b4bf8bd75
chander_codebrew <chandar@codebrewinnovations.com>
2022-04-07 17:44:43 +0530

    css changes

--- resources/views/frontend/cate-subcategorys.blade.php
+++ resources/views/frontend/cate-subcategorys.blade.php
@@ -175,8 +175,8 @@
                                                 @if(!empty($category->childs) && count($category->childs) > 0)
                                                     @foreach($category->childs->toArray() as $cate)
                                                     <div class="col-xl-3 col-6 col-grid-box">
-                                                        <a href="{{route('categoryDetail', $cate['slug'])}}"  class="product-box scale-effect m-0">
-                                                            <div class="product-image p-0"><img width="100%" alt="" class="blur-up lazyload" data-src="{{$cate['icon']['proxy_url'] . '500/500' . $cate['icon']['image_path']}}" ></div>
+                                                        <a href="{{route('categoryDetail', $cate['slug'])}}"  class="product-box scale-effect m-0" onmouseover='changeImage(this,1)' onmouseout='changeImage(this,0)'>
+                                                            <div class="product-image p-0"><img width="100%" alt="" class="blur-up lazyload" data-icon_two="{{isset($cate['icon_two']) && !is_null($cate['icon_two']) ? $cate['icon_two']['image_fit'].'500/500'.$cate['icon_two']['image_path'] : $cate['icon']['image_fit'].'500/500'.$cate['icon']['image_path']}}" data-icon="{{$cate['icon']['image_fit']}}500/500{{$cate['icon']['image_path']}}" data-src="{{$cate['icon']['proxy_url'] . '500/500' . $cate['icon']['image_path']}}" ></div>
                                                             <div class="media-body align-self-center">
                                                                 <div class="inner_spacing w-100">
                                                                     <h3 class="d-flex align-items-center justify-content-between">
@@ -209,4 +209,24 @@
 </section>
 @endsection
 @section('script')
+<script type="text/javascript">
+	function changeImage(image2, check) {
+       var image = $(image2).children('.product-image').children("img");
+       var  icon = image.attr('data-icon');
+       var  icon_two = image.attr('data-icon_two');
+       if(check == 1)
+       {
+	        setTimeout(function () {
+	            image.attr('data-src',icon_two);
+	            image.attr('src',icon_two);
+	        },200);
+       }else if(check == 0){
+            setTimeout(function () {
+                image.attr('data-src',icon);
+                image.attr('src',icon);
+            },200);
+
+       }
+    }
+</script>
 @endsection
