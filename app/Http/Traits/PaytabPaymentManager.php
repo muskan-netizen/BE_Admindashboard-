e6641fe799cdba543ea5e851033cab23cabc5c3a
Sujata Mehta <sujata@codebrewinnovations.com>
2022-04-11 15:43:16 +0530

    paytab setup

--- app/Http/Traits/PaytabPaymentManager.php
+++ app/Http/Traits/PaytabPaymentManager.php
@@ -5,14 +5,18 @@ use Paytabscom\Laravel_paytabs\Facades\paypage;
 use Auth, Log;
 trait PaytabPaymentManager{
 
-  public function createPaymentpage($data)
+  public function createPaymentpage($data,$user,$address = null)
   {
-    $pay= paypage::sendPaymentCode('all')
-        ->sendTransaction('sale')
-        ->sendCart(10,1000,'test')
-        ->sendCustomerDetails('Walaa Elsaeed', 'w.elsaeed@paytabs.com', '0101111111', 'test', 'Nasr City', 'Cairo', 'EG', '1234','100.279.20.10')
-        ->sendShippingDetails('Walaa Elsaeed', 'w.elsaeed@paytabs.com', '0101111111', 'test', 'Nasr City', 'Cairo', 'EG', '1234','100.279.20.10')
-        ->sendURLs('https://sales.focushires.com', 'https://sales.focushires.com')
+    if(is_null($address))
+    {
+      $address = (object)[];
+    }
+    $pay = paypage::sendPaymentCode('all')
+        ->sendTransaction('Auth')
+        ->sendCart(mt_rand(10000000,99999999),(int)$data['amount'],'test1')
+        ->sendCustomerDetails($user->name??'', $user->email??'', '0101111111', $address->address??'', $address->city??'', $address->state??'', $address->country_code??'', $address->pincode??'','100.279.20.11')
+        ->sendShippingDetails('same as billing')
+        ->sendURLs(route('payment.paytab.callback'), route('payment.paytab.callback'))  
         ->sendLanguage('en')
         ->create_pay_page();
     return $pay;
