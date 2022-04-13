016d0f1c1d5c16ad8aca3122df6f52e184de3191
Sujata Mehta <sujata@codebrewinnovations.com>
2022-04-13 12:55:24 +0530

    some layout changes

--- resources/views/frontend/home-template-two.blade.php
+++ resources/views/frontend/home-template-two.blade.php
@@ -608,27 +608,15 @@
     </div>
 </div>
 @endsection
+@section('js-script')
+<script type="text/javascript" src="{{asset('front-assets/js/slick.js')}}"></script> 
+<script type="text/javascript" src="{{asset('front-assets/js/jquery.exitintent.js')}}"></script>
+<script type="text/javascript" src="{{asset('front-assets/js/fly-cart.js')}}"></script>
+{{--<script type="text/javascript" src="{{asset('js/aos.js')}}"></script>--}}
+@endsection
 @section('script')
-
-<script src="{{asset('js/aos.js')}}"></script>
-<script>
-  AOS.init();
-</script>
-<script src="{{asset('front-assets/js/jquery.exitintent.js')}}"></script>
-<script src="{{asset('front-assets/js/fly-cart.js')}}"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-
-        $("#doneeee").click(function() {
-            console.log("nejhbfe");
-            // $(".hide_div").hide();
-        });
-    });
-    // $(".mobile-back").on("click", function() {
-    //     $(".sm-horizontal").css("right", "-410px");
-    // });
-    //alert();
-
+    // AOS.init();
     function changeImage(image2, check) {
         var image = $(image2).children("img");
        var  icon = image.attr('data-icon');
