40bb1092d4fc0e3090af64964dbeae28af065c26
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-04-14 17:19:16 +0530

    all vendor api changes

--- app/Http/Controllers/Api/v1/VendorController.php
+++ app/Http/Controllers/Api/v1/VendorController.php
@@ -2018,7 +2018,7 @@ class VendorController extends BaseController{
                 });
             });
         }
-        $vendorData = $vendorData->with('slot', 'slotDate')->where('status', 1)->distinct('id')->paginate($limit, $page);
+        $vendorData = $vendorData->with('slot', 'slotDate')->where('status', 1)->paginate($limit, $page);
 
         foreach ($vendorData as $vendor) {
             unset($vendor->products);
