cffa426ef70f24b3d3400b713bb536b0b4201e14
chander_codebrew <chandar@codebrewinnovations.com>
2022-04-18 12:46:00 +0530

    css

--- resources/views/frontend/vendor-products-with-categories.blade.php
+++ resources/views/frontend/vendor-products-with-categories.blade.php
@@ -508,7 +508,7 @@
                                     </div>
                                     <div class="col-md-8 col-lg-6">
                                             <div class="row my-2 d-flex align-items-center">
-                                                <div class="col-sm-6 vendor-search-bar mb-sm-0 mb-2">
+                                                <div class="col-7 vendor-search-bar mb-sm-0 mb-2">
                                                     <div class="radius-bar w-100">
                                                         <div class="search_form d-flex align-items-center justify-content-between border">
                                                             <button class="btn"><i class="fa fa-search" aria-hidden="true"></i></button>
@@ -519,7 +519,7 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="col-sm-6 text-right">{{ __('Sort By') }} :
+                                                <div class="col-5 text-right pl-0"><span > {{ __('Sort By') }} :</span>
                                                     <select name="order_type" id='order_type' class="product_tag_filter p-1">
                                                         <option value="featured">{{ __('Featured') }}</option>
                                                         <option value="a_to_z">{{ __('A to Z') }}</option>
