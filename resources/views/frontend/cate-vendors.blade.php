4621471a3e7333acb81387ee3ab5283952d5f726
chander_codebrew <chandar@codebrewinnovations.com>
2022-04-19 17:42:54 +0530

    css

--- resources/views/frontend/cate-vendors.blade.php
+++ resources/views/frontend/cate-vendors.blade.php
@@ -176,7 +176,7 @@
                                                         $vendor_url = route('vendorDetail', $data->slug);
                                                     }
                                                     @endphp
-                                                    <div class=" col-md-3 col-6 col-grid-box mt-3 " >
+                                                    <div class=" col-md-3  col-grid-box mt-3 " >
                                                         <div class="product-card-box position-relative">
                                                             <div class="add-to-fav">
                                                                 <input id="fav_pro_one" type="checkbox">
