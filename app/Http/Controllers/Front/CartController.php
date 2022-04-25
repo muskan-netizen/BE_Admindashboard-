5093df56608c586f69697ccb1a6f9c54fd37fc76
dineshk123 <dineshk@codebrewinnovations.com>
2022-04-25 16:38:37 +0530

    merge

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -1822,8 +1822,10 @@ class CartController extends FrontController
                 if(isset($request->schedule_pickup) && !empty($request->schedule_pickup))    # for pickup laundry
                 $request->schedule_pickup = Carbon::parse($request->schedule_pickup, $user->timezone)->setTimezone('UTC')->format('Y-m-d H:i:s');
 
-                if(isset($request->schedule_dropoff) && !empty($request->schedule_dropoff) && $request->schedule_dropoff != 'undefined undefined')  # for pickup laundry
+                if(isset($request->schedule_dropoff) && !empty($request->schedule_dropoff))  # for pickup laundry
                 $request->schedule_dropoff = Carbon::parse($request->schedule_dropoff, $user->timezone)->setTimezone('UTC')->format('Y-m-d H:i:s');
+
+                Log::info($request->toArray());
 
                 if ($user) {
                     $cart_detail = Cart::where('user_id', $user->id)->first();
