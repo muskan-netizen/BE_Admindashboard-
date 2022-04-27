48e3d5cdaeb0062ecbb95a5de2ec01db06f808fc
gourav sharma <gourav.s@codebrewinnovations.com>
2022-04-27 16:14:30 +0530

    changes

--- app/Http/Controllers/Front/ToyyibPayController.php
+++ app/Http/Controllers/Front/ToyyibPayController.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Front\OrderController;
 use App\Http\Controllers\Front\WalletController;
 use App\Http\Controllers\Front\UserSubscriptionController;
 use App\Http\Controllers\Front\PickupDeliveryController;
-use App\Models\{User, UserVendor, Cart, CartAddon, CartCoupon, CartProduct, CartProductPrescription, Payment, PaymentOption, Client, ClientPreference, ClientCurrency, Order, OrderProduct, OrderProductAddon, OrderProductPrescription, VendorOrderStatus, OrderVendor, OrderTax, SubscriptionPlansUser, UserAddress};
+use App\Models\{User, UserVendor, Cart, CartAddon, CartCoupon, CartProduct, CartProductPrescription, Payment, PaymentOption, Client, ClientPreference, ClientCurrency, Order, OrderProduct, OrderProductAddon, OrderProductPrescription, VendorOrderStatus, OrderVendor, OrderTax, SubscriptionPlansUser, UserAddress,Transaction};
 use Toyyibpay;
 use Auth;
 use Illuminate\Support\Facades\Log;
