b3275f395df281a3da8b4735624a2ab9decf7a12
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-04-27 15:33:29 +0530

    paytab payment in app

--- app/Http/Controllers/Api/v1/PaytabController.php
+++ app/Http/Controllers/Api/v1/PaytabController.php
@@ -88,7 +88,7 @@ class PaytabController extends BaseController
             elseif($request->action == 'subscription'){
                 $request->request->add(['payment_option_id' => 27, 'transaction_id' => $transaction_id]);
                 $subscriptionController = new UserSubscriptionController();
-                $subscriptionController->purchaseSubscriptionPlan($request, '', $request->subscription_id);
+                $subscriptionController->purchaseSubscriptionPlan($request, $request->subscription_id);
             }
             return $this->successResponse('', __('Payment completed successfully'), 200);
         }
@@ -99,19 +99,15 @@ class PaytabController extends BaseController
     public function failedPayment(Request $request)
     {
         try{
-            $user = Auth::user();
             if($request->action == 'cart'){
                 $order_number = $request->order_number;
                 $order = Order::where('order_number', $order_number)->first();
                 if($order){
                     $wallet_amount_used = $order->wallet_amount_used;
-                    Log::info($order);
                     if($wallet_amount_used > 0){
                         $transaction = Transaction::where('type', 'deposit')->where('meta', 'LIKE', '%'.$order->order_number.'%')->first();
-                        Log::info($transaction);
                         if(!$transaction){
                             $wallet = $user->wallet;
-                            Log::info($wallet);
                             $wallet->depositFloat($wallet_amount_used, ['Wallet has been <b>refunded</b> for cancellation of order <b>'. $order->order_number. '</b>']);
                         }
                     }
