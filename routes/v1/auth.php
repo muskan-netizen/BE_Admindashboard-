45830eabfdad2e4ec42404237eba6ff2de69b1bc
Sujata Mehta <sujata@codebrewinnovations.com>
2022-04-27 12:45:38 +0530

    passbase api and demo issues

--- routes/v1/auth.php
+++ routes/v1/auth.php
@@ -163,5 +163,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['ApiLocalization']], function (
         Route::group(['prefix' => 'edit-order'], function () {
             Route::post('approve/reject', 'Api\v1\OrderController@submitEditedOrder');
         }); 
+        //Passbase Store 
+        Route::post('passbase/store','Front\PassbaseController@storeAuthkey');
     });
 });
