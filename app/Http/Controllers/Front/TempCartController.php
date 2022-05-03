02527c4ba4939ab70f9584b829bef273333c2358
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-05-03 15:53:33 +0530

    temp cart changes

--- app/Http/Controllers/Front/TempCartController.php
+++ app/Http/Controllers/Front/TempCartController.php
@@ -909,13 +909,12 @@ class TempCartController extends FrontController
         try{
             $order_vendor_id = $request->order_vendor_id;
             $getallproduct = OrderVendor::with(['products.addon'])->where('id', $order_vendor_id)->first();
-            \Log::info($getallproduct->toArray());
             if(!$request->has('user_id')){
                 $request->request->add(['user_id' => $getallproduct->user_id]);
             }
             $langId = ClientLanguage::where(['is_primary' => 1, 'is_active' => 1])->value('language_id');
             $currId = ClientCurrency::where(['is_primary' => 1])->value('currency_id');
-            $cart = TempCart::where('status', '0')->where('user_id', $request->user_id)->where('order_vendor_id', $order_vendor_id)->where('is_submitted', '!=', 1)->where('is_approved', '!=', 1)->first();
+            $cart = TempCart::where('status', '0')->where('user_id', $request->user_id)->where('order_vendor_id', $order_vendor_id)->first();
             if(!$cart){
                 foreach($getallproduct->products as $data){
                     $request->request->add([
@@ -935,7 +934,7 @@ class TempCartController extends FrontController
                     $this->postAddToTempCart($request);
                 }
             }
-            $cart = TempCart::with(['address','currency','coupon.promo'])->where('status', '0')->where('user_id', $request->user_id)->where('order_vendor_id', $order_vendor_id)->orderBy('id', 'desc')->first();
+            $cart = TempCart::with(['address','currency','coupon.promo'])->where('status', '0')->where('user_id', $request->user_id)->where('order_vendor_id', $order_vendor_id)->first();
             $cartData = $this->getCart($cart, $langId, $currId, '');
 
             return $this->successResponse($cartData, 'Order added to cart.', 201);
