eb3447f1146b54a25b9a50f8b5e1c76159c135bc
Sujata Mehta <sujata@codebrewinnovations.com>
2022-05-06 16:08:15 +0530

    template four

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -55,6 +55,9 @@ if(isset($set_template))
       @include('layouts.store/left-sidebar-template-one')
       @endif
     </header>
+    @if(isset($set_template)  && $set_template->template_id == 4)
+    @include('frontend.template_four.layouts.vendor_type')
+    @endif
     @yield('content')
     @if(isset($set_template)  && $set_template->template_id == 1)
     @include('layouts.store/footer-content-template-one')
