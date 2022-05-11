a5313ad876d652756667cd33a211c4308d1a4b55
Sujata Mehta <sujata@codebrewinnovations.com>
2022-05-11 12:10:00 +0530

    braintree integrstion

--- public/js/custom.js
+++ public/js/custom.js
@@ -541,6 +541,8 @@ $(document).ready(function () {
                 paymentViaToyyibPay('');
             } else if (payment_option_id == 25) {
                 payWithEasebuss('');
+            }else if (payment_option_id == 33) {
+                paymentViaBraintree('');
             }
         } else {
             _this.attr("disabled", false);
@@ -1350,8 +1352,6 @@ $(document).ready(function () {
     }
 
     window.placeOrderBeforePayment = function placeOrderBeforePayment(address_id = 0, payment_option_id, tip = 0) {
-        alert("here");
-        return false;
         var task_type = $("input[name='task_type']").val();
         var schedule_dt = $("#schedule_datetime").val();
         var slot = $("#slot").val();
@@ -3885,6 +3885,15 @@ $(document).ready(function () {
                     return false;
                 }
             break;
+
+            case '33':
+                var order = placeOrderBeforePayment(address_id, payment_option_id, tip);
+                    if (order != '') {
+                        paymentViaBraintree(address_id, order);
+                    } else {
+                        return false;
+                    }
+            break;
         
         }
 
@@ -4028,7 +4037,7 @@ $(document).ready(function () {
                 payWithFlutterWave('', payment_option_id, '');
         break;
 
-        case 32:
+        case 33:
                 paymentViaBraintree('', payment_option_id, ''); 
         break;
     }
