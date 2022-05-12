b72bc9991fbd85c96bded2c006a72b861983a7ee
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-05-12 12:28:16 +0530

    stripe error fixed and payment function undefined error fixed

--- app/Http/Controllers/Front/StripeGatewayController.php
+++ app/Http/Controllers/Front/StripeGatewayController.php
@@ -53,9 +53,9 @@ class StripeGatewayController extends FrontController
            
             $payment_form = $request->payment_form;
            
-            $saved_payment_method = $this->getSavedUserPaymentMethod($request);
+            // $saved_payment_method = $this->getSavedUserPaymentMethod($request);
            
-            if (!$saved_payment_method) {
+            // if (!$saved_payment_method) {
                 $customerResponse = $this->gateway->createCustomer(array(
                     'description' => 'Creating Customer',
                     'name' => $user->name,
@@ -73,14 +73,14 @@ class StripeGatewayController extends FrontController
                     $request->request->set('customerReference', $customer_id);
                     $save_payment_method_response = $this->saveUserPaymentMethod($request);
                 }
-            }else {
-                $customer_id = $saved_payment_method->customerReference;
-                // \Stripe\Stripe::setApiKey($this->API_KEY);
-                // $retrieve_customer = \Stripe\Customer::retrieve(
-                //     $customer_id, 
-                //     []
-                // );
-            }
+            // }else {
+            //     $customer_id = $saved_payment_method->customerReference;
+            //     // \Stripe\Stripe::setApiKey($this->API_KEY);
+            //     // $retrieve_customer = \Stripe\Customer::retrieve(
+            //     //     $customer_id, 
+            //     //     []
+            //     // );
+            // }
             
 
             $postdata = [
@@ -234,8 +234,8 @@ class StripeGatewayController extends FrontController
             $address = UserAddress::where('user_id', $user->id);
             $token = $request->stripe_token;
             $plan = SubscriptionPlansUser::where('slug', $request->subscription_id)->firstOrFail();
-            $saved_payment_method = $this->getSavedUserPaymentMethod($request);
-            if (!$saved_payment_method) {
+            // $saved_payment_method = $this->getSavedUserPaymentMethod($request);
+            // if (!$saved_payment_method) {
                 $customerResponse = $this->gateway->createCustomer(array(
                     'description' => 'Creating Customer for subscription',
                     'email' => $request->email,
@@ -247,9 +247,9 @@ class StripeGatewayController extends FrontController
                     $request->request->set('customerReference', $customer_id);
                     $save_payment_method_response = $this->saveUserPaymentMethod($request);
                 }
-            } else {
-                $customer_id = $saved_payment_method->customerReference;
-            }
+            // } else {
+            //     $customer_id = $saved_payment_method->customerReference;
+            // }
 
             // $subscriptionResponse = $this->gateway->createSubscription(array(
             //     "customerReference" => $customer_id,
@@ -335,9 +335,9 @@ class StripeGatewayController extends FrontController
 
             $user = Auth::user();
 
-            $saved_payment_method = $this->getSavedUserPaymentMethod($request);
+            // $saved_payment_method = $this->getSavedUserPaymentMethod($request);
            
-            if (!$saved_payment_method) {
+            // if (!$saved_payment_method) {
                 $customerResponse = $stripe->customers->create([
                     'name' => $user->name,
                     'email' => $user->email,
@@ -351,17 +351,17 @@ class StripeGatewayController extends FrontController
                 // Find the card ID
                 $customer_id = $customerResponse->id;
                 if ($customer_id) {
-                    $request->request->set('customerReference', $customer_id);
+                    $request->request->add(['customerReference' => $customer_id, 'payment_option_id' => 19]);
                     $save_payment_method_response = $this->saveUserPaymentMethod($request);
                 }
-            }else {
-                $customer_id = $saved_payment_method->customerReference;
-                // \Stripe\Stripe::setApiKey($this->API_KEY);
-                // $retrieve_customer = \Stripe\Customer::retrieve(
-                //     $customer_id, 
-                //     []
-                // );
-            }
+            // }else {
+            //     $customer_id = $saved_payment_method->customerReference;
+            //     // \Stripe\Stripe::setApiKey($this->API_KEY);
+            //     // $retrieve_customer = \Stripe\Customer::retrieve(
+            //     //     $customer_id, 
+            //     //     []
+            //     // );
+            // }
 
             $description = '';
             $payment_form = $request->payment_form;
