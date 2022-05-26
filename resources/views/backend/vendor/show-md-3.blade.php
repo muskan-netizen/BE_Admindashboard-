b170eaf0e98cdc7cda3dc2ea2dad8d40570539a0
dineshk123 <dineshk@codebrewinnovations.com>
2022-05-26 18:42:56 +0530

    vendor issue

--- resources/views/backend/vendor/show-md-3.blade.php
+++ resources/views/backend/vendor/show-md-3.blade.php
@@ -459,28 +459,27 @@
 
     <div class="inbox-widget mt-3" data-simplebar style="max-height: 350px;">
         @foreach($vendor->permissionToUser as $users)
-        <div class="inbox-item pb-0">
-            <div class="inbox-item-img">
-                <img src="{{$users->user ? $users->user->image['proxy_url'].'40/40'.$users->user->image['image_path'] : asset('assets/images/users/user-2.jpg')}}" class="rounded-circle" alt="">
+            @if($users->user)
+                <div class="inbox-item pb-0">
+                    <div class="inbox-item-img">
+                        <img src="{{$users->user ? $users->user->image['proxy_url'].'40/40'.$users->user->image['image_path'] : asset('assets/images/users/user-2.jpg')}}" class="rounded-circle" alt="">
+                    </div>
+                    <p class="inbox-item-author">{{ $users->user->name??'' }}  </p>
+                    <p class="inbox-item-text"><label class="d-block"><i class="fa fa-envelope mr-1" aria-hidden="true"></i> {{ $users->user->email??'' }}  
+                        @if($users->user)
+                        </label><label class="d-block"><i class="fa fa-phone mr-1" aria-hidden="true"></i> {{ $users->user->phone_number??'' }}</label> </p> 
+                        @endif
+                    </p>
+                    @if($users->user && $users->user->id != Auth::id())
+                    <form class="delete-user position-absolute" method="POST" action="{{route('user.vendor.permission.destroy', $users->id)}}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-primary-outline" onclick="return confirm('Are you sure ?');"> <i class="mdi mdi-delete"></i></button>
 
-                {{-- <img src="{{asset('assets/images/users/user-2.jpg')}}" class="rounded-circle" alt=""> --}}
-            </div>
-            <p class="inbox-item-author">{{ $users->user->name??'' }}  </p>
-            <p class="inbox-item-text"><label class="d-block"><i class="fa fa-envelope mr-1" aria-hidden="true"></i> {{ $users->user->email??'' }}  
-                @if($users->user)
-                  </label><label class="d-block"><i class="fa fa-phone mr-1" aria-hidden="true"></i> {{ $users->user->phone_number??'' }}</label> </p> 
-                @endif
-            </p>
-            @if($users->user->id != Auth::id())
-            <form class="delete-user position-absolute" method="POST" action="{{route('user.vendor.permission.destroy', $users->id)}}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-primary-outline" onclick="return confirm('Are you sure ?');"> <i class="mdi mdi-delete"></i></button>
-
-                    </form>
-            @endif
-        </div>
-
+                            </form>
+                    @endif
+                </div>
+            @endif    
         @endforeach
     </div>
 </div>
