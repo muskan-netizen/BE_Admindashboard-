646bb4d6cc945af085759cd3b007e88579d79ff4
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-05-27 12:50:42 +0530

    done

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -1039,9 +1039,10 @@ class OrderController extends BaseController
              $vendor_details = Vendor::where('id', $vendor)->select('id', 'name', 'latitude', 'phone_no', 'email', 'longitude', 'address')->first();
              $tasks = array();
              $meta_data = '';
- 
+             $rtype = 'P';
              $unique = Auth::user()->code;
-             if ($colm == 1) {     # 1 for pickup from customer drop to vendor
+             if ($colm == 1) { 
+                $rtype = 'P';    # 1 for pickup from customer drop to vendor
                  $desc = $order->comment_for_pickup_driver ?? null;
                  $tasks[] = array(
                      'task_type_id' => 1,
@@ -1079,6 +1080,7 @@ class OrderController extends BaseController
  
  
              if ($colm == 2) { # 1 for pickup from vendor drop to customer
+                 $rtype = 'D';
                  $desc = $order->comment_for_dropoff_driver ?? null;
                  $tasks[] = array(
                      'task_type_id' => 1,
@@ -1138,7 +1140,8 @@ class OrderController extends BaseController
                  'barcode' => '',
                  'order_team_tag' => $team_tag,
                  'call_back_url' => $call_back_url ?? null,
-                 'task' => $tasks
+                 'task' => $tasks,
+                 'request_type'=> $rtype
              ];
  
  
