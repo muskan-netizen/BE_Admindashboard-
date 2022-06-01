faceb86ee3031ae50974f898c74030c4ab754b6b
dineshk123 <dineshk@codebrewinnovations.com>
2022-06-01 13:06:39 +0530

    merge cart issue

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -798,7 +798,10 @@ class CartController extends BaseController
                                 foreach ($prod->product->taxCategory->taxRate as $tckey => $tax_value) {
                                     $rate = round($tax_value->tax_rate);
                                     $tax_amount = ($price_in_doller_compare * $rate) / 100;
-                                    $product_tax = ($quantity_price+$total_addon_price) * $rate / 100;
+                                    //    $product_tax = ($quantity_price+$total_addon_price) * $rate / 100;
+
+                                    $product_tax = ($quantity_price+$addon_price) * $rate / 100;
+                                    
                                     $taxData[$tckey]['rate'] = $rate;
                                     $taxData[$tckey]['tax_amount'] = $tax_amount;
                                     $taxData[$tckey]['product_tax'] = $product_tax;
