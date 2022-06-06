eb76e2ca6c265eb879ffce385d870a29857cf835
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-06-06 15:21:54 +0530

    bugs fixed

--- app/Http/Controllers/Api/v1/MyCashGatewayController.php
+++ app/Http/Controllers/Api/v1/MyCashGatewayController.php
@@ -153,8 +153,7 @@ class MyCashGatewayController extends BaseController
                             'request_id' => $request->request_id,
                             'order_reference' => $data['order_id'],
                             'payment_form' => $request->payment_form,
-                            'come_from' => 'app',
-                            '_token' => csrf_token()
+                            'come_from' => 'app'
                         );
                         if($payment_form == 'subscription'){
                             $rdata['formData']['subscription_id'] = $request->subscription_id;
