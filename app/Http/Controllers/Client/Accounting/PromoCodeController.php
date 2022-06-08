ffa87f584a3f7f51e004ca917585b420aa892cb5
Dinesh Kumar <dineshk@codebrewinnovations.com>
2022-06-08 15:10:14 +0530

    merge dishef issues

--- app/Http/Controllers/Client/Accounting/PromoCodeController.php
+++ app/Http/Controllers/Client/Accounting/PromoCodeController.php
@@ -111,9 +111,9 @@ class PromoCodeController extends Controller{
                 })
                 ->addColumn('view_url', function($vendor_orders) {
                     if(!empty($vendor_orders->order_id) && !empty($vendor_orders->vendor_id)){
-                        $vendor_orders->view_url = route('order.show.detail', [$vendor_orders->order_id, $vendor_orders->vendor_id]);
+                        return route('order.show.detail', [$vendor_orders->order_id, $vendor_orders->vendor_id]);
                     }else{
-                        return '';
+                        return '#';
                     }
                 })
                 ->addColumn('vendor_paid_promo', function($vendor_orders){
