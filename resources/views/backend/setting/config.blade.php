06f6f9c115d026eda051a4f3263aef5d930604e7
harbans <harbans.singh@codebrewinnovations.com>
2022-06-13 18:50:29 +0530

    update config

--- resources/views/backend/setting/config.blade.php
+++ resources/views/backend/setting/config.blade.php
@@ -1092,7 +1092,7 @@ $sms_crendential = json_decode($preference->sms_credentials);
                   <div class="col-12">
                      <div class="form-group mb-0 switchery-demo">
                         <label for="sos" class="d-flex align-items-center justify-content-between">
-                           <h5 class="social_head"><i class="fab fa-twitter"></i> <span>SOS</span></h5>
+                           <h5 class="social_head"> <span>SOS</span></h5>
                            <button class="btn btn-info btn-block save_btn" type="submit"> {{ __("Save") }} </button>
                         </label>
                         <label for="" class="mr-3">{{ __("Enable") }}</label>
