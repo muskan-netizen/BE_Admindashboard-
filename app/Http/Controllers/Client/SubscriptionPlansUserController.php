e4758988d33d377b5487e688a67a2bba50eead9c
mohit_vashisth <mohitvashisht@codebrewinnovations.com>
2022-06-21 17:26:40 +0530

    subscriptin plan popup setting issue resolved

--- app/Http/Controllers/Client/SubscriptionPlansUserController.php
+++ app/Http/Controllers/Client/SubscriptionPlansUserController.php
@@ -170,10 +170,6 @@ class SubscriptionPlansUserController extends BaseController
 
     public function showSubscriptionPlanCustomer(Request $request){
         
-        // $showSubscriptionPlan = ShowSubscriptionPlanOnSignup::find(1);
-        // if(empty($showSubscriptionPlan)){
-        //     $showSubscriptionPlan =  new ShowSubscriptionPlanOnSignup();
-        // }
         $showSubscriptionPlan = ShowSubscriptionPlanOnSignup::firstOrNew(array('id' => 1));
         $showSubscriptionPlan->id = 1;
         if($request->showSubscriptionType == 'show_plan_customer'){
