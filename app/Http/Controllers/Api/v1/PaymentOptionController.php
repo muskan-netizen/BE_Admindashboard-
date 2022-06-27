f3df47e16ab32fbbfe51f5de8ddbca67a0aa31d6
Dinesh Kumar <dineshk@codebrewinnovations.com>
2022-06-27 16:40:03 +0530

    merge payment

--- app/Http/Controllers/Api/v1/PaymentOptionController.php
+++ app/Http/Controllers/Api/v1/PaymentOptionController.php
@@ -224,8 +224,8 @@ class PaymentOptionController extends BaseController{
     public function postPaymentVia_mvodafone(Request $request){
         $gateway = new MvodafoneController();
         return $gateway->createPayLinkApp($request);
-        }
-        
+    }
+
     public function postPaymentVia_paypal(Request $request){
         try{
             $paypal_creds = PaymentOption::select('credentials')->where('code', 'paypal')->where('status', 1)->first();
