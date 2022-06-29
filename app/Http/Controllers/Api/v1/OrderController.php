1253c71143bdc3af339d902fc4d66956472fb5e5
mohit_vashisth <mohitvashisht@codebrewinnovations.com>
2022-06-29 11:48:35 +0530

    Your order of amount message issue resolved

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -1413,7 +1413,7 @@ class OrderController extends BaseController
                     $to = '+' . $user->dial_code . $user->phone_number;
                 }
                 $provider = $prefer->sms_provider;
-                $body = "Hi " . $user->name . ", Your order of amount " . $currSymbol . decimal_format($order->payable_amount) . " for order number " . $order->order_number . " has been placed successfully.";
+                $body = __("Hi ") . $user->name . __(", Your order of amount ") . $currSymbol . decimal_format($order->payable_amount) . __(" for order number ") . $order->order_number . __(" has been placed successfully.");
                 if (!empty($prefer->sms_provider)) {
                     $send = $this->sendSms($provider, $prefer->sms_key, $prefer->sms_secret, $prefer->sms_from, $to, $body);
                 }
