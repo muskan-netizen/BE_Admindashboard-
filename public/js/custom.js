cc6896d240bd7862fcfee7ef48b910a57dcfe195
harbans <harbans.singh@codebrewinnovations.com>
2022-06-29 12:01:14 +0530

    update opent pay

--- public/js/custom.js
+++ public/js/custom.js
@@ -4731,6 +4731,9 @@ $(document).ready(function () {
             case 40:
                 paymentViaUseRede('', payment_option_id, ''); 
             break;
+            case 41:
+                payWithOpenPay('', payment_option_id, ''); 
+            break;
 
         }
     }
