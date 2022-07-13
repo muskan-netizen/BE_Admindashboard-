4ad64c61e8eb93d17ec2e29f5bffbb1126a21aff
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-07-13 15:07:50 +0530

    custom mods not display bug fixed and add new customer status bug fixed

--- app/Http/Controllers/Client/UserController.php
+++ app/Http/Controllers/Client/UserController.php
@@ -268,6 +268,7 @@ class UserController extends BaseController
         $user->phone_number = $phone;
         $user->is_email_verified = ($request->has('is_email_verified') && $request->is_email_verified == 'on') ? 1 : 0;
         $user->is_phone_verified = ($request->has('is_phone_verified') && $request->is_phone_verified == 'on') ? 1 : 0;
+        $user->status = 1;
         if ($request->hasFile('image')) {    /* upload logo file */
             $file = $request->file('image');
             $user->image = Storage::disk('s3')->put('/profile', $file, 'public');
