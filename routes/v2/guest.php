5c564ddfd0924ce2b6f07a208ab1f5c678156bd8
root <root@ip-172-31-28-173.us-west-2.compute.internal>
2022-07-20 07:07:53 +0000

    changes

--- routes/v2/guest.php
+++ routes/v2/guest.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group(['prefix' => 'v1/v2', 'middleware' => ['ApiLocalization']], function () {
-    Route::group(['middleware' => ['dbCheck', 'checkAuth', 'apilogger']], function() {
+    Route::group(['middleware' => ['dbCheck', 'checkAuth']], function() {
 
         Route::get('category/{id?}', 'Api\v1\v2\CategoryController@categoryData');
         Route::post('category/filters/{id?}', 'Api\v1\v2\CategoryController@categoryFilters');
