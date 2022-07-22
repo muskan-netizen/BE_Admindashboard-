eddc862367279aa17d133e9b11064c45aec9279d
surendradbit <surendrasingh@codebrewinnovations.com>
2022-07-22 10:23:33 +0530

    app delivery fee issue solved

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -838,7 +838,6 @@ class CartController extends BaseController
 
                             $deliveries = $this->getDeliveryOptions($vendorData,$preferences,$payable_amount,$address);
                             $deliveryDuration = 0;
-                            $deliveryDistance = 0;
                             if(isset($deliveries[0]))
                             {
                                 
@@ -849,24 +848,20 @@ class CartController extends BaseController
                                      foreach($new as $rate){
                                          $deliveryCharges = $rate['rate'];
                                          $deliveryDuration = $rate['duration'];
-                                         $deliveryDistance = $rate['distance'];
                                      }
                                      if($deliveryCharges)
                                      {
                                          $deliveryCharges = $rate['rate'];
                                          $deliveryDuration = $rate['duration'];
-                                         $deliveryDistance = $rate['distance'];
                                      }else{
                                          $deliveryCharges = $deliveries[0]['rate'];
                                          $deliveryDuration = $deliveries[0]['duration'];
-                                         $deliveryDistance = $deliveries[0]['distance'];
                                          $code = $deliveries[0]['code'];
                                      }
  
                                  }else{
                                      $deliveryCharges = $deliveries[0]['rate'];
                                      $deliveryDuration = $deliveries[0]['duration'];
-                                     $deliveryDistance = $deliveries[0]['distance'];
                                      $code = $deliveries[0]['code'];
                                  }
 
@@ -878,7 +873,7 @@ class CartController extends BaseController
  
                         if(isset($deliveryCharges) && !empty($deliveryCharges)){
                                 $dtype = explode('_',$code);
-                                CartDeliveryFee::updateOrCreate(['cart_id' => $cart->id, 'vendor_id' => $vendorData->vendor->id],['delivery_fee' => $deliveryCharges, 'delivery_duration' => $deliveryDuration, 'delivery_distance' => $deliveryDistance,'shipping_delivery_type' => $dtype[0]??'D','courier_id'=>$dtype[1]??'0']);
+                                CartDeliveryFee::updateOrCreate(['cart_id' => $cart->id, 'vendor_id' => $vendorData->vendor->id],['delivery_fee' => $deliveryCharges, 'delivery_duration' => $deliveryDuration,'shipping_delivery_type' => $dtype[0]??'D','courier_id'=>$dtype[1]??'0']);
                         }
                         
                      
@@ -1597,7 +1592,6 @@ class CartController extends BaseController
             if (!empty($deliver_response_array[0])){
                 $deliver_charge = (!empty($deliver_response_array[0]['delivery_fee']))?number_format($deliver_response_array[0]['delivery_fee'], 2, '.', ''):'0.00';
                 $delivery_duration = (!empty($deliver_response_array[0]['total_duration']))?number_format($deliver_response_array[0]['total_duration'], 0, '.', ''):'0.00';
-                $delivery_distance = (!empty($deliver_response_array[0]['total_distance']))?number_format($deliver_response_array[0]['total_distance'], 0, '.', ''):'0.00';
                 $option[] = array(
                     'type'=>'D',
                     'courier_name'=>__('Dispatcher'),
@@ -1605,7 +1599,6 @@ class CartController extends BaseController
                     'courier_company_id' => 0,
                     'etd' => 0,
                     'duration' => $delivery_duration,
-                    'distance' => $delivery_distance,
                     'etd_hours' => 0,
                     'estimated_delivery_days' => 0,
                     'code' => 'D_0'
