8c8f0615bf2eff2ced19d5be31128088088ca394
chander_codebrew <chandar@codebrewinnovations.com>
2022-07-26 14:37:08 +0530

    home page product image

--- app/Http/Controllers/Front/UserhomeController.php
+++ app/Http/Controllers/Front/UserhomeController.php
@@ -968,11 +968,11 @@ class UserhomeController extends FrontController
 
         }
         $set_template = WebStylingOption::where('web_styling_id', 1)->where('is_selected', 1)->first();
-        $p_dim = '260/100';
+        $p_dim = '300/300';
         if (isset($set_template)  && $set_template->template_id == 3){
-            $p_dim = '300/350';
+            $p_dim = '300/300';
         }elseif(isset($set_template)  && $set_template->template_id == 2){
-            $p_dim = '260/180';
+            $p_dim = '300/300';
         }
         elseif(isset($set_template)  && $set_template->template_id == 6){
             $p_dim = '300/300';
