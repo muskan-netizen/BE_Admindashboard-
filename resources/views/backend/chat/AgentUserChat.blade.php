1a7f7d876e9c9f36db2315490c515bf1a156e928
amitmehraFullstack <amit.mehra@codebrewinnovations.com>
2022-08-01 16:06:40 +0530

    code clean

--- resources/views/backend/chat/AgentUserChat.blade.php
+++ resources/views/backend/chat/AgentUserChat.blade.php
@@ -26,7 +26,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box">
-                    <h4 class="page-title">{{ getNomenclatureName('Agent/User Chat', true) }}</h4>
+                    <h4 class="page-title">{{ getNomenclatureName('Driver/User Chat', true) }}</h4>
                 </div>
             </div>
         </div>
@@ -61,19 +61,38 @@
     </script>
       @php
       $authData = json_encode(@$data->toArray());
+      $user_type = 'admin';
+      $to_message = 'to_user_agent';
+      $from_message = 'from_admin';
+      $chat_type = 'agent_to_user';
+      $startChatype = 'agent_to_user';
+      $apiPre = 'client';
+      $rePre = 'client/chat/user';
+      $fetchDe = 'fetchAllRoom';
   @endphp
 @endsection
-@section('script-bottom')
 
+@section('script-bottom')
+<script>
+    var to_message = `<?php echo $to_message; ?>`;
+    var user_type = `<?php echo $user_type; ?>`;
+    var from_message = `<?php echo $from_message; ?>`;
+    var chat_type = `<?php echo $chat_type; ?>`;
+    var startChatype = `<?php echo $startChatype; ?>`;
+    var apiPre = `<?php echo $apiPre; ?>`;
+    var rePre = `<?php echo $rePre; ?>`;
+    var fetchDe = `<?php echo $fetchDe; ?>`;
+
+</script>
 <script src="https://momentjs.com/downloads/moment.min.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="{{asset('assets/js/chat/admin_user_agent_chat.js')}}"></script>
+{{-- <script src="{{asset('assets/js/chat/admin_user_agent_chat.js')}}"></script> --}}
+<script src="{{asset('assets/js/chat/commonChat.js')}}"></script>
 <script src="{{asset('assets/js/chat/socket_chat.js')}}"></script>
 {{-- <script src="{{asset('assets/js/chat/chatNotifications.js')}}"></script> --}}
 <script>
     var client_data = `<?php echo $authData; ?>`;
     fetchChatGroups(client_data);
-
 </script>
 
 <script>
