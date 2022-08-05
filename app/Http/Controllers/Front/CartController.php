ce489c9160e87c29619688cded42ae3e043ebc12
mohit_vashisth <mohitvashisht@codebrewinnovations.com>
2022-08-05 17:08:12 +0530

    product order form nomen clature done

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -1952,7 +1952,9 @@ class CartController extends FrontController
         //     $expected_vendor_html = view('frontend.modals.expected_vendor_pricing')->with(['expected_vendors'=>$expected_vendors,'clientCurrency' => $clientCurrency])->render();
         // }
         if($cart_details){
-            $mycartView = view('frontend.cart-page')->with(['cart_details' => (($cart_details)?json_decode($cart_details):[])])->render();
+            $nomenclatures_translation_id=Nomenclature::where('label','Product Order Form')->first()->id;
+            $nomenclatureProductOrderForm = NomenclatureTranslation::where(['nomenclature_id'=>$nomenclatures_translation_id,'language_id'=>$langId])->exists() ? NomenclatureTranslation::where(['nomenclature_id'=>$nomenclatures_translation_id,'language_id'=>$langId])->first()->name : "Product Order Form";
+            $mycartView = view('frontend.cart-page')->with(['cart_details' => (($cart_details)?json_decode($cart_details):[]), 'nomenclatureProductOrderForm'=>$nomenclatureProductOrderForm])->render();
         }
         return response()->json(['status' => 'success', 'schedule_datetime' => $request->schedule_date_delivery, 'cart_details' => $cart_details, 'expected_vendor_html' => $expected_vendor_html,'expected_vendors' => $expected_vendors, 'client_preference_detail' => $client_preference_detail,'mycart'=>$mycartView??'']);
     }
