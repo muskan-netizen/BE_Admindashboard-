3ee6aff24481327b5dd2086ab6c5a70b5a0bb863
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-08-08 15:14:40 +0530

    conflicts resolved

--- public/js/location.js
+++ public/js/location.js
@@ -516,34 +516,6 @@ $(document).ready( async function () {
                     var path = window.location.pathname;
                     if (path == '/') {
 
-<<<<<<< HEAD
-                        if(index ==  0){
-                            console.log('hereeee');
-                            $(".al_desktop_banner").html('');
-                            console.log(response.data.banners);
-                            console.log("=>>>>>>>>>  response.data.banners");
-                            if(response.data.banners.length > 0) {
-                                let desktop_banners_template = await _.template($('#desktop_banners_template').html());
-                                $(".al_desktop_banner").append(desktop_banners_template({ banners: response.data.banners }));
-
-                                $(".al_mobile_banner").html('');
-                                let mobile_banners_template = await _.template($('#mobile_banners_template').html());
-                                $(".al_mobile_banner").append(mobile_banners_template({ banners: response.data.mobile_banners }));
-
-                                 //$('.carousel').carousel();
-                                // setTimeout(()=>{
-                                //     $('.banner_main_shimer').hide();
-                                //     $('.category_main_shimer').hide();
-                                // },1000);
-                            } else {
-                                // $('.banner_main_shimer').hide();
-                                // $('.category_main_shimer').hide();
-                            }
-
-                            //alert('ddd');
-                            //$('.banner_main_shimer').hide();
-                        }
-=======
                         // if(index ==  0){
                         //     $(".al_desktop_banner").html('');
                         //     let desktop_banners_template = _.template($('#desktop_banners_template').html());
@@ -555,7 +527,6 @@ $(document).ready( async function () {
 
                         //     $('.carousel').carousel();
                         // }
->>>>>>> vendorTypeHarbans
 
                         let products_template = _.template($('#products_template').html());
                         console.log("->>>>>>>>>>>>>>>>>>>>>");
