40ed4336a3b094a10e9994209fe9ddd91fd9b6e5
harbans <harbans.singh@codebrewinnovations.com>
2022-08-09 11:54:23 +0530

    udpate comflict

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -1919,7 +1919,6 @@ class OrderController extends BaseController
             // })->get();
 
            // $order['user_document_value'] =  $user_docs;
-<<<<<<< HEAD
             if(auth()->user()->is_admin){
                 $order['total_amount'] = $order->total_amount  - $total_markup_Price;
                 $order['payable_amount'] = $order->payable_amount  - $total_markup_Price;
@@ -1927,8 +1926,6 @@ class OrderController extends BaseController
                 $order['total_amount'] = $order->total_amount;
                 $order['payable_amount'] = $order->payable_amount;
             }
-=======
->>>>>>> pre_stage
            /* Check if other taxes available like: Tax on service fee, container charges, delivery fee and fixed fee .etc */
            $total_other_taxes = 0;
            if($order->total_other_taxes!=''){
