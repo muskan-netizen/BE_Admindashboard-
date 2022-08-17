f2df6e647f7b6e526970dd0fa19a579fe65ef4e7
preet_inder <preetinder.pal@codebrewinnovations.com>
2022-08-17 12:06:50 +0530

    nomenclature issue fixed

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -268,8 +268,8 @@ class ProductController extends BaseController
         $nomenclatureProductOrderForm = "Product Order Form";
         if(!empty($nomenclature)){
             $nomenclatureTranslation = NomenclatureTranslation::where(['nomenclature_id'=>$nomenclature->id,'language_id'=>$langId])->first();
-            if(!empty($nomenclatureTranslation->name)){
-                $nomenclatureProductOrderForm = $nomenclatureTranslation->name;
+            if($nomenclatureTranslation){
+                $nomenclatureProductOrderForm = $nomenclatureTranslation->name ?? null;
             }
         }
 
--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -144,8 +144,8 @@ class CartController extends FrontController
         $nomenclatureProductOrderForm = "Product Order Form";
         if(!empty($nomenclature)){
             $nomenclatureTranslation = NomenclatureTranslation::where(['nomenclature_id'=>$nomenclature->id,'language_id'=>$langId])->first();
-            if(!empty($nomenclatureTranslation->name)){
-                $nomenclatureProductOrderForm = $nomenclatureTranslation->name;
+            if($nomenclatureTranslation){
+                $nomenclatureProductOrderForm = $nomenclatureTranslation->name ?? null;
             }
         }
 
