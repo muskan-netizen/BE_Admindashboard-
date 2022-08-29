49b7f74003605864c99d7f7093a15f00bd979808
harbans <harbans.singh@codebrewinnovations.com>
2022-08-29 11:55:07 +0530

    update servireArea vendor

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -302,9 +302,6 @@ class FrontController extends Controller
                 $vendors[] = $value->id;
             }
         }
-
-        Session::put('vendors', $vendors);
-
         return $vendors;
     }
 
