ff786fdce72e2d9cd991fe6b5850ed02e0a0f92d
harbans <harbans.singh@codebrewinnovations.com>
2022-09-23 12:40:05 +0530

    update logs

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -34,8 +34,8 @@ class FrontController extends Controller
                 if(!empty($client_preference->sms_secret) && !empty($client_preference->sms_from)){
                     $client = new TwilioClient($client_preference->sms_key, $client_preference->sms_secret);
                     $send =  $client->messages->create($to, ['from' => $client_preference->sms_from, 'body' => $body]);
-                    Log::info('SMS twilio respons');
-                    Log::info($send);
+                    // Log::info('SMS twilio respons');
+                    // Log::info($send);
                 }else{
                     return 2;
                 }
@@ -64,8 +64,8 @@ class FrontController extends Controller
                 if(!empty($sms_secret) && !empty($sms_from)){
                     $client = new TwilioClient($sms_key, $sms_secret);
                     $send =  $client->messages->create($to, ['from' => $sms_from, 'body' => $body]);
-                    Log::info('SMS twilio respons');
-                    Log::info($send);
+                    // Log::info('SMS twilio respons');
+                    // Log::info($send);
                 }else{
                     return 2;
                 }
@@ -73,8 +73,8 @@ class FrontController extends Controller
             //return $send;
         }
         catch(\Exception $e){
-            Log::info('SMS logs');
-            Log::info($e->getMessage());
+            // Log::info('SMS logs');
+            // Log::info($e->getMessage());
             return '2';
         }
         return '1';
