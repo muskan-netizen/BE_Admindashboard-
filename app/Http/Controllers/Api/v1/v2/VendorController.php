7cd660449985f4dc6fd161ecbc2666a87efdc6c8
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-09-27 16:10:48 +0530

    working

--- app/Http/Controllers/Api/v1/v2/VendorController.php
+++ app/Http/Controllers/Api/v1/v2/VendorController.php
@@ -2198,9 +2198,12 @@ class VendorController extends BaseController{
                     }]);
                     
                     if(isset($request->category_id))
-                    $vendor_categories = $vendor_categories->where('id',$request->category_id);
-
-                    // return $vendor_categories->get();
+                    {
+                        $vendor_categories = $vendor_categories->where(function($q) use ($request)
+                        {
+                            $q->where('id',$request->category_id)->orWhere('parent_id',$request->category_id);
+                        });
+                     }
                  
                     
                     $vendor_categories = $vendor_categories->get()->map(function ($query) {
