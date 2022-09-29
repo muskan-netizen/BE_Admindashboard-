35b73d44231182e78964fd182f092253fbe44b09
arun_lalotra <arunlalotra@codebrewinnovations.com>
2022-09-29 16:46:16 +0530

    css

--- resources/views/backend/setting/config.blade.php
+++ resources/views/backend/setting/config.blade.php
@@ -1189,7 +1189,7 @@ $sms_crendential = json_decode($preference->sms_credentials);
                   <div class="col-12">
                      <div class="form-group mb-0 switchery-demo">
                         <label for="fb_login" class="d-flex align-items-center justify-content-between">
-                           <h5 class="social_head"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg> <span>{{ __("Hubspot") }}</span></h5>
+                           <h5 class="social_head"><i class="mdi mdi-hubspot"></i>  <span>{{ __("Hubspot") }}</span></h5>
 
                            <button class="btn btn-info btn-block save_btn" name="hubspot_submit" type="submit"> {{ __("Save") }} </button>
                         </label>
