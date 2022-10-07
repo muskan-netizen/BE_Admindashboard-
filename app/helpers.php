357d01ec72f92f37f87fdcacc1d5cddd8c04439f
sandeep kumar <sandeep.kumar@codebrewinnovations.com>
2022-10-07 11:10:42 +0530

    additional fields

--- app/helpers.php
+++ app/helpers.php
@@ -44,17 +44,16 @@ if (!function_exists('getAdditionalPreference')) {
     function getAdditionalPreference($key=array()){
         $user = ClientData::first();
         $return = [];
+        $dbreturn= [];
         if(sizeof($key)){
-            $result = (checkColumnExists('client_preference_additional','key_name')) ? ClientPreferenceAdditional::whereIn('key_name',$key)->where(['client_code' => $user->code])->get() : [];
-            foreach ($key as $i => $variable) {
-                    $value = '';
-                    if(sizeof($result)){
-                        if(@$result[$i]->key_name == $variable){
-                            $value = $result[$i]->key_value;
-                        }
-                    }
-                    $return[$variable] = $value;
-            } 
+            $result = (checkColumnExists('client_preference_additional','key_name')) ? ClientPreferenceAdditional::select('key_name','key_value')->whereIn('key_name',$key)->where(['client_code' => $user->code])->get() : [];
+            $return = array_column($result->toArray(), 'key_value', 'key_name');
+                if(sizeof($result)){
+                $dbreturn = array_column($result->toArray(), 'key_value', 'key_name');
+                }   
+            $emp = array_diff($key,array_keys($dbreturn));
+            $emptyArr = array_fill_keys($emp, '');
+            $return = array_merge($emptyArr, $dbreturn);
         } 
         return $return;
     }
