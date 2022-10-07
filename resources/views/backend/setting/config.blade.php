78fe568741f3d0e6a55feafd286586f29e78e80d
arun_lalotra <arunlalotra@codebrewinnovations.com>
2022-10-07 13:08:43 +0530

    dashboardchanges

--- resources/views/backend/setting/config.blade.php
+++ resources/views/backend/setting/config.blade.php
@@ -1514,8 +1514,8 @@ $sms_crendential = json_decode($preference->sms_credentials);
                     <span> <input type="checkbox" data-plugin="switchery" name="is_long_term_service_switch" id="is_long_term_service_switch" class="form-control checkbox_change" data-className="is_long_term_service"  data-color="#43bee1" @if( @getAdditionalPreference(['is_long_term_service'])['is_long_term_service'] == '1') checked='checked' @endif>
                      </span>
                      <input type="hidden"  @if(@getAdditionalPreference(['is_long_term_service'])['is_long_term_service'] == 1) value="1" @else value="0" @endif  name="is_long_term_service"  id="is_long_term_service"/>
-                    
-                   
+
+
                   </div>
                </div>
                </div>
