2eb45de90776193149d9b5b05697b88056c7a1d7
sudhanshu sharma <sudhanshu@codebrewinnovations.com>
2022-10-11 17:29:38 +0530

    fixed issue

--- resources/views/backend/product/edit.blade.php
+++ resources/views/backend/product/edit.blade.php
@@ -311,7 +311,7 @@ if($client_preference_detail->appointment_check == 1 && ($client_preference_deta
                                         @foreach ($roles as $key => $_role)
                                             <div class="col-4 mb-2">
                                                 {!! Form::label('title', $_role['role'].' '. __('Price'), ['class' => 'control-label']) !!}
-                                                <input type="number" class="form-control" min="0" id="{{lcfirst($_role['role'])}}_price" onkeyup="isNumberKeyMax(event)" placeholder="0" name="role_price[{{$_role['id']}}]" value="{{ $product->productVariantByRoles[$key] ? (decimal_format($product->productVariantByRoles[$key]->amount) ?? 0.00) : 0.00 }}">
+                                                <input type="number" class="form-control" min="0" id="{{lcfirst($_role['role'])}}_price" onkeyup="isNumberKeyMax(event)" placeholder="0" name="role_price[{{$_role['id']}}]" value="{{ isset($product->productVariantByRoles[$key]) ? (decimal_format($product->productVariantByRoles[$key]->amount) ?? 0.00) : 0.00 }}">
                                                 <input type="hidden" class="form-control" min="0" name="role_id[{{lcfirst($_role['role'])}}]" value="{{$_role['id']}}">
                                             </div>
                                         @endforeach
