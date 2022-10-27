61e42e5893c14bb4bf264440ce4ec48125e1b757
Jasmeet Singh <jassmeet.s@codebrewinnovations.com>
2022-10-27 17:49:00 +0530

    changes in template

--- resources/views/frontend/home_page_8/recently_viewed.blade.php
+++ resources/views/frontend/home_page_8/recently_viewed.blade.php
@@ -1,5 +1,4 @@
-<div class="product-card-box position-relative al_box_third_template al"  >
-    {{-- {{ dd($product)}} --}}
+<div class="product-card-box position-relative al_box_third_template al">
     {{--<div class="add-to-fav 12">
         <input id="fav_pro_one" type="checkbox">
         <label for="fav_pro_one"><i class="fa fa-heart-o fav-heart" aria-hidden="true"></i></label>
