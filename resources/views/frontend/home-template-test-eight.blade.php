b5b6bb751f9777e5dc0323aa785792b41639f185
Praveen121 <praveen.kumar@codebrewinnovations.com>
2022-10-27 18:56:55 +0530

    design changes

--- resources/views/frontend/home-template-test-eight.blade.php
+++ resources/views/frontend/home-template-test-eight.blade.php
@@ -697,19 +697,21 @@
 
 		@endif
         @elseif($homePageLabel->slug == 'spotlight_deals' && (count($homePageData['spotlight_deals']) != 0))
-		<section class="container mb-0 render_full_{{$homePageLabel->slug}}" id="{{$homePageLabel->slug.$key}}">
-			<div class="top-heading d-flex justify-content-between">
-				<h2 class="h2-heading"> @php
-					echo (!empty($homePageLabel->translations->first()->title)) ? $homePageLabel->translations->first()->title : __($homePageLabel->title);
-					@endphp </h2>
-			</div>
-			<div class="row">
-				<div class="col-12">
-					<div class="product-4-{{$homePageLabel->slug}} product-m  render_{{$homePageLabel->slug}}" id="{{$homePageLabel->slug.$key}}">
-						@foreach ($homePageData[$homePageLabel->slug] as $vendor )
-						@include('frontend.home_page_8.vendor')
-						@endforeach
-					</div>
+		
+		<section class="mb-0 render_full_{{$homePageLabel->slug}}" id="{{$homePageLabel->slug.$key}}">
+			<div class="container">
+				<div class="top-heading d-flex justify-content-between">
+					<h2 class="h2-heading"> @php
+						echo (!empty($homePageLabel->translations->first()->title)) ? $homePageLabel->translations->first()->title : __($homePageLabel->title);
+						@endphp </h2>
+						<a class="" href="">See All  <img class="" src="{{asset('images/template-8/arrow.png')}}" alt="" title=""> </a> 
+				</div>
+				<div class="product-4-{{$homePageLabel->slug}} product-m  render_{{$homePageLabel->slug}}" id="{{$homePageLabel->slug.$key}}">
+					<div class="row">
+							@foreach ($homePageData[$homePageLabel->slug] as $vendor )
+							@include('frontend.home_page_8.vendor')
+							@endforeach
+						</div>
 				</div>
 			</div>
 		</section>
--- resources/views/frontend/home_page_8/vendor.blade.php
+++ resources/views/frontend/home_page_8/vendor.blade.php
@@ -1,24 +1,28 @@
-<div class="product-card-box position-relative text-center al_custom_vendors_sec"  >
-    <a class="suppliers-box d-block" href="{{route('vendorDetail')}}/{{ $vendor->slug }}">
-        <div class="suppliers-img-outer position-relative ">
-            @if($vendor->is_vendor_closed==1) 
-                <img class="fluid-img mx-auto blur-up lazyload grayscale-image" data-src="{{ $vendor->logo['image_fit'] }}200/200{{ $vendor->logo['image_path'] }}" alt="" title="">
-            @else
-                <img  class="fluid-img mx-auto blur-up lazyload" data-src="{{ $vendor->logo['image_fit'] }}200/200{{ $vendor->logo['image_path'] }}" alt="" title="">
-            @endif
+<div class="col-md-4">
+    <div class="deals-product product-card-box position-relative text-center al_custom_vendors_sec"  >
+        <a class="suppliers-box d-block" href="{{route('vendorDetail')}}/{{ $vendor->slug }}">
+            <div class="suppliers-img-outer position-relative ">
+                @if($vendor->is_vendor_closed==1) 
+                    <img class="fluid-img mx-auto blur-up lazyload grayscale-image" data-src="{{ $vendor->logo['image_fit'] }}200/200{{ $vendor->logo['image_path'] }}" alt="" title="">
+                @else
+                    <img  class="fluid-img mx-auto blur-up lazyload" data-src="{{ $vendor->logo['image_fit'] }}200/200{{ $vendor->logo['image_path'] }}" alt="" title="">
+                @endif
 
-        </div>
-        <div class="supplier-rating">
-            <h6 class="mb-1 ellips">{{ $vendor->name }}</h6>
-            {{--<p title="<%=vendor.categoriesList %>" class="vendor-cate mb-1 ellips d-none">
-                <%=vendor.categoriesList %>
-            </p>--}}
-                {{-- <% if(vendor.timeofLineOfSightDistance !=undefined){%>
-                    <div class="pref-timing"> <span><%=vendor.timeofLineOfSightDistance %></span> </div>
-                <%}%> --}}
-        </div>
-        @if($client_preference_detail && $client_preference_detail->rating_check==1)
-        @if($vendor->vendorRating >0) <span class="rating-number">{{ $vendor->vendorRating }} </span>
-        @endif @endif 
-    </a>
+            </div>
+            <div class="supplier-rating">
+                <h4>{{ $vendor->name }}</h4>
+                <h5>10-50% OFF</h5>
+                <a href="#">SHOP NOW</a>
+                <!-- {{--<p title="<%=vendor.categoriesList %>" class="vendor-cate mb-1 ellips d-none">
+                    <%=vendor.categoriesList %>
+                </p>--}}
+                    {{-- <% if(vendor.timeofLineOfSightDistance !=undefined){%>
+                        <div class="pref-timing"> <span><%=vendor.timeofLineOfSightDistance %></span> </div>
+                    <%}%> --}} -->
+            </div>
+            @if($client_preference_detail && $client_preference_detail->rating_check==1)
+            @if($vendor->vendorRating >0) <span class="rating-number">{{ $vendor->vendorRating }} </span>
+            @endif @endif 
+        </a>
+    </div>
 </div>
