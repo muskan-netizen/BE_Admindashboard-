7a5d594fcd63335434083ef562e3dcc490b4bde3
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2022-11-01 17:10:45 +0530

    changes

--- resources/views/frontend/home_page_8/category.blade.php
+++ resources/views/frontend/home_page_8/category.blade.php
@@ -1,11 +1,10 @@
-
-    
-    <div class="col-md-3">
-        <div class="category-block">
-            <a class="brand-box d-block black-box" href="javascript:void(0);">
-                <div class="brand-img"> 
-                    <img class="blur-up lazyload" data-src="{{$category->icon['proxy_url'].'200/200'.$category->icon['image_path']}}" alt="" title="">
-                </div> 
-            </a>
-        </div>
+<div class="col-md-3">
+    <div class="category-block">
+        <a class="brand-box d-block black-box" href="javascript:void(0);">
+            <div class="brand-img"> 
+                <img class="blur-up lazyload" data-src="{{$category->icon['proxy_url'].'200/200'.$category->icon['image_path']}}" alt="" title="">
+            </div>
+            <h6>{{ $category->name }}</h6> 
+        </a>
     </div>
+</div>
