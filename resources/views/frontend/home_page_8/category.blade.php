2155ceaaf3f51213f8cf59742897412a1e0202dd
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2022-11-01 17:33:40 +0530

    changes

--- resources/views/frontend/home_page_8/category.blade.php
+++ resources/views/frontend/home_page_8/category.blade.php
@@ -1,10 +1,11 @@
+{{-- @dd($category) --}}
 <div class="col-md-3">
     <div class="category-block">
-        <a class="brand-box d-block black-box" href="javascript:void(0);">
+        <a class="brand-box d-block black-box" href="{{route('categoryDetail', $category->slug)}}">
             <div class="brand-img"> 
                 <img class="blur-up lazyload" data-src="{{$category->icon['proxy_url'].'200/200'.$category->icon['image_path']}}" alt="" title="">
             </div>
-            <h6>{{ $category->name }}</h6> 
+            <h6 class="text-center">{{ $category->name }}</h6> 
         </a>
     </div>
 </div>
