0a1d62c5c74f8acee9dc770ed7ff354af96852d7
harbans <harbans.singh@codebrewinnovations.com>
2022-11-18 12:28:08 +0530

    udpdate

--- resources/views/layouts/store/footer-content-template-one.blade.php
+++ resources/views/layouts/store/footer-content-template-one.blade.php
@@ -1,6 +1,5 @@
 @php
 $clientData = \App\Models\Client::where('id', '>', 0)->first();
-dd($clientData);
 $urlImg = $clientData ? $clientData->logo['original'] : ' ';
 $pages = \App\Models\Page::with(['translations' => function($q) {$q->where('language_id', session()->get('customerLanguage') ??1);}])->whereHas('translations', function($q) {$q->where(['is_published' => 1, 'language_id' => session()->get('customerLanguage') ??1]);})->orderBy('order_by','ASC')->get();
 $applocale = 'en';
