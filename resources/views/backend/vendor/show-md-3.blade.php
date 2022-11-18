2b83eed087ddcf17cb44e52344e8818a0e7a45f5
surendrasingh@codebrewinnovations.com <surendrasingh@codebrewinnovations.com>
2022-11-18 19:23:33 +0530

    pragati and nandani tasks

--- resources/views/backend/vendor/show-md-3.blade.php
+++ resources/views/backend/vendor/show-md-3.blade.php
@@ -518,8 +518,18 @@
                             <input class="form-control" onkeypress="return isNumberKey(event)" name="commission_monthly" type="text" value="{{$vendor->commission_monthly}}">
                         </div>
                     </div> -->
-                    <div class="col-md-12">
-                        <div class="form-group" id="service_fee_percentInput">
+                    <div class="col-md-12 mb-2 d-flex align-items-center justify-content-between">
+                        {!! Form::label('title', __('Fixed Service Fee'),['class' => 'control-label']) !!}
+                        <input type="checkbox" data-plugin="switchery" name="fixed_service_charge" class="form-control" data-color="#43bee1" @if($vendor->fixed_service_charge == 1) checked @endif {{$vendor->status == 1 ? '' : 'disabled'}}>
+                    </div>
+                    <div class="col-md-12" id="fixed_service_charge_div" style="display:{{$vendor->fixed_service_charge == 1 ? 'block' : 'none'}}">
+                        <div class="form-group">
+                            {!! Form::label('title', __('Service Fee'),['class' => 'control-label']) !!}
+                            <input class="form-control" name="service_charge_amount" type="text" value="{{$vendor->service_charge_amount}}" min="0" {{$vendor->status == 1 ? '' : 'disabled'}} >
+                        </div>
+                    </div>
+                    <div class="col-md-12" id="service_fee_percentInput" style="display:{{$vendor->fixed_service_charge == 1 ? 'none' : 'block'}}">
+                        <div class="form-group">
                             {!! Form::label('title', __('Service Fee Percent'),['class' => 'control-label']) !!}
                             <input class="form-control" name="service_fee_percent" type="text" min="0" maxlength="5" value="{{$vendor->service_fee_percent}}" onkeypress="return isNumberKey(event)" onkeydown="if(this.value.length > 6) return false;">
                         </div>
@@ -1269,6 +1279,18 @@ $( document ).ready(function() {
             $("#need_container_charges").css("display", "none");
         } else {
             $("#need_container_charges").css("display", "block");
+        }
+    })
+
+    $("input[name='fixed_service_charge']").change(function() {
+        if($(this).prop('checked')){
+            $("#fixed_service_charge_div").css("display", "block");
+            $("#service_fee_percentInput").css("display", "none");
+            $("input[name='service_fee_percent']").val(0.00);
+        } else {
+            $("#fixed_service_charge_div").css("display", "none");
+            $("input[name='service_charge_amount']").val(0.00);
+            $("#service_fee_percentInput").css("display", "block");
         }
     })
 </script>
