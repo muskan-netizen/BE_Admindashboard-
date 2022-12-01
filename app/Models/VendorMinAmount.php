3e47826c46531f1e32c9f1baa2e47a2f7f4ca5f8
amitmehraFullstack <amit.mehra@codebrewinnovations.com>
2022-12-01 09:45:39 +0000

    fixes

--- app/Models/VendorMinAmount.php
+++ app/Models/VendorMinAmount.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class VendorMinAmount extends Model
 {
     use HasFactory;
+    protected $fillable=['vendor_id','role_id','order_min_amount','created_at','updated_at'];
 }
