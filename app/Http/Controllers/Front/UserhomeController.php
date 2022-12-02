d2494592d7bafa6a813ae57c99e2790dabf52eec
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2022-12-02 17:17:15 +0530

    changes

--- app/Http/Controllers/Front/UserhomeController.php
+++ app/Http/Controllers/Front/UserhomeController.php
@@ -856,6 +856,8 @@ class UserhomeController extends FrontController
                 'category' => ($on_sale_product_detail->category->categoryDetail->translation) ? ( $on_sale_product_detail->category->categoryDetail->translation->first()->name ?? $on_sale_product_detail->category->categoryDetail->slug): $on_sale_product_detail->category->categoryDetail->slug??''
             );
         }
+
+        $top_rated_products = '';
           
         if($this->checkTemplateForAction(8)){
             $recently_viewed = $this->productvendorProducts($vendor_ids, $language_id, $currency_id, '', $request->type,$p_dim);
