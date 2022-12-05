fa98f255d59dfa8669464babc397489b2e603777
harbans <harbans.singh@codebrewinnovations.com>
2022-12-05 15:01:55 +0530

    issue fixed

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -18,7 +18,10 @@ class Vendor extends Model implements Auditable{
     }
 
     public function products(){
-      return $this->hasMany('App\Models\Product', 'vendor_id', 'id')->where('is_long_term_service',0);
+      if(checkColumnExists('products','is_long_term_service')){
+        return $this->hasMany('App\Models\Product', 'vendor_id', 'id')->where('is_long_term_service',0);
+      }
+      return $this->hasMany('App\Models\Product', 'vendor_id', 'id');
     }
 
     public function productsLive(){
