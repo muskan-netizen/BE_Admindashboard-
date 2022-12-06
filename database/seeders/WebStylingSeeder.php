e4157b59542679eec73d881f8bd39d59065d0503
Mukesh yadav <mukeshkyadav@codebrewinnovations.com>
2022-12-06 19:24:34 +0530

    - fixed

--- database/seeders/WebStylingSeeder.php
+++ database/seeders/WebStylingSeeder.php
@@ -54,6 +54,13 @@ class WebStylingSeeder extends Seeder{
             'is_selected' => '0',
             'template_id' => '4',
         ]);
+        $app_styling_option = WebStylingOption::insert([
+            'web_styling_id' => $app_styling,
+            'name' => 'p2p',
+            'image' => 'template-nine.png',
+            'is_selected' => '0',
+            'template_id' => '9',
+        ]);
 
 
      
