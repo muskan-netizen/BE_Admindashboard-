fcf5188c0b6a93963f1d0153b538476002cba76d
Praveen121 <praveen.kumar@codebrewinnovations.com>
2022-12-06 18:45:41 +0530

    template8 design issues fixed

--- public/js/location.js
+++ public/js/location.js
@@ -270,8 +270,7 @@ $(document).ready( async function () {
             dataType: 'json',
             url: `/setSessionIndex?type=${type}`,
             success: function (response) {
-               //location.reload();
-               window.location.href = home_page_url;
+               location.reload();
             }
         });
     }
@@ -737,7 +736,7 @@ $(document).ready( async function () {
                                         dots: false,
                                         infinite: true,
                                         speed: 300,
-                                        slidesToShow: 4,
+                                        slidesToShow: 5,
                                         slidesToScroll: 3,
                                         responsive: [
                                             {breakpoint: 1367,settings: {slidesToShow: 4,slidesToScroll: 2,infinite: true}},
@@ -1267,3 +1266,4 @@ $(document).delegate("#edit-address #address-input", "focus", function(){
 
 
 
+P
