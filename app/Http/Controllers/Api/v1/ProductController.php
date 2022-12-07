2cf539e2ea9d79687ee32937eadfacdbdcecdebb
harbans <harbans.singh@codebrewinnovations.com>
2022-12-07 17:37:34 +0530

    add missing coloum in product api

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -150,7 +150,7 @@ class ProductController extends BaseController
                             $q2->select('addon_options.id', 'addon_options.title', 'addon_options.price', 'apt.title', 'addon_options.addon_id');
                             $q2->where('apt.language_id', $langId)->groupBy(['addon_options.id', 'apt.language_id']);
                         },
-                        ])->select('id', 'sku', 'url_slug', 'weight', 'weight_unit', 'vendor_id', 'is_new', 'is_featured', 'is_physical', 'has_inventory', 'has_variant', 'sell_when_out_of_stock', 'requires_shipping', 'Requires_last_mile', 'averageRating','minimum_order_count','batch_count','minimum_duration_min','minimum_duration')
+                        ])->select('id', 'sku', 'url_slug', 'weight', 'weight_unit', 'vendor_id', 'is_new', 'is_featured', 'is_physical', 'has_inventory', 'has_variant', 'sell_when_out_of_stock', 'requires_shipping', 'Requires_last_mile', 'averageRating','minimum_order_count','batch_count','minimum_duration_min','minimum_duration','additional_increments','additional_increments_min')
                         ->where('id', $pid)
                         ->first();
 
