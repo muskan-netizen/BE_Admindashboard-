8cdbcf1364a8644242038eee3fadda19d2b54598
Mukesh yadav <mukeshkyadav@codebrewinnovations.com>
2022-12-13 18:06:22 +0530

    add post

--- resources/views/layouts/store/left-sidebar-template-four.blade.php
+++ resources/views/layouts/store/left-sidebar-template-four.blade.php
@@ -332,7 +332,7 @@ $pages = \App\Models\Page::with([
                                     <ul class="header-dropdown ml-auto d-lg-block d-none">
                                         @if($client_preference_detail->header_quick_link == 1)
                                         @if( p2p_module_status() )
-                                            <li><a href="{{route('vendor.index', ['fullPage'=>1])}}">{{ __('Add Post') }}</a></li>
+                                            <li><a href="{{route('posts.index', ['fullPage'=>1])}}">{{ __('Add Post') }}</a></li>
                                         @endif
                                         <li class="onhover-dropdown quick-links quick-links pr-2 mr-2">
 
