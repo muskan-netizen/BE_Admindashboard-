2c1284c2d1ecb9ece2d6099233362ed1e8acc9fb
Praveen121 <praveen.kumar@codebrewinnovations.com>
2022-12-14 17:21:26 +0530

    design changes

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -492,7 +492,7 @@ $clientData = \App\Models\Client::select('socket_url')->first();
                                             else
                                                 $checkSlot = 0;
                                         @endphp
-                                        <div class="btn-wrapper d-flex align-items-center">
+                                        <div class="btn-wrapper">
                                             <div id="product_variant_quantity_wrapper" style="display: <?php echo ($product->category->categoryDetail->type_id == 10) ? 'none':'inline-block'; ?>">
                                                 @if($product->inquiry_only == 0)
                                                 <div class="product-description border-product pb-0">
