5a479e5f345466623d48902b33b45642bdd68f28
harbans <harbans.singh@codebrewinnovations.com>
2022-12-16 11:30:32 +0530

    update dev issue

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -42,7 +42,7 @@ use App\Models\OrderProductPrescription;
 use App\Models\SubscriptionInvoicesUser;
 use App\Models\UserRegistrationDocuments;
 use App\Models\DriverRegistrationDocument;
-use App\Models\{VendorOrderDispatcherStatus, VerificationOption ,DispatcherStatusOption, ReturnReason};
+use App\Models\{VendorOrderDispatcherStatus, VerificationOption ,DispatcherStatusOption, ReturnReason,OrderDeliveryStatusIcon};
 
 use Illuminate\Http\Request;
 use App\Models\LuxuryOption;
@@ -53,6 +53,7 @@ use App\Http\Traits\{ApiResponser,CartManager};
 use App\Models\AddonOption;
 use App\Models\{OrderLongTermServices,OrderLongTermServicesAddon,OrderLongTermServiceSchedule};
 use App\Models\ProductVariantSet;
+
 use GuzzleHttp\Client as GCLIENT;
 use App\Models\AutoRejectOrderCron;
 use Illuminate\Support\Facades\Session;
