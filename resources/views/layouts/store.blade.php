72df995d768763e0490941c7157059fe0107222a
Praveen121 <praveen.kumar@codebrewinnovations.com>
2022-12-19 15:43:28 +0530

    - fixed

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -66,7 +66,7 @@ if(isset($set_template))
   elseif($set_template->template_id == 8)
     $body_class = "al_body_template_eight";
   elseif($set_template->template_id == 9)
-    $body_class = "al_body_template_nine";
+    $body_class = "al_body_template_nine p2p-module";
 }
 
 
@@ -103,7 +103,7 @@ if(isset($set_template))
 </script>
 @include('layouts.language')
 @yield('headerJs')
-<body  class="{{$dark_mode}}{{ Request::is('category/cabservice') ? 'cab-booking-body' : '' }} {{$body_class}} {{p2p_module_status() ? 'p2p-module' : ''}}" dir="{{session()->get('locale') == 'ar' ? 'rtl' : ''}}">
+<body  class="{{$dark_mode}}{{ Request::is('category/cabservice') ? 'cab-booking-body' : '' }} {{$body_class}} " dir="{{session()->get('locale') == 'ar' ? 'rtl' : ''}}">
 <article id="page-container">
   <article id="content-wrap">
   @if(isset($set_template)  && ($set_template->template_id == 3 || $set_template->template_id == 6 || $set_template->template_id == 1 ))
