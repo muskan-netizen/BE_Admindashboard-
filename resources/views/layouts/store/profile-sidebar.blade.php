a323717424c182fcc96efc9e5cbc1d2467480fdb
bamit kumar <bamit.kumar@codebrewinnovations.com>
2022-12-20 18:27:30 +0530

    chat user

--- resources/views/layouts/store/profile-sidebar.blade.php
+++ resources/views/layouts/store/profile-sidebar.blade.php
@@ -30,7 +30,11 @@ $clientData = \App\Models\Client::select('id', 'logo','socket_url')->first();
                             <li  class="{{ (request()->is('user/chat/userVendor')) ? 'active' : '' }}">
                                 <a href="{{route('userChat.UservendorChat')}}">{{ __('Vendor Chat') }}</a>
                             </li>
-
+                            @if(p2p_module_status())
+                            <li  class="{{ (request()->is('user/chat/vendorUser')) ? 'active' : '' }}">
+                                <a href="{{route('userChat.vendorUserChat')}}">{{ __('User Chat') }}</a>
+                            </li>
+                            @endif
                             <li  class="{{ (request()->is('user/chat/userAgent')) ? 'active' : '' }}">
                                 <a href="{{route('userChat.UserAgentChat')}}">{{ __('Driver Chat') }}</a>
                             </li>
