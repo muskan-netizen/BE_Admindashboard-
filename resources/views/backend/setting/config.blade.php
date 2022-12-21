6281d6896c60a2d402878feee4fd3f80dfe481d3
harbans <harbans.singh@codebrewinnovations.com>
2022-12-21 11:54:54 +0530

    conflict solve

--- resources/views/backend/setting/config.blade.php
+++ resources/views/backend/setting/config.blade.php
@@ -1748,7 +1748,6 @@ $getAdditionalPreference = getAdditionalPreference(['hubspot_access_token','is_h
    </div>
 </div>
 
-<<<<<<< HEAD
      
       <div class="col-md-4">
          <div class="form-group d-flex justify-content-between mb-3 alCustomToggleColor">
@@ -1766,16 +1765,6 @@ $getAdditionalPreference = getAdditionalPreference(['hubspot_access_token','is_h
                      <input type="hidden"  @if(@getAdditionalPreference(['is_cab_pooling'])['is_cab_pooling'] == 1) value="1" @else value="0" @endif  name="is_cab_pooling"  id="is_cab_pooling"/>
                   </div>
                </div>
-=======
-<div class="col-md-4">
-   <div class="form-group d-flex justify-content-between mb-3 alCustomToggleColor">
-      <label for="is_cab_pooling_switch" class="mr-2 mb-0">{{__('Cab Pooling')}}<small class="d-block pr-5">{{__("Enable to allow customers to book Cab Pooling.")}}</small></label>
-      <span> <input type="checkbox" data-plugin="switchery" name="is_cab_pooling_switch" id="is_cab_pooling_switch" class="form-control checkbox_change" data-className="is_cab_pooling" data-color="#43bee1" @if(@getAdditionalPreference(['is_cab_pooling'])['is_cab_pooling']==1) checked='checked' @endif>
-      </span>
-      <input type="hidden" @if(@getAdditionalPreference(['is_cab_pooling'])['is_cab_pooling']==1) value="1" @else value="0" @endif name="is_cab_pooling" id="is_cab_pooling" />
-   </div>
-</div>
->>>>>>> pre_dev
 
 <div class="col-md-4">
    <div class="form-group d-flex justify-content-between mb-3 alCustomToggleColor">
