464a3e61bac412382b30019c027961378cc00c36
harbans <harbans.singh@codebrewinnovations.com>
2022-12-28 19:25:51 +0530

    upate order view

--- resources/views/backend/order/view.blade.php
+++ resources/views/backend/order/view.blade.php
@@ -752,7 +752,7 @@ $timezone = Auth::user()->timezone;
                 <p class="mb-2"><span class="fw-semibold me-2">{{ __('Address') }}:</span>
                     {{ $order->address ? $order->address->house_number . ',' : '' }}
                     {{ $order->address ? $order->address->address : '' }}
-                    {{ $processorProduct->address ? $processorProduct->address : ''}}
+                    {{ (isset($processorProduct) &&!empty($processorProduct->address )) ?  $processorProduct->address : ''}}
 
                 </p>
                 @if (isset($order->address) && !empty($order->address->street))
