a449adf4daa9eb9b80c6df57917ea23f531ffb1f
surendrasingh@codebrewinnovations.com <surendrasingh@codebrewinnovations.com>
2022-12-28 18:53:29 +0530

    subscription save

--- app/Http/Controllers/Front/StripeGatewayController.php
+++ app/Http/Controllers/Front/StripeGatewayController.php
@@ -371,10 +371,10 @@ class StripeGatewayController extends FrontController
                 $returnUrl = route('user.orders');
             }
             elseif($payment_form == 'subscription'){
-                // $subscription = $parameters['subscription_id'];
-                // $request = new Request(['payment_option_id' => 4, 'amount' => $amount, 'transaction_id' => $transactionId]);
-                // $subscriptionController = new UserSubscriptionController();
-                // $subscriptionController->purchaseSubscriptionPlan($request, '', $subscription);
+                $subscription = $parameters['subscription_id'];
+                $request = new Request(['payment_option_id' => 4, 'amount' => $amount, 'transaction_id' => $transactionId]);
+                $subscriptionController = new UserSubscriptionController();
+                $subscriptionController->purchaseSubscriptionPlan($request, '', $subscription);
                 $message = __('Your subscription has been activated successfully.');
                 $returnUrl = route('user.subscription.plans');
             }
