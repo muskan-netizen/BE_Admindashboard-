4de9310408aa3058206f15fc1b5371139d32bef8
surendrasingh@codebrewinnovations.com <surendrasingh@codebrewinnovations.com>
2022-12-29 18:19:55 +0530

    subs changes

---
+++ database/migrations/2022_10_11_063222_create_processor_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProcessorProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('processor_products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned();
+            $table->tinyInteger('is_processor_enable')->nullable()->default(0)->comment('0-vendor, 1-processor');
+            $table->string('name');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('processor_products');
+    }
+}
