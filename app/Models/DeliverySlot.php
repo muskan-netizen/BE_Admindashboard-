e0389316f94bb279626bb713a548c1ff97d952e2
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2022-12-30 17:54:42 +0530

    changes

--- app/Models/DeliverySlot.php
+++ app/Models/DeliverySlot.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class DeliverySlot extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = ['title', 'start_time', 'end_time', 'price', 'status'];
     
 }
