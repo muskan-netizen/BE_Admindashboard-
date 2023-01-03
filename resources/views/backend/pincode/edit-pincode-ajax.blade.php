14478f104fa5aef704588481885aaa315142196c
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-01-03 19:57:49 +0530

    changes

--- resources/views/backend/pincode/edit-pincode-ajax.blade.php
+++ resources/views/backend/pincode/edit-pincode-ajax.blade.php
@@ -1,4 +1,5 @@
 <div class="row">
+    
     <div class="col-12">
         <input type="hidden" name="pincode_id" id="pincode_id" value="{{$pincode->id}}">
         <input type="hidden" name="vendor_id" id="vendor_id" value="{{$pincode->vendor_id}}">
