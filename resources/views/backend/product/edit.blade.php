5a8687d8922a2fe3c6a3732da6e8d25b4b140121
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-01-03 20:25:20 +0530

    changes

--- resources/views/backend/product/edit.blade.php
+++ resources/views/backend/product/edit.blade.php
@@ -145,11 +145,13 @@ div#attribute_section .col-sm-9 .form-check-inline.w-100 {
   100% { transform: rotate(360deg); }
 }
 .field_wrapper a.add_button i {
+
     font-size: 30px;
     margin-top: -4px;
     display: inline-block;
 }
 .field_wrapper a.remove_button i {
+    
     font-size: 30px;
     margin-top: -4px;
     display: inline-block;
