acb3555232242de42b866f68edc695273c272249
Vishal Saini <vishalsaini@codebrewinnovations.com>
2023-01-06 09:00:04 +0530

    Recurring booking

--- resources/views/backend/setting/config.blade.php
+++ resources/views/backend/setting/config.blade.php
@@ -973,7 +973,7 @@
                                 </div>
                                 </div>
                             </div>
-                            
+
                         </div>
                     </div>
                 </form><!-- SMS Configuration end -->
@@ -2368,6 +2368,15 @@
                                   <span> <input type="checkbox" data-plugin="switchery" name="is_cust_success_signup_email_switch" id="is_cust_success_signup_email_switch" class="form-control checkbox_change" data-className="is_cust_success_signup_email"  data-color="#43bee1" @if( @getAdditionalPreference(['is_cust_success_signup_email'])['is_cust_success_signup_email'] == '1') checked='checked' @endif>
                                    </span>
                                    <input type="hidden"  @if(@getAdditionalPreference(['is_cust_success_signup_email'])['is_cust_success_signup_email'] == 1) value="1" @else value="0" @endif  name="is_cust_success_signup_email"  id="is_cust_success_signup_email"/>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group d-flex justify-content-between mb-3 alCustomToggleColor">
+                                   <label for="is_recurring_booking_switch" class="mr-2 mb-0">{{__('Recurring Booking')}}<small class="d-block pr-5">{{__("Enable to vendor on which product recurring booking.")}}</small></label>
+                                  <span> <input type="checkbox" data-plugin="switchery" name="is_recurring_booking_switch" id="is_recurring_booking_switch" class="form-control checkbox_change" data-className="is_recurring_booking"  data-color="#43bee1" @if( @getAdditionalPreference(['is_recurring_booking'])['is_recurring_booking'] == '1') checked='checked' @endif>
+                                   </span>
+                                   <input type="hidden"  @if(@getAdditionalPreference(['is_recurring_booking'])['is_recurring_booking'] == 1) value="1" @else value="0" @endif  name="is_recurring_booking"  id="is_recurring_booking"/>
                                 </div>
                             </div>
                         </div>
