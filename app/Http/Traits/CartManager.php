a35ec8f70c22a9839906ddfc77a16182b10367af
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-01-09 18:59:39 +0530

    changes

--- app/Http/Traits/CartManager.php
+++ app/Http/Traits/CartManager.php
@@ -439,7 +439,9 @@ trait cartManager{
             }
           // $sub_total+=$opt_price_in_currency;
             /* Getting in vendor loop */
+
             $quantity_role_price = [];
+            
             foreach ($cartData as $ven_key => $vendorData) {
                 $opt_quantity_price_new = 0.00;
                 $addon_price=0;
