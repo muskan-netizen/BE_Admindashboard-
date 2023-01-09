b088ca1b18c370ed270066d2da783363ba11d725
Inderjit <inderjit.singh@codebrewinnovations.com>
2023-01-09 11:45:09 +0530

    add files tofinder

--- config/app.php
+++ config/app.php
@@ -171,7 +171,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         AkibTanjim\Currency\CurrencyServiceProvider::class,
         Kreait\Laravel\Firebase\ServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Orangehill\Iseed\IseedServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
