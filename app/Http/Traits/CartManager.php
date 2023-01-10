5d4b79670035cd8db91267fe7d2597cb39fe4cfe
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-01-10 12:51:32 +0530

    changes

--- app/Http/Traits/CartManager.php
+++ app/Http/Traits/CartManager.php
@@ -379,7 +379,7 @@ trait cartManager{
         ]);
        
         
-        $cartData = $cartData->select('vendor_id', 'luxury_option_id', 'vendor_dinein_table_id', 'id as cart_product_id', 'schedule_type', 'scheduled_date_time', 'schedule_slot','total_booking_time','product_id','cart_id','delivery_date','slot_price','slot_id')->where('status', [0, 1])->where('cart_id', $cart_id)->groupBy('vendor_id')->orderBy('created_at', 'asc')->get();
+        $cartData = $cartData->select('vendor_id', 'luxury_option_id', 'vendor_dinein_table_id', 'id as cart_product_id', 'schedule_type', 'scheduled_date_time', 'schedule_slot','total_booking_time','product_id','cart_id', 'delivery_date', 'slot_price', 'slot_id')->where('status', [0, 1])->where('cart_id', $cart_id)->groupBy('vendor_id')->orderBy('created_at', 'asc')->get();
         // dd($cartData);
        //Get All Taxes    
        $taxRates = $this->getTaxes();
