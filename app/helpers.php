be09cdb3b8c5b1927153603f700059b103ea27e7
harbans <harbans.singh@codebrewinnovations.com>
2023-01-18 11:59:37 +0530

    get only p2p type category in case of p2p business type

--- app/helpers.php
+++ app/helpers.php
@@ -1255,7 +1255,7 @@ if (!function_exists('getCategoryTypesServices')) {
                 $typeArray = ['rental_service'];
                 break;
             case "p2p":
-                $typeArray = ['products_service', 'on_demand_service', 'appointment_service', 'p2p' ];
+                $typeArray = ['p2p' ];
                 break;
             case "super_app":
                 $typeArray = ['pick_drop_service', 'on_demand_service', 'appointment_service', 'rental_service', 'products_service', 'p2p'];
