69eac7d8043ba1a7b5da3698f27b71855836f6ea
bamit kumar <bamit.kumar@codebrewinnovations.com>
2023-02-07 16:57:11 +0530

    css changes 07-02-2023

--- resources/views/frontend/home_page_9/product.blade.php
+++ resources/views/frontend/home_page_9/product.blade.php
@@ -1,5 +1,5 @@
 
-<div class="col-md-2">
+<div class="item">
     <div class="product-card-box position-relative al_box_third_template al"  >
         {{-- {{ dd($product)}} --}}
         {{--<div class="add-to-fav 12">
@@ -9,7 +9,7 @@
         {{-- @dd($product['updated_at']) --}}
         <a class="common-product-box text-center" href="{{ $product['vendor']->slug }}/product/{{ $product['url_slug'] }}">
             <div class="img-outer-box position-relative"> <img class="blur-up lazyload" data-src="{{ $product['image_url'] }}" alt="" title="">
-                <div class="pref-timing"> </div>
+                {{--<div class="pref-timing"> </div>--}}
                 <div class="wishlist-icon btn-default" prosku="{{ $product['sku'] }}" remwishlist='<i class="fa fa-heart" aria-hidden="true"></i>' addwishlist='<i class="fa fa-heart-o" aria-hidden="true"></i>'>
                     @if($product['is_inwishlist_btn'] == 0) 
                         <i class="fa fa-heart-o" aria-hidden="true"></i>
