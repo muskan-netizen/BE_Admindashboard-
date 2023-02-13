66f1ec1a4aaec6b0fe83078cddb5a59a8da9d584
harbans <harbans.singh@codebrewinnovations.com>
2023-02-13 21:27:38 +0530

    update order api controller

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -587,7 +587,8 @@ class OrderController extends BaseController
                                 $order_product->dispatch_agent_id = ! empty($vendor_cart_product->dispatch_agent_id) ? $vendor_cart_product->dispatch_agent_id : null;
                             }
                             $order_product->schedule_slot = ! empty($vendor_cart_product->schedule_slot) ? $vendor_cart_product->schedule_slot : '';
-                            
+                            $order_product->scheduled_date_time = $vendor_cart_product->schedule_type == 'schedule' ? $vendor_cart_product->scheduled_date_time : null;
+                           
                             $order_product->save();
 
 
