bbab4ce7d1dc2f98fc7dd90836e783f34fcfe614
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-02-14 16:04:38 +0530

    fix 404 issue

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -340,12 +340,19 @@ class ProductController extends FrontController{
             if($user){
                 $user_vendor =  UserVendor::where('user_id', $user->id)->first();
             }
-            
-            return view('frontend.'.$product_page)->with(['user_vendor' => $user_vendor, 'shareComponent' => $shareComponent, 'sets' => $sets, 'vendor_info' => $vendor, 'product' => $product, 'navCategories' => $navCategories, 'newProducts' => $newProducts, 'rating_details' => $rating_details, 'is_inwishlist_btn' => $is_inwishlist_btn, 'category' => $category, 'product_in_cart' => $product_in_cart,'is_available'=>$is_available, 'getAdditionalPreference' => $getAdditionalPreference, 'suggested_category_products' => $suggested_category_products, 'suggested_brand_products'=> $suggested_brand_products, 'suggested_vendor_products'=>$suggested_vendor_products, 'coupon_list' => $coupon_list, 'attr_array' => $attr_array, 'set_template' => $set_template]);
+
+            // Date Time Comparison
+            $cutoff_time            = $product->vendor->cutOff_time??'';
+
+            $current_time           = Carbon::now()->toTimeString();
+
+            $parsed_cutoff_time     = Carbon::parse($cutoff_time);
+            $current_time_response  = false;
 
             if( $parsed_cutoff_time->gt($current_time) ) {
                 $current_time_response = true;
             }
+            
             return view('frontend.'.$product_page)->with(['user_vendor' => $user_vendor, 'shareComponent' => $shareComponent, 'sets' => $sets, 'vendor_info' => $vendor, 'product' => $product, 'navCategories' => $navCategories, 'newProducts' => $newProducts, 'rating_details' => $rating_details, 'is_inwishlist_btn' => $is_inwishlist_btn, 'category' => $category, 'product_in_cart' => $product_in_cart,'is_available'=>$is_available, 'getAdditionalPreference' => $getAdditionalPreference, 'suggested_category_products' => $suggested_category_products, 'suggested_brand_products'=> $suggested_brand_products, 'suggested_vendor_products'=>$suggested_vendor_products, 'coupon_list' => $coupon_list, 'attr_array' => $attr_array, 'set_template' => $set_template, 'current_time_response' => $current_time_response]);
         }
    }
