a6635d798842701024c4fc57fa7f0e280037b04c
surendrasingh@codebrewinnovations.com <surendrasingh@codebrewinnovations.com>
2023-02-14 19:21:04 +0530

    my meddy changes

--- resources/views/frontend/cart-page.blade.php
+++ resources/views/frontend/cart-page.blade.php
@@ -1124,6 +1124,7 @@
                                         </b>
                                     </div>
                                 </div>
+                            @endif
                             @if ($serviceType == 'rental')
                                 <div class="row">
                                     <div class="col-6">{{ __('Security Amount') }}</div>
