48e1d80fb8a2e45289bc4717fa4cf2b1d62fdc00
Inderjit <inderjit.singh@codebrewinnovations.com>
2023-02-15 19:09:06 +0530

    done farmMeat Code merge with pre_dev

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -9,11 +9,7 @@ use Redirect;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Front\FrontController;
-<<<<<<< HEAD
-use App\Models\{AddonSet, Cart, CartAddon, CartProduct, User, Product, ClientCurrency, ProductVariant, ProductVariantSet,OrderProduct,VendorOrderStatus,OrderProductRating,Category, Vendor,ProductFaq,ClientLanguage, ProductFaqSelectOption, WebStylingOption,ProductRecentlyViewed, Attribute, ProductAttribute,DeliverySlotProduct, UserVendor, DeliverySlot, ProcessorProduct};
-=======
-use App\Models\{AddonSet, Cart, CartAddon, CartProduct, User, Product, ClientCurrency, ProductVariant, ProductVariantSet,OrderProduct,VendorOrderStatus,OrderProductRating,Category, Vendor,ProductFaq,ClientLanguage, ProductFaqSelectOption, WebStylingOption,ProductRecentlyViewed, Attribute, ProductAttribute,DeliverySlotProduct, UserVendor, DeliverySlot,UserAddress};
->>>>>>> pre_dev
+use App\Models\{AddonSet, Cart, CartAddon, CartProduct, User, Product, ClientCurrency, ProductVariant, ProductVariantSet,OrderProduct,VendorOrderStatus,OrderProductRating,Category, Vendor,ProductFaq,ClientLanguage, ProductFaqSelectOption, WebStylingOption,ProductRecentlyViewed, Attribute, ProductAttribute,DeliverySlotProduct, UserVendor, DeliverySlot,UserAddress,ProcessorProduct};
 
 use Carbon\Carbon;
 use App\Http\Traits\{ProductActionTrait, ProductTrait};
@@ -307,13 +303,8 @@ class ProductController extends FrontController{
                             $product_attr[$key]['attribute_id'] = $value->attribute_id ?? '';
                             $product_attr[$key]['hexacode'] = optional($value->attributeOption)->hexacode ?? '';
                             $product_attr[$key]['type'] = optional($value->attribute)->type ?? '';
-<<<<<<< HEAD
-
-                            if( !empty($value->attribute) && $value->attribute->type != 4) {
-=======
                             
                             if( !empty($value->attribute) && $value->attribute->type != 4 && $value->attribute->type != 6) {
->>>>>>> pre_dev
                                 $product_attr[$key]['value'] = optional($value->attributeOption)->title ?? '';
                             }
                             else {
@@ -350,7 +341,6 @@ class ProductController extends FrontController{
             if($user){
                 $user_vendor =  UserVendor::where('user_id', $user->id)->first();
             }
-<<<<<<< HEAD
 
             // Date Time Comparison
             $cutoff_time            = $product->vendor->cutOff_time??'';
@@ -359,10 +349,8 @@ class ProductController extends FrontController{
 
             $parsed_cutoff_time     = Carbon::parse($cutoff_time);
             $current_time_response  = false;
-=======
             
             return view('frontend.'.$product_page)->with(['user_vendor' => $user_vendor, 'shareComponent' => $shareComponent, 'sets' => $sets, 'vendor_info' => $vendor, 'product' => $product, 'navCategories' => $navCategories, 'newProducts' => $newProducts, 'rating_details' => $rating_details, 'is_inwishlist_btn' => $is_inwishlist_btn, 'category' => $category, 'product_in_cart' => $product_in_cart,'is_available'=>$is_available, 'getAdditionalPreference' => $getAdditionalPreference, 'suggested_category_products' => $suggested_category_products, 'suggested_brand_products'=> $suggested_brand_products, 'suggested_vendor_products'=>$suggested_vendor_products, 'coupon_list' => $coupon_list, 'attr_array' => $attr_array, 'set_template' => $set_template]);
->>>>>>> pre_dev
 
             if( $parsed_cutoff_time->gt($current_time) ) {
                 $current_time_response = true;
