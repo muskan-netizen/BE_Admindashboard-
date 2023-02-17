29152c4f3f591bc2ab2996e30251cbb8cda0dee3
shabnamnazir <shabnam@codebrewinnovations.com>
2023-02-17 16:21:03 +0530

    hotfixes

--- app/Http/Controllers/Client/OrderController.php
+++ app/Http/Controllers/Client/OrderController.php
@@ -2835,6 +2835,12 @@ class OrderController extends BaseController
                     'content-type' => 'application/json'
                 ]
             ]);
+            // Log::info("header", [
+            //     'personaltoken' => $dispatch_domain->delivery_service_key,
+            //     'shortcode' => $dispatch_domain->delivery_service_key_code,
+            //     'content-type' => 'application/json'
+            // ]);
+            //\Log::info("header", $postdata);
             $url = $dispatch_domain->delivery_service_key_url;
 
             $res = $client->post(
