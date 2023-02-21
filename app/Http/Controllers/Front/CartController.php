8b50bd70117fcbf667c05cba6dc1c10ef25de0ca
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-02-21 19:14:13 +0530

    slot fixes

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -553,7 +553,6 @@ class CartController extends FrontController
             }
 
             if($isnew == 1){
-                // dd($cart_product_detail);
                 $cartProduct = CartProduct::create($cart_product_detail);
                 // //\Log::info(json_encode($cart_product_detail));11
 
