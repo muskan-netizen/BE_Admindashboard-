c1cf8663431fed20c0bd93c23ded8d045d8426dd
Gagandeep Singh <gagandeep.singh@codebrewinnovations.com>
2023-02-27 19:01:04 +0530

    ParcelWorks UI Changes done

--- resources/views/frontend/booking/cabbooking-single-module.blade.php
+++ resources/views/frontend/booking/cabbooking-single-module.blade.php
@@ -12,11 +12,11 @@
                 
  @endphp
 
- <section class="cab-banner-area alTaxiBannerStart"
+ <section class="test cab-banner-area alTaxiBannerStart"
      style="background:url({{ $img }});background-size: cover;background-repeat: no-repeat;background-position: center;">
-     <div class="container p-64">
+     <div class="container-fluid p-64 py-64">
          <div class="row">
-             <div class="col-md-8 col-lg-6">
+             <div class="col-md-8 col-lg-7 col-xl-5">
                  <div class="card-box mb-0">
                      <h2>{{ $homePageLabel->translations->first() ? $homePageLabel->translations->first()->title : '' }}
                      </h2>
