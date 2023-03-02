1a5edf85b9a9b637438d2fe75bf96050cf6c7b7b
nitish <nitish@codebrewinnovations.com>
2023-03-02 17:36:04 +0530

    vendor name in orders screen

--- resources/views/frontend/account/orders/active_orders.blade.php
+++ resources/views/frontend/account/orders/active_orders.blade.php
@@ -32,7 +32,7 @@
                                                                     </div>
                                                                     <div class="col-md-3 alOrderStatus">
                                                                         <h4>{{ __(getNomenclatureName('Vendor Name',true)) }}</h4>
-                                                                        <span><a class="text-capitalize">{{ $order->user->name }}</a></span>
+                                                                        <span><a class="text-capitalize">{{ @$order->vendors[0]['vendor']->name }}</a></span>
                                                                     </div>
                                                                     @if ($client_preference_detail->business_type != 'taxi')
                                                                         <div class="col-md-3 ellipsis">
