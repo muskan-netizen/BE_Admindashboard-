85836f5713c86f59fece35520f6b1666a037894b
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-03-06 12:35:50 +0530

    Corrections done

--- public/js/custom.js
+++ public/js/custom.js
@@ -3726,11 +3726,11 @@ $(document).ready(function () {
         }
     });
     $(document).on('click', '.qty-plus-ondemand', function () {
-        $(this).find('.fa').removeClass("fa-minus").addClass("fa-spinner fa-pulse");
         let base_price = $(this).data('base_price');
         let cartproduct_id = $(this).attr("data-id");
         let qty = $('#quantity_ondemand_' + cartproduct_id).val();
         $('#quantity_ondemand_' + cartproduct_id).val(++qty);
+        $(this).find('.fa').removeClass("fa-minus").addClass("fa-spinner fa-pulse");
         updateQuantityOnDemand(cartproduct_id, qty, base_price);
     });
 
