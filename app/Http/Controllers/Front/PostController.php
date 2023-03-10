dcc25862addb2d012e9429ca5bad1da967db6b47
bamit kumar <bamit.kumar@codebrewinnovations.com>
2023-03-10 16:23:29 +0530

    p2p add post             }

--- app/Http/Controllers/Front/PostController.php
+++ app/Http/Controllers/Front/PostController.php
@@ -284,51 +284,57 @@ class PostController extends FrontController
         $user_vendor = UserVendor::where('user_id', $user->id)->first();
         // dd($user_vendor);
         if(@$user_vendor->vendor_id){
-            $product = new Product();
-            $product->sku = $slug;
-            $product->url_slug = $generated_slug;
-            $product->title = empty($request->product_name) ? $request->sku : $request->product_name;
-            $product->type_id = $request->type_id ?? 1;
-            $product->category_id = $request->category_id;
-            $product->vendor_id = $user_vendor->vendor_id ?? $user->id;
-            $product->is_live = 1;
-            $product->publish_at = date('Y-m-d H:i:s');
-            $client_lang = ClientLanguage::where('is_primary', 1)->first();
-            if (!$client_lang) {
-                $client_lang = ClientLanguage::where('is_active', 1)->first();
-            }
-            $product->save();
-            
-
-            
-            if ($product->id > 0) {
-                $datatrans[] = [
-                    'title' => $request->product_name??null,
-                    'body_html' => '',
-                    'meta_title' => '',
-                    'meta_keyword' => '',
-                    'meta_description' => '',
-                    'product_id' => $product->id,
-                    'language_id' => $client_lang->language_id
-                ];
-                $product_category = new ProductCategory();
-                $product_category->product_id = $product->id;
-                $product_category->category_id = $request->category_id;
-                $product_category->save();
-                $proVariant = new ProductVariant();
-                $proVariant->price = $request->price;
-                $proVariant->sku =$slug;
-                $proVariant->title =$slug . '-' .  empty($request->product_name) ?$slug : $request->product_name;
-                $proVariant->product_id = $product->id;
-                $proVariant->barcode = $this->generateBarcodeNumber();
-                $proVariant->quantity = 1;            
-                $proVariant->status = 1;
-                $proVariant->save();
-                ProductTranslation::insert($datatrans);
+            $vendor = Vendor::where('id', $user_vendor->vendor_id)->first();
+            if(@$vendor){
+                $product = new Product();
+                $product->sku = $slug;
+                $product->url_slug = $generated_slug;
+                $product->title = empty($request->product_name) ? $request->sku : $request->product_name;
+                $product->type_id = $request->type_id ?? 1;
+                $product->category_id = $request->category_id;
+                $product->vendor_id = $user_vendor->vendor_id ?? $user->id;
+                $product->is_live = 1;
+                $product->publish_at = date('Y-m-d H:i:s');
+                $client_lang = ClientLanguage::where('is_primary', 1)->first();
+                if (!$client_lang) {
+                    $client_lang = ClientLanguage::where('is_active', 1)->first();
+                }
+                $product->save();
                 
-            }
 
-            return $product;
+                
+                if ($product->id > 0) {
+                    $datatrans[] = [
+                        'title' => $request->product_name??null,
+                        'body_html' => '',
+                        'meta_title' => '',
+                        'meta_keyword' => '',
+                        'meta_description' => '',
+                        'product_id' => $product->id,
+                        'language_id' => $client_lang->language_id
+                    ];
+                    $product_category = new ProductCategory();
+                    $product_category->product_id = $product->id;
+                    $product_category->category_id = $request->category_id;
+                    $product_category->save();
+                    $proVariant = new ProductVariant();
+                    $proVariant->price = $request->price;
+                    $proVariant->sku =$slug;
+                    $proVariant->title =$slug . '-' .  empty($request->product_name) ?$slug : $request->product_name;
+                    $proVariant->product_id = $product->id;
+                    $proVariant->barcode = $this->generateBarcodeNumber();
+                    $proVariant->quantity = 1;            
+                    $proVariant->status = 1;
+                    $proVariant->save();
+                    ProductTranslation::insert($datatrans);
+                    
+                }
+
+                return $product;
+            }
+            else{
+                throw new \ErrorException('Sorry, You are not a vendor.', 400);
+            }
         }else{
             throw new \ErrorException('Sorry, You are not a vendor.', 400);
         }
