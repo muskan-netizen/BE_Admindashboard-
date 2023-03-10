a0a5825b58698059c8e331bdcbb2a8779ee61b3d
amitMehraFullstack <amit.mehra@codebrewinnovations.com>
2023-03-10 12:32:15 +0530

    p2p fixes

--- resources/views/frontend/home_page_2/product.blade.php
+++ resources/views/frontend/home_page_2/product.blade.php
@@ -1,26 +1,30 @@
 <div>
-    <a class="card scale-effect text-center" href="{{ $product['vendor']->slug }}/product/{{ $product['url_slug'] }}">
-        <label class="product-tag">@if($product["tag_title"] != 0) {{$product["tag_title"]}} @else {{$homePageLabel->title}}@endif </label>
+    <a class="card scale-effect text-center" href="{{ $product->vendor_slug }}/product/{{ $product->url_slug }}">
+        <label class="product-tag">@if($product->title != 0) {{$product->title}} @else {{$homePageLabel->title}}@endif </label>
         <div class="product-image">
-            <img class="blur-up lazyloaded" src="{{ $product['image_url'] }}" alt="">
+            <img class="blur-up lazyloaded" src="{{ get_file_path($product->path,'FILL_URL','260','260') }}" alt="">
         </div>
         <div class="media-body align-self-center">
             <div class="inner_spacing px-0">
                 <div class="d-flex align-items-center justify-content-between">
-                    <h3 class="m-0">{{ $product["title"] }}</h3>
-                    @if($client_preference_detail)
-                        @if($client_preference_detail->rating_check == 1)
-                            @if($product["averageRating"] >0)
-                                <span class="rating">{{ $product["averageRating"] }} <i class="fa fa-star text-white p-0"></i></span>
+                    <h3 class="m-0">{{ $product->title }}</h3>
+                        @if($client_preference_detail && $client_preference_detail->rating_check==1) 
+                            @if($product->averageRating >0)
+                                <span class="rating-number">{{ $product->averageRating }}</span>
                             @endif 
-                        @endif
-                    @endif
+                        @endif 
                 </div>
-                <p>{{ $product["vendor_name"] }}</p>
+                <p>{{ $product->vendor_name }}</p>
                 <h4>
-                    @if($product["inquiry_only"] == 0)
-                    {!!$product["price"] ?? ''!!}
-                    @endif
+                    <b> {{ showPriceWithCurrency($product->price_numeric) }} </b>
+
+                        <!-- <p><i class="fa fa-clock-o"></i> 30-40 min</p>  -->
+                        @php
+                        $comp = @$product->compare_price_numeric??0;
+                        @endphp
+                        @if(@$comp && $comp>0)
+                            {!!showPriceWithCurrency($product->compare_price_numeric,'1') !!}
+                        @endif
                 </h4>
             </div>
         </div>
