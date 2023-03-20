02e836c07cbf599d31d2873aa1c40d7d5a99d7ce
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-03-20 19:53:33 +0530

    stripe issue resolve

--- app/Http/Controllers/Front/StripeGatewayController.php
+++ app/Http/Controllers/Front/StripeGatewayController.php
@@ -944,8 +944,9 @@ class StripeGatewayController extends FrontController
             ////// Create webhook Endpoint ///////
             $secret_key = stripeDynamicPaymentCredentials('stripe_ideal')->secret_key;
             $stripe = new \Stripe\StripeClient($secret_key);
-            
+           
             $webhook_url = 'https://'.$domain.'/payment/webhook/stripe_ideal';
+            
             $webhook_exists = false;
 
             // $stripe->webhookEndpoints->delete(
@@ -1018,12 +1019,12 @@ class StripeGatewayController extends FrontController
                 'payment_method_types' => ['ideal'],
                 'amount' => $amount * 100,
                 'currency' => $this->currency, //'eur'
-                // // 'customer' => '',
-                // 'receipt_email' => $user->email ?? '',
-                // 'metadata' => [
-                //     'user_id' => $user->id,
-                //     'payment_form' => $payment_form
-                // ]
+                // 'customer' => '',
+                'receipt_email' => $user->email ?? '',
+                'metadata' => [
+                    'user_id' => $user->id,
+                    'payment_form' => $payment_form
+                ]
             ];
 
             // if(isset($customer_id) && !empty($customer_id)){
