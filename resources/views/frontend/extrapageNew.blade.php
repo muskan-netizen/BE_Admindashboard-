5f01ab6c16c700495175d3673f26af9e14830732
harbans <harbans.singh@codebrewinnovations.com>
2023-03-28 16:14:01 +0530

    saller module changes

--- resources/views/frontend/extrapageNew.blade.php
+++ resources/views/frontend/extrapageNew.blade.php
@@ -223,7 +223,7 @@
                                         <div class="col-md-6 mb-3" id="nameInput">
                                             <label for="vendortype">{{__('Vendor Type')}}</label>
                                             <select name="vendor_type" id="vendor_type" class="form-control">
-                                                <option value="1">ClickOKartPartner</option>
+                                                <option value="1">Seller</option>
                                                 <option value="0">Shopper</option>
                                             </select>
                                         </div>
