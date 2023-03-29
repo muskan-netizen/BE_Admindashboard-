a3fb1e6cec4be79deaef9b60f0f7f20607b52a33
nitish <nitish@codebrewinnovations.com>
2023-03-29 12:29:24 +0530

    view more description issue fixed

--- app/Http/Controllers/Api/v1/v2/VendorController.php
+++ app/Http/Controllers/Api/v1/v2/VendorController.php
@@ -2791,7 +2791,7 @@ class VendorController extends BaseController{
                     },
                     'media.image', 
                     'translation' => function($q) use($langId){
-                        $q->select('product_id', 'title', 'body_html', 'meta_title', 'meta_keyword', 'meta_description','language_id')->where('language_id', $langId)->orderBy('id','desc');
+                    $q->select('product_id', 'title', 'body_html', 'meta_title', 'meta_keyword', 'meta_description','language_id','body_html as translation_description')->where('language_id', $langId)->orderBy('id','desc');
                     },
                     'variant' => function($q) use($langId, $multipli){
                         $q->select('id','sku', 'product_id', 'quantity', 'price','markup_price', 'barcode', 'compare_at_price',DB::raw("'$multipli' as multiplier"),)->orderBy('quantity', 'desc');
