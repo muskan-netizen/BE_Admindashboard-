c022a990a6db312b15986a69958bc5eebd711f50
nitish <nitish@codebrewinnovations.com>
2023-03-29 17:38:17 +0530

    tax issue revert

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -418,7 +418,7 @@ class OrderController extends BaseController
                                     if (!in_array($tax_rate_detail->id, $tax_category_ids)) {
                                         $tax_category_ids[] = $tax_rate_detail->id;
                                     }
-                                    $rate = $tax_rate_detail->tax_rate;
+                                    $rate = round($tax_rate_detail->tax_rate);
                                     $tax_amount = ($price_in_dollar_compare * $rate) / 100;
                                     $product_tax = ($quantity_price+$productAddon_price) * $rate / 100;
                                     $taxable_amount = $taxable_amount + $product_tax;
@@ -464,7 +464,7 @@ class OrderController extends BaseController
                                 }
                             }
                             //$taxable_amount += $product_taxable_amount;
-                            $vendor_taxable_amount += decimal_format($taxable_amount);
+                            $vendor_taxable_amount += $taxable_amount;
                             //$total_amount += ($vendor_cart_product->quantity * $variant->price) + ($vendor_cart_product->quantity * $variant->container_charges);
                             $total_amount += ($vendor_cart_product->quantity * $variant->price);
                             $order_product = new OrderProduct;
@@ -1851,7 +1851,7 @@ class OrderController extends BaseController
 
                         $returnHTML = view('email.newOrderProducts')->with(['cartData' => $cartDetails, 'order' => $order, 'currencySymbol' => $currSymbol, 'luxuryOptionTitle' => $luxuryOptionTitle])->render();
                     } else {
-                        $returnHTML = view('email.newOrderVendorProducts')->with(['cartData' => $cartDetails,'order' => $order, 'id' => $vendor_id, 'currencySymbol' => $currSymbol, 'luxuryOptionTitle' => $luxuryOptionTitle])->render();
+                        $returnHTML = view('email.newOrderVendorProducts')->with(['cartData' => $cartDetails, 'order' => $order, 'id' => $vendor_id, 'currencySymbol' => $currSymbol, 'luxuryOptionTitle' => $luxuryOptionTitle])->render();
                     }
                     $email_template_content = str_ireplace("{description}",'', $email_template_content);
                     $email_template_content = str_ireplace("{customer_name}", ucwords($user->name), $email_template_content);
@@ -2698,7 +2698,7 @@ class OrderController extends BaseController
                                         if (!in_array($tax_rate_detail->id, $tax_category_ids)) {
                                             $tax_category_ids[] = $tax_rate_detail->id;
                                         }
-                                        $rate = $tax_rate_detail->tax_rate;
+                                        $rate = round($tax_rate_detail->tax_rate);
                                         $tax_amount = ($price_in_dollar_compare * $rate) / 100;
                                         $product_tax = $quantity_price * $rate / 100;
                                         // $taxable_amount = $taxable_amount + $product_tax;
