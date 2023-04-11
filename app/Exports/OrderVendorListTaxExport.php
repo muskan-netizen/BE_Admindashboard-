c387d89913b2ae0ec336a45767b23a09957b64a0
nitish <nitish@codebrewinnovations.com>
2023-04-11 16:17:02 +0530

    Export paltform revenue changes

--- app/Exports/OrderVendorListTaxExport.php
+++ app/Exports/OrderVendorListTaxExport.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use App\Models\ClientPreference;
 
 
 
@@ -60,8 +61,10 @@ class OrderVendorListTaxExport implements FromCollection,WithHeadings,WithMappin
             $vendor_orders = $vendor_orders->where('order_status_option_id',$status);
         }
         $vendor_orders = $vendor_orders->get();  
-
+        $client_preference_detail= ClientPreference::first();
+        
         foreach ($vendor_orders as $vendor_order) {
+            $adminDiscount = 0.00;
             $vendor_order->created_date = dateTimeInUserTimeZone($vendor_order->created_at, $timezone);
             $vendor_order->user_name = $vendor_order->user ? $vendor_order->user->name : '';
             $order_status = '';
@@ -75,9 +78,17 @@ class OrderVendorListTaxExport implements FromCollection,WithHeadings,WithMappin
                 }
             }
             $tip = !empty($vendor_order->orderDetail)?number_format($vendor_order->orderDetail->tip_amount, 2):0.00;
-            
+            if ($vendor_order->coupon_paid_by == 1) {
+                $adminDiscount = $vendor_order->discount_amount;
+            }
             $vendor_order->total_amount = $tip+$vendor_order->payable_amount;
             $vendor_order->order_status = $order_status;
+            $revenue = $vendor_order->admin_commission_percentage_amount + $vendor_order->admin_commission_fixed_amount + $vendor_order->total_markup_price;           
+            if($client_preference_detail->is_tax_price_inclusive){
+                $vendor_order->admin_revenue = ($revenue + $vendor_order->total_container_charges + $vendor_order->service_fee_percentage_amount + $vendor_order->delivery_fee) - $adminDiscount - number_format($vendor_order->orderDetail->loyalty_amount_saved??0.00);                                            
+            }else{
+                $vendor_order->admin_revenue = ($revenue + $vendor_order->taxable_amount +$vendor_order->total_container_charges+  $vendor_order->service_fee_percentage_amount  + $vendor_order->delivery_fee) - $adminDiscount - decimal_format($vendor_order->orderDetail->loyalty_amount_saved??0.00);                                             
+            }
         }
         return $vendor_orders;
     }
@@ -167,7 +178,7 @@ class OrderVendorListTaxExport implements FromCollection,WithHeadings,WithMappin
                 decimal_format($order_vendors->total_amount),
                 $order_vendors->orderDetail ? $order_vendors->orderDetail->loyalty_points_used : '',
                 $order_vendors->orderDetail ? $order_vendors->orderDetail->loyalty_points_earned : '',
-                decimal_format($order_vendors->admin_commission_percentage_amount + $order_vendors->admin_commission_fixed_amount + $order_vendors->service_fee_percentage_amount),
+                decimal_format($order_vendors->admin_revenue),
                 ($order_vendors->orderDetail && $order_vendors->orderDetail->paymentOption) ? $order_vendors->orderDetail->paymentOption->title : '',
                 $order_vendors->order_status,
                 $order_vendors->orderDetail->shipping_delivery_type == 'L' ?'Lalamove' :'Dispatcher',
