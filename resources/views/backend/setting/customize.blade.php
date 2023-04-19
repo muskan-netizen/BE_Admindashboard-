2ac71857fcc40eec21a394931757fc27fe5075a4
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-04-19 14:58:21 +0530

    changes in nomenclature

--- resources/views/backend/setting/customize.blade.php
+++ resources/views/backend/setting/customize.blade.php
@@ -1656,7 +1656,32 @@ $getAdditionalPreference = getAdditionalPreference(['is_phone_signup', 'gtag_id'
                                     <input type="hidden" name="p2p_language_ids[]" value="{{$client_language->langId}}">
                                     <input type="text" name="p2p_id[]" class="form-control al_box_height" value="{{ App\Models\NomenclatureTranslation::getNameBylanguageId($client_language->langId, App\Models\Nomenclature::getIdByName('P2P'))}}">
                                     @if($k == 0)
-                                        @if($errors->has('similar_product.0'))
+                                        @if($errors->has('p2p_id.0'))
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ __("The primary language name field is required.") }}</strong>
+                                            </span>
+                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                      <div class="row mb-2 mx-0 flex-nowrap d-flex align-items-center">
+                            <div class="col-sm-2">
+                                <div class="form-group mb-0">
+                                    <label for="custom_domain">{{ __("Where can we pick you up?") }}</label>
+                                </div>
+                            </div>
+                            @php
+                        
+                            @endphp
+                            @foreach($client_languages as $k => $client_language)
+                            <div class="col-sm-2">
+                                <div class="form-group mb-0">
+                                    <input type="hidden" name="pickup_language_ids[]" value="{{$client_language->langId}}">
+                                    <input type="text" name=pickup_id[]" class="form-control al_box_height" value="{{ App\Models\NomenclatureTranslation::getNameBylanguageId($client_language->langId, App\Models\Nomenclature::getIdByName('Where can we pick you up?'))}}">
+                                    @if($k == 0)
+                                        @if($errors->has('pickup_id.0'))
                                             <span class="text-danger" role="alert">
                                                 <strong>{{ __("The primary language name field is required.") }}</strong>
                                             </span>
