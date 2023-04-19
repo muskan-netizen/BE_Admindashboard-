a5c22958f1e548633706dad043556d3b17070e67
shabnamnazir <shabnam@codebrewinnovations.com>
2023-04-19 18:07:48 +0530

    homepage category limit and get all cats

--- routes/v2/guest.php
+++ routes/v2/guest.php
@@ -17,6 +17,7 @@ Route::group(['prefix' => 'v1/v2', 'middleware' => ['ApiLocalization']], functio
         Route::match(['get','post'],'vendor-optimize-category/{id}', 'Api\v1\v2\VendorController@productsByVendorCategoryOptimize');  
         Route::post('vendor/vendorProductsFilterOptimize', 'Api\v1\v2\VendorController@vendorProductsFilterOptimize');
         Route::post('homepage', 'Api\v1\v2\HomeController@homepage');
+        Route::post('categoriesAll', 'Api\v1\v2\HomeController@categoriesAll');
         Route::post('get_products', 'Api\v1\v2\HomeController@get_spotlight_deals_selected_producst');
         Route::post('search/{type}/{id?}', 'Api\v1\v2\HomeController@globalSearch');
         
