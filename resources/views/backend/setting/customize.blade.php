83ee20fe254869667e38d3b272e78f0ef50c7409
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-04-19 12:54:45 +0530

    Ekobridge issue added p2p to nomenclature

--- resources/views/backend/setting/customize.blade.php
+++ resources/views/backend/setting/customize.blade.php
@@ -1640,6 +1640,32 @@ $getAdditionalPreference = getAdditionalPreference(['is_phone_signup', 'gtag_id'
                             </div>
                             @endforeach
                         </div>
+                        
+                      <div class="row mb-2 mx-0 flex-nowrap d-flex align-items-center">
+                            <div class="col-sm-2">
+                                <div class="form-group mb-0">
+                                    <label for="custom_domain">{{ __("P2P") }}</label>
+                                </div>
+                            </div>
+                            @php
+                        
+                            @endphp
+                            @foreach($client_languages as $k => $client_language)
+                            <div class="col-sm-2">
+                                <div class="form-group mb-0">
+                                    <input type="hidden" name="p2p_language_ids[]" value="{{$client_language->langId}}">
+                                    <input type="text" name="p2p_id[]" class="form-control al_box_height" value="{{ App\Models\NomenclatureTranslation::getNameBylanguageId($client_language->langId, App\Models\Nomenclature::getIdByName('P2P'))}}">
+                                    @if($k == 0)
+                                        @if($errors->has('similar_product.0'))
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ __("The primary language name field is required.") }}</strong>
+                                            </span>
+                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
 
                     </div>
                 </div>
