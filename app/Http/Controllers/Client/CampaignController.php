e3f3471ec968f36e5901902ce0cd667dfe800066
root <ajay.kaundal@codebrewinnovations.com>
2023-04-25 15:31:29 +0530

    campaign function

--- app/Http/Controllers/Client/CampaignController.php
+++ app/Http/Controllers/Client/CampaignController.php
@@ -295,115 +295,85 @@ class CampaignController extends BaseController
             if ($usertype == 1)    // for all users
             {
                 $users = User::where(['status' => 1]);
-                if ($request->type == 1) {
-                    $users->whereNotNull('phone_number')->groupBy('phone_number');
-                }
-                if ($request->type == 2) {
-                    $users->whereNotNull('email')->groupBy('email');
-                }
-                if ($request->type == 3) {
-                    $users->whereHas('device');
-                }
-                $users = $users->get();
-                $getusercount = count($users);
-                $totalbatches = ceil($getusercount / $request->request_user_count);
-                for ($i = 1; $i <= $totalbatches; $i++) {
-                    $usercount = $request->request_user_count;
-                    if ($i == 1) {
-                        $notification_time = $notification_time;
-                    } else {
-                        $notification_time = $notification_time->addMinute($request->request_time_gap);
-                    }
-                    $roasterdata = [];
-                    $conditionalvalue = (($i * $usercount) <= $getusercount) ? $i * $usercount : $getusercount;
-                    for ($j = (($i - 1) * $usercount); $j < $conditionalvalue; $j++) {
-                        if ($request->type == 3) {
-
-                            $getdevicedetails = UserDevice::where('user_id', $users[$j]->id)->get(['device_type', 'device_type']);
-                            if (count($getdevicedetails) > 0) {
-                                foreach ($getdevicedetails as $getdevicedetail) {
-                                    $roasterdata[] = array(
-                                        'campaign_id'   =>  $campaign->id,
-                                        'user_id'   =>  $users[$j]->id,
-                                        'notification_time'   =>  $notification_time,
-                                        'notofication_type'   =>  $notification_type,
-                                        'device_type'   =>  $getdevicedetail->device_type,
-                                        'device_token'   =>  $getdevicedetail->device_token,
-                                        'status'    =>  0
-                                    );
-                                }
-                            }
-                        } else {
-                            $roasterdata[] = array(
-                                'campaign_id'   =>  $campaign->id,
-                                'user_id'   =>  $users[$j]->id,
-                                'notification_time'   =>  $notification_time,
-                                'notofication_type'   =>  $notification_type,
-                                'device_type'   =>  "",
-                                'device_token'   =>  "",
-                                'status'    =>  0
-                            );
-                        }
-                    }
-
-                    $insertroaster = CampaignRoster::insert($roasterdata);
-                }
-                $total_requests = CampaignRoster::where('campaign_id', $campaign->id)->count();
-                Campaign::where('id', $campaign->id)->update(['total_request_count' => $total_requests]);
+                  $rosterData = $this->getRosterData($request, $users, $campaign->id, $notification_time, $notification_type);
             } else {  //for vendors only
-                $vendors = UserVendor::select('user_id')->with('user')->groupBy('user_id')->get();
-                $getusercount = count($vendors);
-                $usercount = $request->request_user_count;
-                $totalbatches = ceil($getusercount / $usercount);
-                for ($i = 1; $i <= $totalbatches; $i++) {
-                    if ($i == 1) {
-                        $notification_time = $notification_time;
-                    } else {
-                        $notification_time = $notification_time->addMinute($request->request_time_gap);
-                    }
-                    $roasterdata = [];
-                    $conditionalvalue = (($i * $usercount) <= $getusercount) ? $i * $usercount : $getusercount;
-                    for ($j = (($i - 1) * $usercount); $j < $conditionalvalue; $j++) {
-                        if ($request->type == 3) {
-                            $getdevicedetails = UserDevice::where('user_id', $vendors[$j]->user_id)->get(['device_type', 'device_type']);
-                            if (count($getdevicedetails) > 0) {
-                                foreach ($getdevicedetails as $getdevicedetail) {
-                                    $roasterdata[] = array(
-                                        'campaign_id'   =>  $campaign->id,
-                                        'user_id'   =>  $vendors[$j]->user_id,
-                                        'notification_time'   =>  $notification_time,
-                                        'notofication_type'   =>  $notification_type,
-                                        'device_type'   =>  $getdevicedetail->device_type,
-                                        'device_token'   =>  $getdevicedetail->device_token,
-                                        'status'    =>  0
-                                    );
-                                }
-                            }
-                        } else {
-                            $roasterdata[] = array(
-                                'campaign_id'   =>  $campaign->id,
-                                'user_id'   =>  $vendors[$j]->user_id,
-                                'notification_time'   =>  $notification_time,
-                                'notofication_type'   =>  $notification_type,
-                                'device_type'   =>  "",
-                                'device_token'   =>  "",
-                                'status'    =>  0
-                            );
-                        }
-                        $insertroaster = CampaignRoster::insert($roasterdata);
-                    }
+                $vendors = User::has('userVendor')->where(['status' => 1]);
+                $rosterData = $this->getRosterData($request, $vendors, $campaign->id, $notification_time, $notification_type);
                 }
-                $total_requests = CampaignRoster::where('campaign_id', $campaign->id)->count();
-                Campaign::where('id', $campaign->id)->update(['total_request_count' => $total_requests]);
-            }
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Campaign created Successfully!',
-                'data' => $campaign
-            ]);
+                if($rosterData){
+                    $total_requests = CampaignRoster::where('campaign_id', $campaign->id)->count();
+                    Campaign::where('id', $campaign->id)->update(['total_request_count' => $total_requests]);
+
+                    return response()->json([
+                        'status' => 'success',
+                        'message' => 'Campaign created Successfully!',
+                        'data' => $campaign
+                    ]);
+                }
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Something went wrong, Please try again!',
+                    'data' => []
+                ]);
+
         }
     }
+    public function getRosterData($request, $users, $campaign_id, $notification_time, $notification_type)
+    {
+        if ($request->type == 1) {
+            $users->whereNotNull('phone_number')->groupBy('phone_number');
+        }
+        if ($request->type == 2) {
+            $users->whereNotNull('email')->groupBy('email');
+        }
+        if ($request->type == 3) {
+            $users->whereHas('device');
+        }
+        $users = $users->pluck('id');
+        $getusercount = count($users);
+        $totalbatches = ceil($getusercount / $request->request_user_count);
+        for ($i = 1; $i <= $totalbatches; $i++) {
+            $usercount = $request->request_user_count;
+            if ($i == 1) {
+                $notification_time = $notification_time;
+            } else {
+                $notification_time = $notification_time->addMinute($request->request_time_gap);
+            }
+            $roasterdata = [];
+              $conditionalvalue = (($i * $usercount) <= $getusercount) ? $i * $usercount : $getusercount;
+            for ($j = (($i - 1) * $usercount); $j < $conditionalvalue; $j++) {
+                if ($request->type == 3) {
+                    $getdevicedetails = UserDevice::where('user_id', $users[$j])->get(['device_token','device_type']);
+                    if (count($getdevicedetails) > 0) {
+                        foreach ($getdevicedetails as $key => $getdevicedetail) {
+                            $roasterdata[] = array(
+                                'campaign_id'   =>  $campaign_id,
+                                'user_id'   =>  $users[$j],
+                                'notification_time'   =>  $notification_time,
+                                'notofication_type'   =>  $notification_type,
+                                'device_type'   =>  $getdevicedetail->device_type,
+                                'device_token'   =>  $getdevicedetail->device_token,
+                                'status'    =>  0
+                            );
+                        }
+                    }
+                } else {
+                    $roasterdata[] = array(
+                        'campaign_id'   =>  $campaign_id,
+                        'user_id'   =>  $users[$j],
+                        'notification_time'   =>  $notification_time,
+                        'notofication_type'   =>  $notification_type,
+                        'device_type'   =>  "",
+                        'device_token'   =>  "",
+                        'status'    =>  0
+                    );
+                }
+            }
 
+            $insertroaster = CampaignRoster::insert($roasterdata);
+        }
+        return $insertroaster;
+    }
     public function GetPushOptions(Request $request)
     {
         $pushoption =  $request->pushvalue;
