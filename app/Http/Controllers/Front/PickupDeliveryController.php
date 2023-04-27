74034c66f69592168e774e36d048fc6ab979fc71
nitish <nitish@codebrewinnovations.com>
2023-04-27 12:37:37 +0530

    pickup issue fixed

--- app/Http/Controllers/Front/PickupDeliveryController.php
+++ app/Http/Controllers/Front/PickupDeliveryController.php
@@ -970,22 +970,15 @@ class PickupDeliveryController extends FrontController{
                     $up_web_hook_code = OrderVendor::where(['order_id' => $order->id,'vendor_id' => $vendor])
                                     ->update(['web_hook_code' => $dynamic,'dispatch_traking_url' => $dispatch_traking_url]);
                     $response['dispatch_traking_url'] = $dispatch_traking_url;
-
-
                     $or_ids = OrderVendor::where(['order_id' => $order->id,'vendor_id' => $vendor])->with(['vendor'])->first();
 
-                     if($or_ids->vendor->auto_accept_order==1){
-                        $update_vendor = VendorOrderStatus::updateOrCreate([
-                            'order_id' =>  $order->id,
-                            'order_status_option_id' => 2,
-                            'vendor_id' =>  $vendor,
-                            'order_vendor_id' =>  $or_ids->id]);
+                    $update_vendor = VendorOrderStatus::updateOrCreate([
+                        'order_id' =>  $order->id,
+                        'order_status_option_id' => 2,
+                        'vendor_id' =>  $vendor,
+                        'order_vendor_id' =>  $or_ids->id]);
 
-                        OrderVendor::where('vendor_id', $vendor)->where('order_id', $order->id)->update(['order_status_option_id' => 2,'dispatcher_status_option_id' => 1]);
-                     }
-                    else {
-                        OrderVendor::where('vendor_id', $vendor)->where('order_id', $order->id)->update(['dispatcher_status_option_id' => 1]);
-                     }
+                    OrderVendor::where('vendor_id', $vendor)->where('order_id', $order->id)->update(['order_status_option_id' => 2,'dispatcher_status_option_id' => 1]);
 
                     $update = VendorOrderDispatcherStatus::updateOrCreate(['dispatcher_id' => null,
                     'order_id' =>  $order->id,
