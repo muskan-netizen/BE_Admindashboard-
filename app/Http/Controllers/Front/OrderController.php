9af6ca367316a78baf689f25e3c5fc6b3c9cb348
nitish <nitish@codebrewinnovations.com>
2023-04-27 10:18:17 +0530

    dunzo api issue fixed

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -2674,7 +2674,7 @@ class OrderController extends FrontController
             $user = User::find($user_id);
         }
         foreach ($order_vendors as $ov) {
-            //// Log::info($ov);
+          Log::info($ov);
             //// Log::info($ov->order_id);
             $request = $ov;
 
@@ -2804,7 +2804,6 @@ class OrderController extends FrontController
         $is_place_order_delivery_zero = getAdditionalPreference([
             'is_place_order_delivery_zero'
         ])['is_place_order_delivery_zero'];
-        // Create Shipping place order request for Dunzo
         $checkdeliveryFeeAdded = OrderVendor::where([
             'order_id' => $request->order_id,
             'vendor_id' => $request->vendor_id
