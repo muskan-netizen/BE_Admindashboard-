a58a6be9812119d7c09a84458cb9a88c2946f47b
nitish <nitish@codebrewinnovations.com>
2023-05-01 10:19:39 +0530

    price and import issue fixed

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -138,7 +138,6 @@ class CategoryController extends FrontController{
         
         $listData = $this->listData($langId, $category->id, $redirect_to,$vendorIds,false);
         $maxPrice = DB::select("SELECT MAX(product_variants.price) as max_price FROM product_variants INNER JOIN products ON products.id = product_variants.product_id WHERE product_variants.status = 1 AND products.is_live = 1 AND products.category_id = ?", [$category->id])[0]->max_price;
-        \Log::info($maxPrice);
         $page = (strtolower($redirect_to) != '') ? strtolower($redirect_to) : 'product';
         // $newProducts =  $this->getNewProducts($vendorIds, $langId, $curId);
         $productAttributes = '';        
