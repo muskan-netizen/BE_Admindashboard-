c65fbf30a8ebc8060d0f779ba07dc0c43faf975c
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-05-05 11:22:50 +0530

    schedule Time Issue in api resolve

--- app/Http/Controllers/Api/v1/PickupDeliveryController.php
+++ app/Http/Controllers/Api/v1/PickupDeliveryController.php
@@ -657,12 +657,20 @@ class PickupDeliveryController extends BaseController{
                     $schedule_datetime_del = Carbon::parse($request->schedule_time, $customer->timezone)->setTimezone('UTC')->format('Y-m-d H:i:s');
                 }
 
-                $task_type = 'now';
-                if($request->has('task_type')){
-                    $task_type = $request->task_type;
-                }elseif(!empty($order->scheduled_date_time)){
-                    $task_type = 'schedule';
+                // $task_type = 'now';
+                // if($request->has('task_type')){
+                //     $task_type = $request->task_type;
+                // }elseif(!empty($order->scheduled_date_time)){
+                //     $task_type = 'schedule';
+                // }
+
+                if(empty($request->task_type) && !empty($request->schedule_time)){
+                    $task_type = 'schedule'; 
+                }else{
+                    $task_type = 'now';  
                 }
+
+
                 $vendor_details = Vendor::where('id', $vendor)->select('order_pre_time')->first();
                 $order_vendor = OrderVendor::where(['order_id' => $order->id,'vendor_id' => $vendor])->first();
                 $dynamic = (!empty($order_vendor->web_hook_code)) ? $order_vendor->web_hook_code : uniqid($order->id.$vendor);
