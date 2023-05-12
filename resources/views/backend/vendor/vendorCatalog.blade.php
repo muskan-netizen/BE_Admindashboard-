7d831210d9ddba07ca806f509208b468d5de84d9
nitish <nitish@codebrewinnovations.com>
2023-05-12 11:32:16 +0530

    column issue in vendor catalog

--- resources/views/backend/vendor/vendorCatalog.blade.php
+++ resources/views/backend/vendor/vendorCatalog.blade.php
@@ -242,7 +242,7 @@ pr($products->toArray());
                                                         <th><input type="checkbox" class="all-product_check"
                                                                 name="all_product_id" id="all-product_check"></th>
                                                         <th>#</th>
-                                                        <th>{{ __('Name 11') }}</th>
+                                                        <th>{{ __('Name') }}</th>
                                                         <th>{{ __('Category') }}</th>
                                                         @if ($client_preference_detail->business_type != 'taxi')
                                                             <th>{{ __('Brand') }}</th>
@@ -760,7 +760,7 @@ pr($products->toArray());
                      <div class="row mb-2">
                          @if ($client_preferences->business_type != 'taxi')
                              <div class="col-md-6 justify-content-between mb-2" id="for_new" style="display:none;">
-                                 {!! Form::label('title', __('New'), ['class' => 'control-label']) !!}
+                                 {!! Form::label('11title', __('New'), ['class' => 'control-label']) !!}
                                  <input type="checkbox" id="is_new" data-plugin="switchery" name="is_new"
                                      class="chk_box" data-color="#43bee1">
                              </div>
@@ -1401,9 +1401,10 @@ pr($products->toArray());
                     {data: 'product_image', name: 'product_image', orderable: false, searchable: false},
                     {data: 'product_name', name: 'product_name', orderable: true, searchable: false},
                     {data: 'product_category', name: 'phone_number', orderable: false, searchable: false},
+                    {data: 'bar_code', name: 'bar_code', orderable: false, searchable: false},
                     {data: 'product_is_live', name: 'product_is_live', orderable: false, searchable: false},
-                    {data: 'action', name: 'action', orderable: false, searchable: false},
-                    {data: 'rental_product_count', name: 'rental_product_count', orderable: false, searchable: false}
+                    {data: 'expiry_date', name: 'expiry_date', orderable: false, searchable: false},
+                    {data: 'action', name: 'action', orderable: false, searchable: false}
                 ];
             }else{
                 return [
