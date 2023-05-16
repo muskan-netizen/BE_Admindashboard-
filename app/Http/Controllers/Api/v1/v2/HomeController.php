c40bed586b0c6ced8201fd90e51030e161d362e3
inderjit <inderjit.singh@codebrewinnovation.com>
2023-05-16 17:50:27 +0530

    done

--- app/Http/Controllers/Api/v1/v2/HomeController.php
+++ app/Http/Controllers/Api/v1/v2/HomeController.php
@@ -480,7 +480,7 @@ class HomeController extends BaseController
             $homeData = ['homePageLabels' => $home_page_labels, 'reqData' => $request->all(), 'selectedAddress' => $selectedAddress, 'latitude' => $latitude, 'longitude' => $longitude, 'enable_layout' => $enable_layout,'image_prefix' => $image_const_arr];
             $homeData['is_admin'] = $user_vendor_count > 0 ? 1 : 0;
             $homeData['mobile_banners'] = $mobile_banners??[];
-            $homeData['vendors'] = $vendorData;
+            $homeData['banners'] = $banners??[];
             $homeData['categories'] = $categories;
             return $this->successResponse($homeData);
         } catch (Exception $e) {
