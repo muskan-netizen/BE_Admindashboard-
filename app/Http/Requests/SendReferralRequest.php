beda12e28dd1984d4090de5084d8e728faf32eed
nitish <nitish@codebrewinnovations.com>
2023-05-24 11:09:34 +0530

    campaign notification issue resolved

--- app/Http/Requests/SendReferralRequest.php
+++ app/Http/Requests/SendReferralRequest.php
@@ -3,8 +3,6 @@
 namespace App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Exceptions\HttpResponseException;
 
 class SendReferralRequest extends FormRequest
 {
