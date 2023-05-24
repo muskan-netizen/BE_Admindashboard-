59243d784145baae2a0de8036028e612ed108ade
nitish <nitish@codebrewinnovations.com>
2023-05-24 12:28:58 +0530

    time format change for moment js param

--- resources/views/frontend/booking/index.blade.php
+++ resources/views/frontend/booking/index.blade.php
@@ -1685,7 +1685,7 @@ input[type=number]::-webkit-outer-spin-button {
                 timePickerIncrement: 1,
                 autoUpdateInput: true,
                 locale: {
-                    format: 'MM-DD-YYYY HH:mm',
+                    format: 'YYYY-MM-DD HH:mm',
                 }
             });
             $('.calendar_icon').click(function() {
