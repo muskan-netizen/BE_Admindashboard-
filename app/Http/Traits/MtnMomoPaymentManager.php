ca001faff0746945f8f84be27273f86c8d88ba24
inderjit <inderjit.singh@codebrewinnovation.com>
2023-05-25 20:04:37 +0530

    done resolved

--- app/Http/Traits/MtnMomoPaymentManager.php
+++ app/Http/Traits/MtnMomoPaymentManager.php
@@ -268,9 +268,8 @@ trait MtnMomoPaymentManager
             'Ocp-Apim-Subscription-Key' => self::$_subscriptionKey,
             'Authorization' => 'Bearer ' . $token,
             'Content-Type' => 'application/json',
-            'X-Callback-Url' => (self::$_isSandbox) ? route('payment.webhook.mtn', []) : route('payment.webhook.mtn', [], true)
+            'X-Callback-Url' => (self::$_isSandbox) ? route('payment.webhook.mtn', []) : url('/payment/webhook/mtn')
             // 'X-Callback-Url' => 'https://webhook.site/8d253f63-1db2-4795-a41c-4dc24902a989'
-
         ];
 
         $env = $data['environment'];
