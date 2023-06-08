a750030e07b7399220305a8392e7a98808a87a5d
inderjit <inderjit.singh@codebrewinnovation.com>
2023-06-08 15:08:21 +0530

    set opay name

--- app/Http/Controllers/Front/giftCard/GiftcardController.php
+++ app/Http/Controllers/Front/giftCard/GiftcardController.php
@@ -121,7 +121,7 @@ class GiftcardController extends FrontController
                 }elseif($payment_option->code == 'authorize_net'){
                     $payment_option->title = __('Credit/Debit Card');
                 }elseif($payment_option->code == 'obo'){
-                    $payment_option->title = __("O'Pay");
+                    $payment_option->title = __("MoMo, Airtel Money, Credit/Debit Cards by O'Pay");
                 }
                 $payment_option->title = __($payment_option->title);
                 unset($payment_option->credentials);
