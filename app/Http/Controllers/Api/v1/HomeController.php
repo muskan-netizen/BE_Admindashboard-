acad44d7ea391728cf84b73541b7e7294fd2585e
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-06-09 14:26:23 +0530

    hotfix done

--- app/Http/Controllers/Api/v1/HomeController.php
+++ app/Http/Controllers/Api/v1/HomeController.php
@@ -66,7 +66,7 @@ class HomeController extends BaseController
                     }
             }
 
-            $getAdditionalPreference = getAdditionalPreference(['advance_booking_amount', 'advance_booking_amount_percentage','update_order_product_price','is_one_push_book_enable', 'is_bid_ride_enable','is_service_product_price_from_dispatch','is_postpay_enable','is_order_edit_enable','is_bid_enable','is_file_cart_instructions','is_cab_pooling','chat_button','call_button','is_user_kyc_for_registration','seller_sold_title','seller_platform_logo','is_service_price_selection','is_particular_driver','is_enable_curb_side']);
+            $getAdditionalPreference = getAdditionalPreference(['advance_booking_amount', 'advance_booking_amount_percentage','update_order_product_price','is_one_push_book_enable', 'is_bid_ride_enable','is_service_product_price_from_dispatch','is_postpay_enable','is_order_edit_enable','is_bid_enable','is_file_cart_instructions','is_cab_pooling','chat_button','call_button','is_user_kyc_for_registration','seller_sold_title','seller_platform_logo','is_service_price_selection','is_particular_driver','is_enable_curb_side','is_enable_variant_set_v2']);
     
             //pr($vendorMode);
             //mohit sir branch code updated by sohail farm meat
@@ -77,7 +77,7 @@ class HomeController extends BaseController
             $homeData['profile']->preferences->call_button = (int) $getAdditionalPreference['call_button'];
             $homeData['profile']->preferences->is_enable_curb_side = (int) $getAdditionalPreference['is_enable_curb_side'];
             $homeData['profile']->preferences->is_user_kyc_for_registration = (int) $getAdditionalPreference['is_user_kyc_for_registration'];
-            $homeData['profile']->preferences->is_enable_variant_set_v2 = (int) @$getAdditionalPreference['is_enable_variant_set_v2'];
+            $homeData['profile']->preferences->is_enable_variant_set_v2 = (int) $getAdditionalPreference['is_enable_variant_set_v2'];
             $homeData['profile']->preferences->rating_check = $preferences->preferences->rating_check;
             //dd($homeData['profile']);
 
