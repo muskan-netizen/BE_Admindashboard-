534dcf20b6f67b444d1b129dea53b906be28a68a
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-06-13 12:54:13 +0530

    fix redirect product issue

--- app/Http/Controllers/Api/v1/v2/VendorController.php
+++ app/Http/Controllers/Api/v1/v2/VendorController.php
@@ -2210,7 +2210,7 @@ class VendorController extends BaseController{
             
             if($vendor->vendor_templete_id == 5){
             
-            $vendor_categories = Category::select('id')->whereHas('vendorCategory',function ($q)use($vid){
+            $vendor_categories = Category::select('categories.id','categories.type_id', 'types.title as redirect_to')->join('types', 'types.id', 'categories.type_id')->whereHas('vendorCategory',function ($q)use($vid){
                 $q->where('vendor_id',$vid)->where('status', 1);
             })->whereHas('data',function ($q)use($vid){
                 $q->where('is_live', 1)->where('vendor_id', $vid);
@@ -2253,7 +2253,6 @@ class VendorController extends BaseController{
                          return $query;
                     });  
                     
-               
                 $listData =  array_values($vendor_categories->toArray());
             }
             else{ 
