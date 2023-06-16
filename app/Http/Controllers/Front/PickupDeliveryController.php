8768b0702ca561ba5ddbe31bf877596d86e83341
inderjit <inderjit.singh@codebrewinnovation.com>
2023-06-16 10:13:52 +0530

    done

--- app/Http/Controllers/Front/PickupDeliveryController.php
+++ app/Http/Controllers/Front/PickupDeliveryController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Api\v1\BaseController;
 use App\Http\Requests\OrderProductRatingRequest;
-use App\Models\{Category,ClientPreference,ClientCurrency,Vendor,ProductVariantSet,Product,SubscriptionInvoicesUser,LoyaltyCard,UserAddress,Order,OrderVendor,OrderProduct,VendorOrderStatus,Client,Promocode,PromoCodeDetail,VendorOrderDispatcherStatus, Payment, Rider, OrderLocations, LuxuryOption, OrderDriverRating, ProductFaq, ProductFaqSelectOption, User, VendorCategory,ClientLanguage, ClientPreferenceAdditional, PaymentOption, PickDropDriverBid, UserBidRideRequest};
+use App\Models\{Category,ClientPreference,ClientCurrency,Vendor,ProductVariantSet,Product,SubscriptionInvoicesUser,LoyaltyCard,UserAddress,Order,OrderVendor,OrderProduct,VendorOrderStatus,Client,Promocode,PromoCodeDetail,VendorOrderDispatcherStatus, Payment, Rider, OrderLocations, LuxuryOption, OrderDriverRating, ProductFaq, ProductFaqSelectOption, User, VendorCategory,ClientLanguage, ClientPreferenceAdditional, PaymentOption, PickDropDriverBid, UserBidRideRequest, UserDevice};
 use App\Http\Traits\{ApiResponser,PaymentTrait};
 use GuzzleHttp\Client as GCLIENT;
 use Illuminate\Support\Facades\Http;
@@ -505,6 +505,15 @@ class PickupDeliveryController extends FrontController{
                     $order_place['data']['user_name'] = $user->email;
                     $order_place['data']['phone_number'] = '+'.$user->dial_code.''.$user->phone_number;
 
+
+                    //Send sendNotificationToCustomer
+                    if (isset($request->schedule_time) && !empty($request->schedule_time))
+                    {
+                        $order_number = $order_place['data']->order_number??$order_place['data']['order_number'];
+                        $device_token = UserDevice::whereUserId($user->id)->orderBy('id','desc')->value('device_token');
+                        sendNotificationToCustomer($device_token,$order_number);
+                    }
+                    
                      //Send message if ride is booked for friend
                     if($request->type == 1 && isset($request->friendPhoneNumber))
                     {
@@ -528,13 +537,21 @@ class PickupDeliveryController extends FrontController{
                     $order_place['data']['user_name'] = $user->email;
                     $order_place['data']['phone_number'] = '+'.$user->dial_code.''.$user->phone_number;
 
+                     //Send sendNotificationToCustomer
+                     if (isset($request->schedule_time) && !empty($request->schedule_time))
+                     {
+                         $order_number = $order_place['data']->order_number??$order_place['data']['order_number'];
+                         $device_token = UserDevice::whereUserId($user->id)->orderBy('id','desc')->value('device_token');
+                         sendNotificationToCustomer($device_token,$order_number);
+                     }
+
+
                      //Send message if ride is booked for friend
                     if($request->type == 1 && isset($request->friendPhoneNumber))
                     {
                         $msg = "Hi ".($request->friendName??'User').", ".$user->name." has booked a ride for you.";
                         $send = $this->sendSms('', '', '', '', $request->friendPhoneNumber, $msg);
                     }
-                    \Log::info($order_place);
                     return  $order_place;
                 }
                 else{
