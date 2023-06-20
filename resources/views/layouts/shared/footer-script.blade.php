a9c2e4c490a68894410de07bcc4723e6bb93d695
nitish <nitish@codebrewinnovations.com>
2023-06-20 18:57:15 +0530

    update status functionality fixed

--- resources/views/layouts/shared/footer-script.blade.php
+++ resources/views/layouts/shared/footer-script.blade.php
@@ -331,7 +331,7 @@ if (Session::has('toaster')) {
 
     });
     @endif
-    $(document).on("click", ".update_order_status", function() {
+    $(document).on("click", ".update-status", function() {
         Swal.fire({
             title: "{{__('Are you Sure?')}}",
             // icon: 'info',
@@ -389,7 +389,7 @@ if (Session::has('toaster')) {
                                 $(this).remove();
                             });
                             setTimeout(function() {
-                                if ($("#received_new_orders").find(".update_order_status").length == 0) {
+                                if ($("#received_new_orders").find(".update-status").length == 0) {
                                     $("#received_new_orders").modal('hide');
                                 }
                             }, 2000);
