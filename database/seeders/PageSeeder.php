f59524c507b801263fd8037b0d612739f8b58325
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-06-22 18:58:39 +0530

    fix seeder issue

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -29,7 +29,7 @@ class PageSeeder extends Seeder{
                 'is_published' => 1, 
                 'page_id' => $page_detail->id, 
                 'type_of_form' => $type[$key],
-                'language_id' => $client_language ? $client_language->language_id : 0, 
+                'language_id' => $client_language ? $client_language->language_id : 1, 
                 'description' => 'We provide Visitors (as defined below) with access to the Website and Registered Members (as defined below) with access to the Platform subject to the following Terms of Use. By browsing the public areas of the Website, you acknowledge that you have read, understood, and agree to be legally bound by these Terms of Use and our Privacy Policy, which is hereby incorporated by reference (collectively, this “Agreement”). If you do not agree to any of these terms, then please do not use the Website, the App, and/or the Platform. We may change the terms and conditions of these Terms of Use from time to time with or without notice to you.', 
             ]);
         }
