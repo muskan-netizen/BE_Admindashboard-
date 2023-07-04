88b991e0e54035d4a8439286b2c75432057fec4e
root <root@ip-172-31-17-91.us-west-2.compute.internal>
2023-07-04 12:29:46 +0000

    add new chages by amit

--- routes/v1/auth.php
+++ routes/v1/auth.php
@@ -53,6 +53,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['ApiLocalization']], function (
         Route::post('chat/vendorUserChatRoom', 'Api\v1\ChatController@vendorUserChatRoom');
         Route::post('chat/userAgentChatRoom', 'Api\v1\ChatController@userAgentChatRoom');
         Route::post('chat/sendNotificationToUser', 'Api\v1\ChatController@sendNotificationToUser');
+        Route::get('chat/s3-sign', 'Api\v1\ChatController@signAws');
 
        // Route::post('category-product-sync-dispatcher', 'Api\v1\DispatcherController@categoryProductSyncDispatcher')->middleware('ConnectDbFromDispatcher');
         Route::post('get-order-panel-detail', 'Api\v1\BaseController@getPanelDetail')->middleware('ConnectDbFromDispatcher');
