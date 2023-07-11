aeb0abebc866e9a3c3ba22c01fa9f3c9ebf8944f
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-07-11 15:19:55 +0530

    fixed categories tag repeat issue

--- app/Http/Controllers/Api/v1/HomeController.php
+++ app/Http/Controllers/Api/v1/HomeController.php
@@ -504,17 +504,23 @@ class HomeController extends BaseController
 
                 $vendor->is_show_category = ($vendor->vendor_templete_id == 2 || $vendor->vendor_templete_id == 4) ? 1 : 0;
 
-                $vendorCategories = VendorCategory::with('category.translation_one')->where('vendor_id', $vendor->id)->where('status', 1)->get();
+                $vendorCategories = VendorCategory::with('category.translation_one')
+                ->where('vendor_id', $vendor->id)
+                ->where('status', 1)
+                ->groupBy('category_id')
+                ->get();
                 $categoriesList = '';
                 foreach ($vendorCategories as $key => $category) {
                     if ($category->category) {
-                        $cat_name = isset($category->category->translation_one) ? $category->category->translation_one->name : $category->category->slug;
+                        $cat_name = isset($category->category->translation_one) ? $category->category->translation_one->name : '';
                         $categoriesList = $categoriesList . $cat_name ?? '';
                         if ($key !=  $vendorCategories->count() - 1) {
                             $categoriesList = $categoriesList . ', ';
                         }
                     }
                 }
+
+              
                 $vendor->categoriesList = $categoriesList;
 
                 $vends[] = $vendor->id;
