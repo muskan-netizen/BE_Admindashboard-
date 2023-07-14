280444cdb021403a7c7da5649de1358d7d6a170b
inderjit <inderjit.singh@codebrewinnovation.com>
2023-07-14 22:41:31 +0530

    done working holoholo

--- app/Http/Controllers/Front/PickupDeliveryController.php
+++ app/Http/Controllers/Front/PickupDeliveryController.php
@@ -532,10 +532,11 @@ class PickupDeliveryController extends FrontController{
                 \Log::info(json_encode($order_place['data']['route']));
 
                 \Log::info('sms In web');
-                \Log::info(json_encode($share_ride_users->phone_number));
+                
                 $share_ride_users = Rider::whereIn('id',$request->share_ride_users)->get();
                  foreach($share_ride_users as $share_ride_users)
                  {
+                    \Log::info(json_encode($share_ride_users->phone_number));
                     $share_ride_users = (object)$share_ride_users;
                     $dialCode = !empty($share_ride_users->dial_code) ? '+91' : $share_ride_users->dial_code;
                     $phone = $share_ride_users->phone_number;
