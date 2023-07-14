5d88b3cb648d16da19e3ad807bc94ced1cb1daaf
inderjit <inderjit.singh@codebrewinnovation.com>
2023-07-14 22:32:26 +0530

    done working holoholo

--- app/Http/Controllers/Front/PickupDeliveryController.php
+++ app/Http/Controllers/Front/PickupDeliveryController.php
@@ -538,9 +538,10 @@ class PickupDeliveryController extends FrontController{
                  {
                     $share_ride_users = (object)$share_ride_users;
                     $dialCode = !empty($share_ride_users->dial_code) ? '+91' : $share_ride_users->dial_code;
+                    $phone = $share_ride_users->phone_number;
                     $msg = "Hi ".($share_ride_users->first_name??'User').", ".$user->name." has booked a ride. Tracking url is ".$order_place['data']['route']??null;
                     \Log::info(json_encode($msg));
-                    $send = $this->sendSms('', '', '', '', $dialCode.$share_ride_users->phone_number, $msg);
+                    $send = $this->sendSms('', '', '', '', $phone, $msg);
                     \Log::info('--response sms--');
                     \Log::info(json_encode($send));
                 }
