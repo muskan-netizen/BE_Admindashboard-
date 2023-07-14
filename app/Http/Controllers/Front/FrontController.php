fa8f2090576de9341b2f601c844b8ee96b3ca3f2
inderjit <inderjit.singh@codebrewinnovation.com>
2023-07-14 22:16:23 +0530

    done working holoholo

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -36,6 +36,8 @@ class FrontController extends Controller
             {
                 if(!empty($client_preference->sms_secret) && !empty($client_preference->sms_from)){
                     $client = new TwilioClient($client_preference->sms_key, $client_preference->sms_secret);
+                    Log::info('SMS twilio respons');
+                    Log::info($client);
                     $send =  $client->messages->create($to, ['from' => $client_preference->sms_from, 'body' => $body]);
                     Log::info('SMS twilio respons');
                     Log::info(json_encode($send));
