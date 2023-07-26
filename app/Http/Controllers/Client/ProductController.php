68b182651073c6cadcb956fe4ed95af8be1e9c67
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2023-07-26 12:46:55 +0530

    fixes

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Storage;
 use App\Http\Traits\ApiResponser;
 use App\Http\Traits\ToasterResponser;
 use App\Http\Traits\SquareInventoryManager;
-use App\Http\Traits\RoadieManager;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\ProductsImport;
 use App\Imports\QrcodesImport;
@@ -23,7 +22,7 @@ use GuzzleHttp\Client as GCLIENT;
 use Carbon\Carbon;
 class ProductController extends BaseController
 {
-    use ApiResponser, SquareInventoryManager, RoadieManager;
+    use ApiResponser, SquareInventoryManager;
     private $folderName = 'prods';
     private $slugIsUnique = true;
     public function __construct()
