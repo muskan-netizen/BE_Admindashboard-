d124d70a2da317d8561a7bd17058f7966fb4a556
inderwadhawan <nitish@codebrewinnovations.com>
2023-08-01 18:19:27 +0530

    error issue fixed

--- app/Http/Controllers/Client/VendorController.php
+++ app/Http/Controllers/Client/VendorController.php
@@ -217,8 +217,8 @@ class VendorController extends BaseController
 
         $vendor_for_pickup_delivery = null;
         $vendor_for_ondemand = null;
-        $vendor_category = VendorCategory::where('vendor_id',$vendors->first()->id);
         if($vendors->isNotEmpty()){
+            $vendor_category = VendorCategory::where('vendor_id',$vendors->first()->id);
             $vendor_for_pickup_delivery = $vendor_category->whereHas('category',function($q){$q->where('type_id',7);})->count();
             $vendor_for_ondemand = $vendor_category->whereHas('category',function($q){$q->where('type_id',8);})->count();
         }
