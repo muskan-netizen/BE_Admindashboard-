56f03a0456980076e803059d26835c8d9a38aa5b
inderwadhs <inderwadhs@gmail.com>
2023-08-07 12:14:49 +0530

    comment table db

--- database/migrations/2023_07_26_064222_alter_table_products.php
+++ database/migrations/2023_07_26_064222_alter_table_products.php
@@ -14,9 +14,9 @@ class AlterTableProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->decimal('height', 10, 4)->nullable();
-            $table->decimal('breadth', 10, 4)->nullable();
-            $table->decimal('length', 10, 4)->nullable();
+            // $table->decimal('height', 10, 4)->nullable();
+            // $table->decimal('breadth', 10, 4)->nullable();
+            // $table->decimal('length', 10, 4)->nullable();
         });
     }
 
