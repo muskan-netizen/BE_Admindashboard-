d22817b12616d59a62d036ab218307d91b1764df
YograjCbl <yograjy@codebrewinnovations.com>
2023-08-07 20:11:38 +0530

    css changes production

--- resources/views/frontend/vendor-products.blade.php
+++ resources/views/frontend/vendor-products.blade.php
@@ -72,7 +72,7 @@ $currentPage = $_GET['page']??1;
                             }
                             @endphp
                             @if($slug)
-                        <div class="collection-collapse-block border-0 mb-2 open pt-2 pb-0 border-0">
+                        <div class="collection-collapse-block border-0 mb-2 open pt-2 border-0">
                            
                             <h3 class="collapse-block-title"> {{$slug . $sets->title}}</h3>
                             <div class="collection-collapse-block-content">
