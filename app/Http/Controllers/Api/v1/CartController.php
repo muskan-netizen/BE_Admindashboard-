3d712dadcb5c108927d0dcf3ae44d6428ea27d61
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-08-08 18:19:37 +0530

    add_to_cart issue

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -660,7 +660,8 @@ class CartController extends BaseController
     /**         *      Cart  Date      *          */
     public function getCart($cart, $langId = '1', $currency = '1', $type = 'delivery',$code = 'D')
     {
-
+           \Log::info('here');
+           \Log::info($cart);
         try{
         $container_charges_tax = 0;
         $deliver_fee_charges_tax = 0;
