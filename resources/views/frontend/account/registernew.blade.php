c169f3e893a0d03140c97e1357240849a6436103
inderwadhawan <nitish@codebrewinnovations.com>
2023-08-08 18:56:24 +0530

    validation issue fixed

--- resources/views/frontend/account/registernew.blade.php
+++ resources/views/frontend/account/registernew.blade.php
@@ -330,7 +330,7 @@
                 @endif
             @endif
             jQuery.validator.addMethod("alphanumeric", function(value, element) {
-                return this.optional(element) || /^[a-zA-Z0-9]+$/i.test(value);
+                return this.optional(element) || /^[a-zA-Z0-9 ]+$/i.test(value);
             }, "Name should contains alphanumeric data.");
             $("#register").validate({
                 errorClass: 'errors',
