dd6539623f5a0e4826eebbf86162dd71c42ca7cd
Yogesh Rana <yogesh.rana@codebrewinnovations.com>
2023-08-09 12:56:56 +0530

    add is_selected key in allergic item api

--- app/Models/AllergicItem.php
+++ app/Models/AllergicItem.php
@@ -4,16 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class AllergicItem extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+    protected $appends = ['is_selected'];
 
     public function users()
     {
         return $this->belongsToMany(User::class, 'user_allergic_items', 'allergic_item_id','user_id');
     }
 
+    public function getIsSelectedAttribute()
+    {
+        if (UserAllergicItem::where(['user_id' => Auth::id(), 'allergic_item_id' => $this->id])->exists()) {
+            return 1;
+        }
+        return 0;
+    }
 }
