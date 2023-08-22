ed0fe46b915c7427c3937090aea20a9785dccd55
inderwadhawan <nitish@codebrewinnovations.com>
2023-08-22 20:45:48 +0530

    promocode issue fixed

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -899,6 +899,7 @@ class CartController extends BaseController
                 $coupon_vendor_ids = [];
                 $coupon_product_discount = 0;
                 $in_or_not = 0;
+                $rate = 0;
                 if (isset($vendorData->coupon) && !empty($vendorData->coupon) && isset($vendorData->coupon->promo) && !empty($vendorData->coupon->promo)){                  
                     if($vendorData->coupon->promo->restriction_on == 0)
                     { 
@@ -1387,7 +1388,7 @@ class CartController extends BaseController
                             }
                         }
                     }
-                    if(isset($rate) && $discount_amount > 0 ){
+                    if($rate > 0 && $discount_amount > 0 ){
                         $discount = ($discount_amount*$rate) / 100;
                         $taxable_amount -= $discount;
                     }
