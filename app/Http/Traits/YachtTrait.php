c54b6d8998571e6f10f2961b8403b6d29cba9176
Gaurav Bhatia <gaurav.bhatia@codebrewinnovations.com>
2023-08-23 15:16:58 +0530

    Fixed issue for duplicate booking

--- app/Http/Traits/YachtTrait.php
+++ app/Http/Traits/YachtTrait.php
@@ -50,7 +50,12 @@ trait YachtTrait
                         // $q->where('pickup_time', '<=', $pickup_time)
                         // ->where('drop_time', '>=', $drop_time);
                     }
-                })->whereDoesntHave('productBooked')
+                })->whereDoesntHave('productBooked', function($q){
+                    if (!empty($pickup_time) && !empty($drop_time)) {
+                        $q->where('start_date_time', '<=', $pickup_time)
+                        ->where('end_date_time', '>=', $drop_time);
+                    }
+                })
                 ->where(function ($q) use ($request) {
                     if ($request->has('seats') && !empty($request->seats)) {
                         $q->whereHas('ProductAttribute', function ($q) use ($request) {
