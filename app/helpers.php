75fc847bc911234528a52868719da1b669be780f
inderwadhs <inderwadhs@gmail.com>
2023-08-25 19:32:17 +0530

    done

--- app/helpers.php
+++ app/helpers.php
@@ -62,14 +62,14 @@ if (!function_exists('getAdditionalPreference')) {
      * @param  mixed $key
      * @return void
      */
-    function getAdditionalPreference($key=array()){
+    function getAdditionalPreference($key=array() , $time = '60'){
         setUserCode();
         $return = [];
         $dbreturn= [];
         if(sizeof($key)){
             // $result = ClientPreferenceAdditional::select('key_name','key_value')->whereIn('key_name',$key)->get();
             $cacheKey = 'client_preferences_additional_'.json_encode($key);
-            $result = Cache::remember($cacheKey, 60, function () use ($key) {
+            $result = Cache::remember($cacheKey, $time, function () use ($key) {
                 return ClientPreferenceAdditional::select('key_name','key_value')->whereIn('key_name',$key)->get();
             });
             $return = array_column($result->toArray(), 'key_value', 'key_name');
