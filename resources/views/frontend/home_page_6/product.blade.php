aa72199084eaf0bdb485afcd1912967bc3f2498c
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-09-01 16:40:33 +0530

    Logs issue resolve

--- resources/views/frontend/home_page_6/product.blade.php
+++ resources/views/frontend/home_page_6/product.blade.php
@@ -5,7 +5,9 @@
        <label for="fav_pro_one"><i class="fa fa-heart-o fav-heart" aria-hidden="true"></i></label>
    </div>--}}
    <a class="common-product-box text-center" href="{{ @$product->vendor_slug }}/product/{{ @$product->url_slug }}">
+    @if(!empty($product->path)){
        <div class="img-outer-box position-relative"> <img class="blur-up lazyload" data-src="{{ get_file_path(@$product->path,'FILL_URL','260','260') }}" alt="" title="">
+       }
            <div class="pref-timing"> </div>
        </div>
        <div class="media-body align-self-start">
