cf2fa53c0ed809cec1f99cf11ec74578c993d0cd
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-09-01 15:26:17 +0530

    RajatDevPreProduction

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -82,22 +82,23 @@ span.nodatafound {font-size: 120% !important;border: 1px solid #FC0;background: 
                 @if(auth()->user()->is_superadmin)
                 <div class="col-sm">
                     <!-- Customer box start here -->
-                    <div class="card alDasBoxItems">
+                    <div class="card alDasBoxItems"><a href="{{route('customer.index')}}">
                         <div class="card-body">
                             <div class="float-right">
                                 <i class="mdi mdi-account-multiple widget-icon"></i>
                             </div>
-                            <h5 class="" title="Number of Customer">{{ __('Customerssss') }}</h5>
+                            <h5 class="" title="Number of Customer">{{ __('Customer') }}</h5>
                             <h3 class="mt-3 mb-3" id="total_customers">0</h3>
                             <p class="mb-0" id="customers_change">
                             </p>
                         </div>
+                    </a>
                     </div><!-- Customer box end here -->
                 </div>
                 @endif
                 <div class="col-sm">
                     <!-- Orders box start here -->
-                    <div class="card alDasBoxItems">
+                    <div class="card alDasBoxItems"> <a href="{{route('order.index')}}">
                         <div class="card-body">
                             <div class="float-right">
                                 <i class="mdi mdi-cart-plus widget-icon"></i>
@@ -107,13 +108,14 @@ span.nodatafound {font-size: 120% !important;border: 1px solid #FC0;background: 
                             <p class="mb-0" id="orders_change">
                             </p>
                         </div>
+                    </a>
                     </div><!-- Orders box end here -->
                 </div>
 
                 <div class="col-sm">
                     @if(auth()->user()->is_superadmin)
                     <!-- Orders box start here -->
-                    <div class="card alDasBoxItems">
+                    <div class="card alDasBoxItems"> <a href="{{route('vendor.index')}}">
                         <div class="card-body">
                             <div class="float-right">
                                 <i class="mdi mdi-account-multiple widget-icon"></i>
@@ -123,6 +125,7 @@ span.nodatafound {font-size: 120% !important;border: 1px solid #FC0;background: 
                             <p class="mb-0" id="orders_vendor">
                             </p>
                         </div>
+                    </a>
                     </div><!-- Orders box end here -->
                     @endif
                 </div>
@@ -176,6 +179,7 @@ span.nodatafound {font-size: 120% !important;border: 1px solid #FC0;background: 
                             <p class="mb-0" id="products_change">
                             </p>
                         </div>
+                    </a>
                     </div><!-- Growth box end here -->
                 </div>
                 @if(auth()->user()->is_superadmin)
