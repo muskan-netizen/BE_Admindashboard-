c3529af9763b1c03ae9ce32b68d8719693025dd7
inderwadhawan <nitish@codebrewinnovations.com>
2023-09-19 20:19:09 +0530

    enqury functionality done

--- app/Http/Controllers/Api/v1/v2/VendorController.php
+++ app/Http/Controllers/Api/v1/v2/VendorController.php
@@ -2292,7 +2292,7 @@ class VendorController extends BaseController{
                     ->select('products.id', 'products.sku', 'products.requires_shipping', 'products.sell_when_out_of_stock', 
                     'products.url_slug', 'products.weight_unit', 'products.weight', 'products.vendor_id', 'products.has_variant',
                      'products.has_inventory', 'products.Requires_last_mile', 'products.averageRating', 'products.category_id', 'products.minimum_order_count', 
-                     'products.batch_count',DB::raw("'$multipli' as variant_multiplier"),'products.is_show_dispatcher_agent', 'products.is_slot_from_dispatch', 'products.mode_of_service','products.tags','products.is_recurring_booking')->distinct()
+                     'products.batch_count',DB::raw("'$multipli' as variant_multiplier"),'products.is_show_dispatcher_agent', 'products.is_slot_from_dispatch', 'products.mode_of_service','products.tags','products.is_recurring_booking','products.inquiry_only')->distinct()
                     ->where('products.vendor_id', $vid)
                     ->where('products.is_live', 1)->withCount(['variantSet','addOn']);
                     
