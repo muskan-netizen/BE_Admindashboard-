af1a0d8df602987412c38351b2d36e7a29e7a94e
CBL-PC-VIKAS <vikasrai@codebrewinnovations.com>
2023-10-12 13:01:22 +0530

    New changes with agent Slots - 12-10-2023

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -284,6 +284,7 @@ class ProductController extends BaseController
 
         $otherProducts                      = Product::with('primary')->select('id', 'sku')->where('is_live', 1)->where('id', '!=', $product->id)->where('vendor_id', $product->vendor_id)->get();
         $configData                         = ClientPreference::select('celebrity_check', 'pharmacy_check', 'need_dispacher_ride', 'need_delivery_service', 'enquire_mode','need_dispacher_home_other_service','delay_order','product_order_form','business_type','minimum_order_batch','age_restriction_on_product_mode','need_appointment_service')->first();
+
         $celebrities                        = Celebrity::select('id', 'name')->where('status', '!=', 3)->get();
         $configData->is_cab_pooling         = $getAdditionalPreference['is_cab_pooling'];
         $configData->is_one_push_book_enable= $getAdditionalPreference['is_one_push_book_enable'];
