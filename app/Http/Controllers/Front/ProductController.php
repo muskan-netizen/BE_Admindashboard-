9c8ec8bfa0a30ab2e16e65f2cecd4bf8ab7fca6b
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-10-12 13:26:48 +0530

    check

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -390,7 +390,7 @@ class ProductController extends FrontController{
                 $current_time_response = true;
             }
 
-            
+            pr($product_page);
             return view('frontend.'.$product_page)->with(['user_vendor' => $user_vendor, 'shareComponent' => $shareComponent, 'sets' => $sets, 'vendor_info' => $vendor, 'product' => $product, 'navCategories' => $navCategories, 'newProducts' => $newProducts, 'rating_details' => $rating_details, 'is_inwishlist_btn' => $is_inwishlist_btn, 'category' => $category, 'product_in_cart' => $product_in_cart,'is_available'=>$is_available, 'getAdditionalPreference' => $getAdditionalPreference, 'suggested_category_products' => $suggested_category_products, 'suggested_brand_products'=> $suggested_brand_products, 'suggested_vendor_products'=>$suggested_vendor_products, 'coupon_list' => $coupon_list, 'attr_array' => $attr_array, 'set_template' => $set_template, 'current_time_response' => $current_time_response, 'processorProduct'=> $processorProduct, 
             'product_notavailability' => $product_notavailability,
             'product_availability' => $product_availability]);
