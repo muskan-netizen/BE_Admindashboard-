c68282e592ddfc32fb284c3aa93b968f141e9cd2
Rajat Kumar <rajat.k@codebrewinnovations.com>
2023-10-12 13:00:58 +0530

    changes related to pickupup delivery to get agents

--- app/Http/Controllers/Api/v1/PickupDeliveryController.php
+++ app/Http/Controllers/Api/v1/PickupDeliveryController.php
@@ -967,7 +967,6 @@ class PickupDeliveryController extends BaseController{
             }
 
             $dispatch_domain = $this->checkIfPickupDeliveryOn();
-              \Log::info($dispatch_domain);
             $header = ['headers' => ['personaltoken' => !empty($dispatch_domain->pickup_delivery_service_key)? $dispatch_domain->pickup_delivery_service_key : "",
                 'shortcode' => !empty($dispatch_domain->delivery_service_key_code)? $dispatch_domain->delivery_service_key_code : $dispatch_domain->pickup_delivery_service_key_code,
                 'content-type' => 'application/json']
