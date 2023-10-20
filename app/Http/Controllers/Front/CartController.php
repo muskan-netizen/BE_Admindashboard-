b9971466a8ebec5dc133312d27b2c34997dc1e18
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-10-20 16:27:09 +0530

    fixed order calculations in invoice and accounting

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -1669,7 +1669,7 @@ class CartController extends FrontController
                 $cart->without_category_kyc = 1;
             }
 
-            pr($cart);
+            
             // echo "Total_payable_amount: ".$total_payable_amount."total_discount_amount: ". $total_discount_amount."loyalty_amount_saved". $loyalty_amount_saved ."wallet_amount_used".$wallet_amount_used."total_taxable_amount".$total_taxable_amount;
             // Total_payable_amount: 695.6total_discount_amount: 97.1loyalty_amount_saved83.4wallet_amount_used0total_taxable_amount102
             //pr($total_payable_amount);
