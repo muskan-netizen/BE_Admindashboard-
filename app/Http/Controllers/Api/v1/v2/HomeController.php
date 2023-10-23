4e14ad34333e9fcaf6877d4b59474e8e93a0ddc3
Yogesh Rana <yogesh.rana@codebrewinnovations.com>
2023-10-23 19:41:00 +0530

    vendor rating filter on homepage api

--- app/Http/Controllers/Api/v1/v2/HomeController.php
+++ app/Http/Controllers/Api/v1/v2/HomeController.php
@@ -1592,6 +1592,9 @@ class HomeController extends BaseController
             }
         }
 
+        if ($request->rating) {
+            $preferences->rating = $request->rating;
+        }
         
         if(count($vendor_ids) > 0){
             $vendors = $this->getVendorForHomePage($preferences, "random_or_admin_rating", $timezone, $additionalPreference['is_admin_vendor_rating'], $request->type, $language_id, $latitude, $longitude, $vendor_ids,null,$this->venderFilterOpenClose,$this->venderFilterbest,$request->nearest_vendor);
