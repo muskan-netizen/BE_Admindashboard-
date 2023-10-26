d448395eae4138fe11e7944ef6eb9c3df87b02bc
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-10-26 11:52:29 +0530

    changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1039,21 +1039,21 @@ $(document).ready(function () {
             return false;
         }
        
-        if (typeof action_type !== "undefined" && action_type === "car_rental") {
+     
 
-            var checkboxes = $('.checked-cart-product');
-            var checkedCheckboxes = checkboxes.filter(':checked');
-            if (checkedCheckboxes.length === 0) {
+            // var checkboxes = $('.checked-cart-product');
+            // var checkedCheckboxes = checkboxes.filter(':checked');
+            // if (checkedCheckboxes.length === 0) {
                 
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Please select at least one product.',
-                    //footer: '<a href="">Why do I have this issue?</a>'
-                });
-                return false;
-            }
-        }
+            //     Swal.fire({
+            //         icon: 'error',
+            //         title: 'Oops...',
+            //         text: 'Please select at least one product.',
+            //         //footer: '<a href="">Why do I have this issue?</a>'
+            //     });
+            //     return false;
+            // }
+        
       
 
         if ($("#agree_term_check").length > 0) {
