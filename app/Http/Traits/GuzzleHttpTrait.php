531aca4d73cb7406a2cd4645975f8ee91a4712f0
inderwadhs <inderwadhs@gmail.com>
2023-10-30 19:46:37 +0530

    done

--- app/Http/Traits/GuzzleHttpTrait.php
+++ app/Http/Traits/GuzzleHttpTrait.php
@@ -11,7 +11,7 @@ trait GuzzleHttpTrait{
         try {
             $client = new Guzzle(['headers' => ['personaltoken' => $dispatch_domain->pickup_delivery_service_key,'shortcode' => $dispatch_domain->pickup_delivery_service_key_code,'content-type' => 'application/json']]);
             $url = $dispatch_domain->pickup_delivery_service_key_url;
-            $res = $client->post($url.$endPoints,['form_params' => ($postdata),'timeout'=>'10']);
+            $res = $client->post($url.$endPoints,['form_params' => ($postdata),'timeout'=>'50']);
             $response = json_decode($res->getBody(), true);
 
             // You can process the response further based on the $statusCode and $responseBody
