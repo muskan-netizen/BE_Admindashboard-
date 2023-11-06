eb8dfe132ba73c0d18d2956cccafd32ece305272
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-11-06 17:23:05 +0530

    changes

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -502,7 +502,7 @@ class FrontController extends Controller
 
     public function loadDefaultImage(){
         $proxy_url = \Config::get('app.IMG_URL1');
-        $image_path = \Config::get('app.IMG_URL2').'/'.\Storage::disk('s3')->url('default/default.png');
+        $image_path = \Config::get('app.IMG_URL2').'/'.\Storage::disk('s3')->url('default/default_image.png');
         $image_fit = \Config::get('app.FIT_URl');
         $default_url = $image_fit .'300/300'. $image_path.'@webp';
        
