b9e6de21af2c6fbad830d201fca74db40edee27b
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-11-17 16:22:13 +0530

    production changes

--- resources/views/frontend/vendor-category-topbar-banner.blade.php
+++ resources/views/frontend/vendor-category-topbar-banner.blade.php
@@ -1,29 +1,40 @@
  
 @if (@$vendor->banner)
-    $url = $vendor->banner['image_fit'] . '1920/1080' . @$vendor->banner['image_path'];
+@php 
+$url = $vendor->banner['image_fit'] . '1920/1080' . @$vendor->banner['image_path'];
     if(!imageExists($url))
     {
       $url = loadDefaultImage();   
      }    
+
+@endphp
     <div class="common-banner my-banner"><img class="blur-up lazyload" alt="" data-src="{{ $url ?? ''}}" >
 
 
-</div>
+    </div>
 @elseif(@$brand)
+@php 
+
 $url = $brand->image_banner['image_fit'] . '1920/1080' . @$brand->image_banner['image_path'];
 if(!imageExists($url))
 {
   $url = loadDefaultImage();   
 
 }
+@endphp
+
     <div class="common-banner my-banner"><img class="blur-up lazyload" alt="" data-src="{{$brand->image_banner['image_fit'] . '1920/1080' . $brand->image_banner['image_path']}}" ></div>
 @elseif(@$celebrity)
+@php
+
 $url = $celebrity->avatar['image_fit'] . '1920/1080' . @$celebrity->avatar['image_path'];
 if(!imageExists($url))
 {
   $url = loadDefaultImage();   
 
 }
+
+@endphp
     <div class="common-banner my-banner"><img class="blur-up lazyload" data-src="{{$celebrity->avatar['image_fit'] . '1920/1080' . $celebrity->avatar['image_path']}}" alt=""></div>
 @elseif(@$category)
 
