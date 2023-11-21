02b812fec5bae7bab78c4e3b8211189be1801f4e
Gagandeep Singh <gagandeep.singh@codebrewinnovations.com>
2023-11-21 16:52:20 +0530

    p2p production  nine template add post  design issues fix 21/11/2023

--- resources/views/frontend/template_nine/posts/add_post_rental.blade.php
+++ resources/views/frontend/template_nine/posts/add_post_rental.blade.php
@@ -52,7 +52,8 @@
                                         @endif
                                     </ul>
                                 </div>
-                                <ul class=" p-0 m-0 no-gutters view-rental_cats d-none">
+                                <ul class=" p-0 m-0 no-gutters view-rental_cats d-none  ">
+                                    <div class="category_responsive">
                                     @if (@$categories)
                                         @foreach ($categories as $key => $category)
                                             @php  $icon = $category['icon']['proxy_url'] . '200/200' . $category['icon']['image_path'];  @endphp
@@ -70,8 +71,10 @@
                                             @endif
                                         @endforeach
                                     @endif
+                                    </div>
                                 </ul>
-                                <ul class=" p-0 m-0 no-gutters   view-p2psell_cats d-none">
+                                <ul class=" p-0 m-0 no-gutters   view-p2psell_cats d-none ">
+                                    <div class="category_responsive">
                                     @if (@$categories)
                                         @foreach ($categories as $key => $category)
                                             @php  $icon = $category['icon']['proxy_url'] . '200/200' . $category['icon']['image_path'];  @endphp
@@ -89,6 +92,7 @@
                                             @endif
                                         @endforeach
                                     @endif
+                                    </div>
                                 </ul>
                                 <label class="cat-error text-danger mt-2 pl-1 d-none">Please select category.</label>
                             </div>
@@ -411,6 +415,7 @@
             switch (value) {
                 case '10':
                 $viewRentalCats.removeClass('d-none');
+                
                 break;
                 case '13':
                 $viewP2PSellCats.removeClass('d-none');               
@@ -420,6 +425,7 @@
                 $viewAllCats.removeClass('d-none');
                 break;
             }
+            $('.category_responsive').slick('refresh');
             $categoryID.val('');
             $selectedCategory.text('');
             $p2pCategoryForm.addClass('d-none');
