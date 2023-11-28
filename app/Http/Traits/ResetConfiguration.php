f211e15365a70a7b04b40697d9812defe44cea8e
Gaurav Sandhu <gaurav.sandhu@codebrewinnovations.com>
2023-11-28 18:30:08 +0530

    changes

--- app/Http/Traits/ResetConfiguration.php
+++ app/Http/Traits/ResetConfiguration.php
@@ -43,9 +43,12 @@ trait ResetConfiguration
     {
 
         $client_preference = ClientPreference::select('business_type')->first();
+        $client_preference = $client_preference->fresh();
+
         $client_preference->hide_order_address = 1;
         $client_preference->celebrity_check = 0;
         $client_preference->is_hyperlocal = 0;
+        $client_preference->save();
 
 
         $vendor_type =  ["dinein_check", "takeaway_check", "delivery_check", "rental_check", "pick_drop_check", "on_demand_check", "laundry_check", "appointment_check", "p2p_check"];
