39d6cb00c8bfdb58664406a98427b9be8d8bcf90
harjot <harjot@gmail.com>
2023-12-20 14:35:14 +0530

    name changed from obo to opay

--- app/Http/Controllers/Front/OboPaymentController.php
+++ app/Http/Controllers/Front/OboPaymentController.php
@@ -47,8 +47,6 @@ class OboPaymentController extends Controller
                 $token = $tokenData['token'];
                 if (isset($token)) {
                     // user details
-                    \Log::info('$tokenData');
-                    \Log::info($tokenData);
                     $user = auth()->user();
                     $userEmail       = $user->email;
                     $userPhone       = $user->dial_code . $user->phone_number;
@@ -89,16 +87,9 @@ class OboPaymentController extends Controller
                         "return_url"    => url('after-payment/obo' . '?' . $urlParams),
                         "custom_pg_id"  => $this->obo_market_place_id,
                     ], JSON_UNESCAPED_SLASHES);
-                    \Log::info('$input');
-                    \Log::info($input);
-                    \Log::info($apiUrl);
                     $responce = Http::withBody($input, 'application/json')->withHeaders($header)->post($apiUrl);
                     $responceData = json_decode($responce->body(), true);
-                    \Log::info('$responce');
-                    \Log::info($responce);
                     if (isset($responceData['status']) && $responceData['status'] ===  "200") {
-                    \Log::info('$responce 4');
-
                         $redirectUrl =  $responceData['data']['url'];
                         return response()->json([
                             'status' => 'Success',
