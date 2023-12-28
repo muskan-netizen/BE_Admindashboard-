509b7c72febb82f5cb255e46ee978b847e74de2b
inderwadhs <inderwadhs@gmail.com>
2023-12-28 20:30:45 +0530

    done solve issue dispatch

--- app/Http/Controllers/Front/UserhomeController.php
+++ app/Http/Controllers/Front/UserhomeController.php
@@ -276,18 +276,18 @@ class UserhomeController extends FrontController
                     $showTag = implode(',', $tag);
                     $client = Client::with('country')->first();
                     $docs = $this->driverDocuments();
-                    
+                    // \Log::info('docs');
+                    // \Log::info($docs);
                     // \Log::info([$docs->documents]);
                     $driver_registration_documents = [];
-                    // if(is_array($docs) && count($docs)>0){
+                    if(is_array($docs) && count($docs)>0){
                         $driverDocs = json_decode($docs, true);
-
-                        $driver_registration_documents = $driverDocs['documents'];
-                        foreach ($driver_registration_documents as $key => $doc) {
-                            $name = str_replace(" ", "_", $doc['name']);
-                            // $doc->slug = $name;
+                        $driver_registration_documents = $driverDocs->documents;
+                        foreach ($driverDocs->documents as $key => $doc) {
+                            $name = str_replace(" ", "_", $doc->name);
+                            $doc->slug = $name;
                         }
-                    // }
+                    }
                 $teams = @$driverDocs->all_teams??[];
                 $tags = @$driverDocs->agent_tags??[];
                 return view('frontend.driver-registration', compact('page_detail', 'navCategories', 'user', 'showTag', 'driver_registration_documents','client', 'teams', 'tags'));
