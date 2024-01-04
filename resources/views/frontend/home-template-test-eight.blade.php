d597f98fbe73e792b65d26c07c9d3d1a318a23f5
Sanjay Kumar <sanjay.kumar@codebrewinnovations.com>
2024-01-04 16:58:04 +0530

    add home page label banner url

--- resources/views/frontend/home-template-test-eight.blade.php
+++ resources/views/frontend/home-template-test-eight.blade.php
@@ -831,7 +831,14 @@
 									$video_extensions = ['mp4', 'avi', 'mov', 'wmv']; // list of video extensions
 								@endphp
 								@if(in_array($extension, $image_extensions))
-									<img alt="" title="" class="blur-up lazyload w-100" src="{{$homePageData['banners'][$homePageLabel->translations->first()->cab_booking_layout_id]}}" height="300">	
+									@php
+										if(!empty($homePageData['banners']['url_'.optional($homePageLabel->translations->first())->cab_booking_layout_id])){
+											$url = $homePageData['banners']['url_'.optional($homePageLabel->translations->first())->cab_booking_layout_id];
+										}else{
+											$url = 'javascript:void(0)';
+										}
+									@endphp
+									<a href="{{$url}}"><img alt="" title="" class="blur-up lazyload w-100" src="{{$homePageData['banners'][$homePageLabel->translations->first()->cab_booking_layout_id]}}" height="300"></a>	
 								@elseif (in_array($extension, $video_extensions))
 									<video id="video1" width="100%" controls autoplay muted>
 										<source src="{{$homePageData['banners'][$homePageLabel->translations->first()->cab_booking_layout_id]}}" type="video/mp4">
