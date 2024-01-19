c1b42c5bf5c93bfa17f7a9b5d8059f53789050e6
harjot <harjot@gmail.com>
2024-01-19 18:50:38 +0530

    changes

--- app/Http/Controllers/Front/CategoryController.php
+++ app/Http/Controllers/Front/CategoryController.php
@@ -412,6 +412,7 @@ class CategoryController extends FrontController{
 
     public function listData($langId, $category_id, $type = '',$vendorIds = array(),$is_max = false){
 
+        Session::put('cus_paginate', 12);
         $pagiNate = (Session::has('cus_paginate')) ? Session::get('cus_paginate') : 12;
         $vendorType = Session::get('vendorType');
 
