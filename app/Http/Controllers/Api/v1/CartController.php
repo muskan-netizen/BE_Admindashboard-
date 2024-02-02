87b7a9a97b58182b30f8ced5db06feb8896950df
root <ajay.kaundal@codebrewinnovations.com>
2024-02-02 15:30:26 +0530

    tip calculation

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -1916,7 +1916,13 @@ class CartController extends BaseController
         if(@$rental_price){
             $cart->total_payable_amount = $rental_price;
         }
+        $total_payable_amount_calc_tip = $cart->total_payable_amount - $cart->total_tax + $loyalty_amount_saved;
 
+        $cart->tip = array(
+            ['label' => '5%', 'value' => decimal_format(0.05 * $total_payable_amount_calc_tip)],
+            ['label' => '10%', 'value' => decimal_format(0.1 * $total_payable_amount_calc_tip)],
+            ['label' => '15%', 'value' => decimal_format(0.15 * $total_payable_amount_calc_tip)]
+        );
         $wallet_amount_used = 0;
         if (isset($user)) {
             if ($user->balanceFloat > 0) {
@@ -1989,13 +1995,7 @@ class CartController extends BaseController
             $cart->total_payable_amount = $rental_price;
         }
 
-        $total_payable_amount_calc_tip = $cart->total_payable_amount - $total_taxable_amount - $cart->total_tax + $cart->wallet_amount_used;
-
-        $cart->tip = array(
-            ['label' => '5%', 'value' => decimal_format(0.05 * $total_payable_amount_calc_tip)],
-            ['label' => '10%', 'value' => decimal_format(0.1 * $total_payable_amount_calc_tip)],
-            ['label' => '15%', 'value' => decimal_format(0.15 * $total_payable_amount_calc_tip)]
-        );
+    
         return $cart;
 
 
