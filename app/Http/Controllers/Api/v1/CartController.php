d4f5ee88e20513627fc1363da9403dae0241dff5
inderwadhs <inderwadhs@gmail.com>
2024-02-07 15:24:45 +0530

    done fix addon price discount in cart

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -678,19 +678,44 @@ class CartController extends BaseController
         $cart->delete();
         return response()->json(['message' => __('Empty cart successfully.')]);
     }
-
+    
+    /**
+     * productPriceAfterVendorDiscount
+     *
+     * @param  mixed $vendorData
+     * @param  mixed $product_discount_amount
+     * @param  mixed $doller_compare
+     * @param  mixed $cart
+     * @return void
+     */
     public function productPriceAfterVendorDiscount($vendorData,$product_discount_amount,$doller_compare,$cart)
     {
-        $allProductsSum = $vendorData->vendorProducts->sum(function ($product)
-        {
-            return $product->pvariant->price * $product->quantity;
-        });
-
+        $allProductsSum = 0;
+        $cart_products = CartProduct::with(['product.variant', 'addon.option'])
+        ->where('vendor_id', $vendorData->vendor_id)
+        ->where('cart_id', $vendorData->cart_id)
+        ->get();
+        foreach ($cart_products as $cart_product) {
+            // Calculate total price for the product variant
+            $total_price =$cart_product->pvariant->actual_price ?? $cart_product->pvariant->price??0;
+            // Calculate the total price of the product (variant price * quantity)
+            $allProductsSum += $total_price * $cart_product->quantity;
+            // Calculate the total price of the addons for the product
+            $product_addon_price = 0;
+            foreach ($cart_product->addon as $addon) {
+                $addon_option = $addon->option;
+                if ($addon_option) {
+                    $addon_price = $addon_option->price * $cart_product->quantity;
+                    $product_addon_price += $addon_price;
+                }
+            }
+            // Add the total addon price to the overall sum
+            $allProductsSum += $product_addon_price;
+        }
         $PromoDelete = 0;
-        if (isset($vendorData->coupon) && !empty($vendorData->coupon) )
+        if (isset($vendorData->coupon) && !empty($vendorData->coupon) ) 
         {
-
-            if ( $PromoDelete !=1)
+            if ( $PromoDelete !=1) 
             {
 
                     $minimum_spend = 0;
@@ -702,8 +727,6 @@ class CartController extends BaseController
                     if (isset($vendorData->coupon->promo->maximum_spend)) {
                         $maximum_spend = $vendorData->coupon->promo->maximum_spend * $doller_compare;
                     }
-
-                    // $vendorData->vendorProducts;
                     if( ($minimum_spend <= $allProductsSum ) && ($maximum_spend >= $allProductsSum))
                     {
                             if ($vendorData->coupon->promo->promo_type_id == 2) {
@@ -712,17 +735,14 @@ class CartController extends BaseController
                                 $vendor_discount_amount = ($product_discount_amount * $vendorData->coupon->promo->amount / 100);
                             }
                     }else{
-
                         $cart->coupon()->delete();
                         $vendorData->coupon()->delete();
                         unset($vendorData->coupon);
-                        return  0;
+                       return  0;
                     }
-            }
+            } 
         }
-
         return $vendor_discount_amount??0;
-
     }
 
 
