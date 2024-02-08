74f413a94ed077bac34dd142cc3e6d803afb67c9
gaurav sandhu <gaurav.sandhu@codebrewinnovations.com>
2024-02-08 17:03:31 +0530

    pooling toggle issue fix

--- resources/views/frontend/booking/index.blade.php
+++ resources/views/frontend/booking/index.blade.php
@@ -368,6 +368,12 @@ input[type=number]::-webkit-outer-spin-button {
                                 <label class="pool_label mb-0  my-2 active " for="cab_booking" id="label_cab_booking">
                                     <h5 class="m-0" id="pool_5">{{ __('Booking') }}</h5>
                                 </label>
+
+                                <input type="radio" class="pool_radio is_cab_pooling_radio" id="cab_pooling" name="is_cab_pooling_radio"
+                                      value="1">
+                                <label class="pool_label mb-0  my-2" for="cab_pooling" id="label_cab_pooling">
+                                    <h5 class="m-0" id="pool_5">{{ __('Pooling') }}</h5>
+                                </label>
                             </div>
                             @endif
 
