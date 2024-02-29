44e7b64cc2e4e1ba805a502aa879ce1b9058b85e
Rohit Mahajan <harishk@codebrewinnovations.com>
2024-02-29 17:24:49 +0530

    cart issue

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -1986,11 +1986,7 @@ class CartController extends BaseController
             $cart->total_payable_amount  += $totalDeliveryCharges;
         }
 
-        $cart->tip = array(
-            ['label' => '5%', 'value' => decimal_format(0.05 * $total_payable_amount_calc_tip)],
-            ['label' => '10%', 'value' => decimal_format(0.1 * $total_payable_amount_calc_tip)],
-            ['label' => '15%', 'value' => decimal_format(0.15 * $total_payable_amount_calc_tip)]
-        );
+        
         $wallet_amount_used = 0;
         if (isset($user)) {
             if ($user->balanceFloat > 0) {
@@ -2060,6 +2056,12 @@ class CartController extends BaseController
         }
 
         $total_payable_amount_calc_tip = $cart->total_payable_amount - $total_taxable_amount;
+        
+        $cart->tip = array(
+            ['label' => '5%', 'value' => decimal_format(0.05 * $total_payable_amount_calc_tip)],
+            ['label' => '10%', 'value' => decimal_format(0.1 * $total_payable_amount_calc_tip)],
+            ['label' => '15%', 'value' => decimal_format(0.15 * $total_payable_amount_calc_tip)]
+        );
 
         if(@$rental_price > 0){
             $cart->total_payable_amount = $rental_price;
