77baaaf248c8cb90361b9f84363480d382b9e8f7
Rajat Kumar <rajat.k@codebrewinnovations.com>
2024-03-05 18:59:03 +0530

    changes related to jiffix taxi calculation

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -679,77 +679,6 @@ class CartController extends BaseController
         return response()->json(['message' => __('Empty cart successfully.')]);
     }
 
-    /**
-     * productPriceAfterVendorDiscount
-     *
-     * @param  mixed $vendorData
-     * @param  mixed $product_discount_amount
-     * @param  mixed $doller_compare
-     * @param  mixed $cart
-     * @return void
-     */
-  public function productPriceAfterVendorDiscount($vendorData,$product_discount_amount,$doller_compare,$cart)
-    {
-
-        $allProductsSum = 0;
-        $cart_products = CartProduct::with(['product.variant', 'addon.option'])
-        ->where('vendor_id', $vendorData->vendor_id)
-        ->where('cart_id', $vendorData->cart_id)
-        ->get();
-        foreach ($cart_products as $cart_product) {
-            // Calculate total price for the product variant
-            $total_price =$cart_product->pvariant->actual_price ?? $cart_product->pvariant->price??0;
-            // Calculate the total price of the product (variant price * quantity)
-            $allProductsSum += $total_price * $cart_product->quantity;
-            // Calculate the total price of the addons for the product
-            $product_addon_price = 0;
-            foreach ($cart_product->addon as $addon) {
-                $addon_option = $addon->option;
-                if ($addon_option) {
-                    $addon_price = $addon_option->price * $cart_product->quantity;
-                    $product_addon_price += $addon_price;
-                }
-            }
-            // Add the total addon price to the overall sum
-            $allProductsSum += $product_addon_price;
-        }
-        $PromoDelete = 0;
-        $data['vendor_discount_amount'] = 0;
-        $data['deliveryfeeOnCoupon'] = 0;
-        if (isset($vendorData->coupon) && !empty($vendorData->coupon) )
-        {
-            if ( $PromoDelete !=1)
-            {
-
-                    $minimum_spend = 0;
-                    if (isset($vendorData->coupon->promo->minimum_spend)) {
-                        $minimum_spend = $vendorData->coupon->promo->minimum_spend * $doller_compare;
-                    }
-
-                    $maximum_spend = 0;
-                    if (isset($vendorData->coupon->promo->maximum_spend)) {
-                        $maximum_spend = $vendorData->coupon->promo->maximum_spend * $doller_compare;
-                    }
-                    if( ($minimum_spend <= $allProductsSum ) && ($maximum_spend >= $allProductsSum))
-                    {
-                            if ($vendorData->coupon->promo->promo_type_id == 2) {
-                                $data['vendor_discount_amount'] = $vendorData->coupon->promo->amount;
-                            } else {
-                                $data['vendor_discount_amount'] = ($product_discount_amount * $vendorData->coupon->promo->amount / 100);
-                            }
-                            if ($vendorData->coupon->promo->allow_free_delivery == 1) {
-                                $data['deliveryfeeOnCoupon'] = 1;
-                            }
-                    }else{
-                        $cart->coupon()->delete();
-                        $vendorData->coupon()->delete();
-                        unset($vendorData->coupon);
-                       return $data;
-                    }
-            }
-        }
-        return $data??0;
-    }
 
     public function getCart($cart, $langId = '1', $currency = '1', $type = 'delivery',$code = 'D')
     {
@@ -1331,7 +1260,7 @@ class CartController extends BaseController
                                     $tax_amount = ($price_in_doller_compare * $rate) / 100;
 
                                      //Find vendor Product Discount here
-                                     $productPriceAfterVendorDiscount  = $this->productPriceAfterVendorDiscount($prod,$quantity_price,$clientCurrency->doller_compare,$cart);
+                                     $productPriceAfterVendorDiscount  = productPriceAfterVendorDiscount($prod,$quantity_price,$clientCurrency->doller_compare,$cart);
                                      $quantity_price = $quantity_price - $productPriceAfterVendorDiscount['vendor_discount_amount'];
                                      if ($productPriceAfterVendorDiscount['deliveryfeeOnCoupon'] == 1) {
                                         $deliveryfeeOnCoupon = 1;
@@ -1354,9 +1283,9 @@ class CartController extends BaseController
                                         'sku' => ucfirst($prod->pvariant->sku),
                                     );
                                 }
-                            }else{
+                            }else{  
                                  //Find vendor Product Discount here
-                                 $productPriceAfterVendorDiscount  = $this->productPriceAfterVendorDiscount($prod,$quantity_price,$clientCurrency->doller_compare,$cart);
+                                 $productPriceAfterVendorDiscount  = productPriceAfterVendorDiscount($prod,$quantity_price,$clientCurrency->doller_compare,$cart);
                                  $quantity_price = $quantity_price - $productPriceAfterVendorDiscount['vendor_discount_amount'];
                                  if ($productPriceAfterVendorDiscount['deliveryfeeOnCoupon'] == 1) {
                                     $deliveryfeeOnCoupon = 1;
