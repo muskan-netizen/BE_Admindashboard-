b72c7380e9ad10250bb72a707c4a73016566b113
inderwadhawan <nitish@codebrewinnovations.com>
2024-03-07 16:24:50 +0530

    wallet amount issue fixed for pickup drop functionality

--- app/Http/Controllers/Front/PickupDeliveryController.php
+++ app/Http/Controllers/Front/PickupDeliveryController.php
@@ -1448,11 +1448,10 @@ class PickupDeliveryController extends FrontController{
                     'order_id' =>  $order->id,
                     'dispatcher_status_option_id' =>  1,
                     'vendor_id' =>  $vendor]);
-
-                    if ($request->payment_option_id == 2){
-                        $wal =   $wallet->forceWithdrawFloat($order->payable_amount, ['Wallet has been <b>debited</b> for order number <b>' . $order->order_number . '</b>']);
+                    $ex_gateways_wallet = [4,36,40,41,22]; // stripe,mycash,userede,openpay,ccavenue
+                    if (in_array($order->payment_option_id, $ex_gateways_wallet )){
+                        $wal =   $wallet->forceWithdrawFloat($order->wallet_amount_used, ['Wallet has been <b>debited</b> for order number <b>' . $order->order_number . '</b>']);
                     }
-                 return $response;
                 }
                 return $response;
             }
