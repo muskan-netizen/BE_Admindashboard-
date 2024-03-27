c7613546e5531c972d22d16029d1cfc14fc181ed
harjot <harjot@gmail.com>
2024-03-27 17:51:10 +0530

    bipbeep pricing rules issue and tod subcategories

--- resources/views/backend/order/order-parts/orderTable.blade.php
+++ resources/views/backend/order/order-parts/orderTable.blade.php
@@ -271,9 +271,8 @@
 <!--                                                         {{ $vendor['delivery_fee'] = 0 }} -->
                                                     @endif
 
-                                                    <span>
-                                                        {{ $clientCurrency->currency->symbol }}{{ decimal_format($vendor['subtotal_amount'] - $vendor['discount_amount'] + $vendor['total_container_charges'] + $vendor['taxable_amount'] + $vendor['service_fee_percentage_amount'] + $vendor['fixed_fee'] + $vendor['delivery_fee'] + $vendor['additional_price'] + $vendor['toll_amount']-$order->wallet_amount_used) }}
-                                                        </span>
+                                                    <span>{{ $clientCurrency->currency->symbol }}{{ decimal_format($vendor['subtotal_amount'] - $vendor['discount_amount'] + $vendor['total_container_charges'] + $vendor['taxable_amount'] + $vendor['service_fee_percentage_amount'] + $vendor['fixed_fee'] + $vendor['delivery_fee'] + $vendor['additional_price'] + $vendor['toll_amount']-$order->wallet_amount_used) }}
+                                                    </span>
                                                 </li>
                                             </ul>
                                         </div>
