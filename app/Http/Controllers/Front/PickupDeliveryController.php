384dd62f9e9083b2d7e6a7da195f822140851d3d
Rajat Kumar <rajat.k@codebrewinnovations.com>
2024-04-08 18:50:13 +0530

    conflictions resolve done

--- app/Http/Controllers/Front/PickupDeliveryController.php
+++ app/Http/Controllers/Front/PickupDeliveryController.php
@@ -1530,26 +1530,14 @@ class PickupDeliveryController extends FrontController{
                     return $this->errorResponse('Coupon Code apply only first order.', 422);
                 }
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> pre_production
             $order_vendor_user_promo_count = OrderVendor::where(['coupon_id' => $request->coupon_id])->count();
             if($order_vendor_user_promo_count >= $cart_detail->limit_total){
                 return $this->errorResponse(__('Coupon Code limit has been reached.'), 422);
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> pre_production
             $order_vendor_user_promo_count = OrderVendor::where(['user_id' => $user->id, 'coupon_id' => $request->coupon_id])->count();
             if($order_vendor_user_promo_count >= $cart_detail->limit_per_user){
                 return $this->errorResponse(__('Coupon Code already applied.'), 422);
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> pre_production
             if($cart_detail->promo_type_id == 2){
                 $cart_detail['new_amount'] = $cart_detail->amount;
                 if($cart_detail['new_amount'] < 0)
