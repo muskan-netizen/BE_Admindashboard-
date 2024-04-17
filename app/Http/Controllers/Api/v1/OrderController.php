9fdde52af302571be8d84dfbcdf6ed3df5ebca16
inderwadhawan <nitish@codebrewinnovations.com>
2024-04-17 15:07:00 +0530

    free delivery calculation issue fixed

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -915,10 +915,10 @@ class OrderController extends BaseController
                                 $vendor_discount_amount = $vendor_discount_amount +  $delivery_fee;
                                 $vendor_payable_amount = $vendor_payable_amount - $delivery_fee;
                                 $total_discount += $delivery_fee;
-                                $totalFreeDeliveryCharges += $delivery_fee;
+                                // $totalFreeDeliveryCharges += $delivery_fee;
                                 $deliveryfeeOnCoupon = 1;
                             }
-                            
+
                             if(isset($rate) && $total_discount > 0 ){
                                $discount = ($total_discount*$rate) / 100;
                                $vendor_taxable_amount -= $discount;
@@ -1058,7 +1058,6 @@ class OrderController extends BaseController
                     $order->total_discount = $total_discount;
                     $payable_amount = $payable_amount + $total_delivery_fee - $total_discount -$totalFreeDeliveryCharges;
 
-
                     if ($loyalty_amount_saved > 0) {
                         if ($loyalty_amount_saved > $payable_amount) {
                             $loyalty_amount_saved = $payable_amount;
@@ -1066,7 +1065,6 @@ class OrderController extends BaseController
                         }
                     }
                     $payable_amount = ($payable_amount + $fixed_fee_amount) - $loyalty_amount_saved;
-
                     $ex_gateways_wallet = [4,36,40,41,22]; // stripe,mycash,userede,openpay
                     $wallet_amount_used = 0;
                     if ($user->balanceFloat > 0) {
