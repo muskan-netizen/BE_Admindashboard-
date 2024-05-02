677d1763ba1092a2bb477cf27edbc7edf698f31e
Suraj <cblsuraj1@gmail.com>
2024-05-02 14:36:14 +0530

    vendor register page and reject order sms and notification

--- database/seeders/SmsTemplateSeeder.php
+++ database/seeders/SmsTemplateSeeder.php
@@ -27,7 +27,8 @@ class SmsTemplateSeeder extends Seeder
             ['id' => 8,'label' => "Otp Sms For Tracking url", 'slug' => 'otp-sms-tracking-url', 'subject' => "Otp Sms Access For Tracking Url", 'content' => "Please enter OTP {otp_code}. Keep it safe and don't show to other.", 'tags' => "{otp_code}"],
             ['id' => 9,'label' => "Order Canceled", 'slug' => 'order-canceled', 'subject' => "Ride Cancel By Driver", 'content' => "Hi {user_name} Your ride {order_number} canceled by driver.", 'tags' => "{user_name},{order_number}"],
             ['id' => 10,'label' => "Order Completed", 'slug' => 'order-completed', 'subject' => "Order Completed", 'content' => "Hi {user_name} Your ride {order_number} successfully completed.", 'tags' => "{user_name},{order_number}"],
-            ['id' => 11,'label' => "Ride Booked", 'slug' => 'ride-booked', 'subject' => "Ride Booked Confirmation.", 'content' => "Hi {user_name} Your ride {order_number} successfully booked.", 'tags' => "{user_name},{order_number}"]
+            ['id' => 11,'label' => "Ride Booked", 'slug' => 'ride-booked', 'subject' => "Ride Booked Confirmation.", 'content' => "Hi {user_name} Your ride {order_number} successfully booked.", 'tags' => "{user_name},{order_number}"],
+            ['id' => 12,'label' => "Order Canceled (Vendor)", 'slug' => 'order-canceled-vendor', 'subject' => "Order Canceled (Vendor)", 'content' => "Your order ({order_id}) is canecelled by Admin", 'tags' => "{order_number}"],
 
         );
 
