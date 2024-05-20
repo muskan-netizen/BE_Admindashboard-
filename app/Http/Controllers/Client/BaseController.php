7dae08c997f3b322e697e2340dedf09643be20d7
Bamit Kumar <bamit.kumar@codebrewinnovations.com>
2024-05-20 13:37:43 +0530

    removed code

--- app/Http/Controllers/Client/BaseController.php
+++ app/Http/Controllers/Client/BaseController.php
@@ -643,24 +643,6 @@ class BaseController extends Controller
         return true;
     }
 
-    public function setMailDetail($mail_driver, $mail_host, $mail_port, $mail_username, $mail_password, $mail_encryption ){
-        $config = array(
-            'driver' => $mail_driver,
-            'host' => $mail_host,
-            'port' => $mail_port,
-
-            'encryption' => $mail_encryption,
-            'username' => $mail_username,
-            'password' => $mail_password,
-            'sendmail' => '/usr/sbin/sendmail -bs',
-            'pretend' => false,
-        );
-        Config::set('mail', $config);
-        $app = App::getInstance();
-        // $app->register('Illuminate\Mail\MailServiceProvider');
-        return  $config;
-    }
-
 
     }
 
--- app/Http/Controllers/Front/DispatcherController.php
+++ app/Http/Controllers/Front/DispatcherController.php
@@ -503,6 +503,10 @@ class DispatcherController extends FrontController
                             if($request->status_option_id == 3 ){
                                 $this->cancelOrderByDriver($checkiftokenExist);
                             }
+                            if($request->status_option_id  == 6){
+                                    $orderData = Order::select('order_number')->where('id', $checkiftokenExist->order_id)->first();
+                                    $this->sendCompletedEmail($orderUserInfo, $orderData);
+                            }
                     }
                 }
 
@@ -584,6 +588,37 @@ class DispatcherController extends FrontController
             return $this->errorResponse($e->getMessage(), $e->getCode());
 
         }
+    }
+
+    public function sendCompletedEmail($user, $orderData) {
+        $client = Client::select('id', 'name', 'email', 'phone_number', 'logo')->where('id', '>', 0)->first();
+            $data = ClientPreference::select('sms_key', 'sms_secret', 'sms_from', 'mail_type', 'mail_driver', 'mail_host', 'mail_port', 'mail_username', 'sms_provider', 'mail_password', 'mail_encryption', 'mail_from')->where('id', '>', 0)->first();
+
+            
+                if (!empty($data->mail_driver) && !empty($data->mail_host) && !empty($data->mail_port) && !empty($data->mail_port) && !empty($data->mail_password) && !empty($data->mail_encryption)) {
+                    $confirured = $this->setMailDetail($data->mail_driver, $data->mail_host, $data->mail_port, $data->mail_username, $data->mail_password, $data->mail_encryption);
+                    $client_name = $client->name;
+                    $mail_from = $data->mail_from;
+                    $sendto = $user->email;
+                    try{
+                        $data = [
+                            'customer_name' => $user->name,
+                            'client_name' => $client_name,
+                            'code_text' => '',
+                            'order_number' => $orderData->order_number,
+                            'logo' => $client->logo['original']
+                        ];
+                        Mail::send('email.deliveredEmail', ['mailData'=>$data],
+                        function ($message) use($sendto, $client_name, $mail_from) {
+                            $message->from($mail_from, $client_name);
+                            $message->to($sendto)->subject('Signup Notification');
+                        });
+                        $response['send_email'] = 1;
+                    }
+                    catch(\Exception $e){
+                        return response()->json(['data' => $e->getMessage()]);
+                    }
+                }
     }
 
     /******************    ---- share all details of order for dispatcher -----   ******************/
