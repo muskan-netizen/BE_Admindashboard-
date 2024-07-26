4a595332199e966fc2cdaef17be00ed984e65244
vaishali <vaishali.kaushik@codebrewinnovations.com>
2024-07-26 11:34:55 +0530

    paystack wallet amount dedduction issue

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -2990,7 +2990,7 @@ class OrderController extends FrontController
             //     $payable_amount = ($request->total_amount + $fixed_fee_amount) - $loyalty_amount_saved ;
             // }
 
-            $ex_gateways_wallet = [4, 36, 40, 41, 22]; // stripe,mycash,userede,openpay
+            $ex_gateways_wallet = [4,5,36, 40, 41, 22]; // stripe,mycash,userede,openpay,paystack
 
             // $tip_amount = 0;
             // if (isset($request->tip)) {
